b4bfdb1a69f12f06ecb245c6e8c1e4876f0ec5c0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-04 20:29:35 -0700

    MISS NOTES

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -4,8 +4,12 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 
+using StringTools;
+
 class Boyfriend extends Character
 {
+	public var stunned:Bool = false;
+
 	public function new(x:Float, y:Float)
 	{
 		super(x, y);
@@ -13,10 +17,14 @@ class Boyfriend extends Character
 		var tex = FlxAtlasFrames.fromSparrow(AssetPaths.BOYFRIEND__png, AssetPaths.BOYFRIEND__xml);
 		frames = tex;
 		animation.addByPrefix('idle', 'BF idle dance', 24, false);
-		animation.addByPrefix('singUP', 'BF NOTE UP', 24, false);
-		animation.addByPrefix('singLEFT', 'BF NOTE LEFT', 24, false);
-		animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT', 24, false);
-		animation.addByPrefix('singDOWN', 'BF NOTE DOWN', 24, false);
+		animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
+		animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
+		animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
+		animation.addByPrefix('singDOWN', 'BF NOTE DOWN0', 24, false);
+		animation.addByPrefix('singUPmiss', 'BF NOTE UP MISS', 24, false);
+		animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
+		animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
+		animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
 		animation.addByPrefix('hey', 'BF HEY', 24, false);
 		playAnim('idle');
 
@@ -27,6 +35,19 @@ class Boyfriend extends Character
 		addOffset("singRIGHT", -38, -7);
 		addOffset("singLEFT", 12, -6);
 		addOffset("singDOWN", -10, -50);
+		addOffset("singUPmiss", -29, 27);
+		addOffset("singRIGHTmiss", -30, 21);
+		addOffset("singLEFTmiss", 12, 24);
+		addOffset("singDOWNmiss", -11, -19);
 		addOffset("hey", 7, 4);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished && !debugMode)
+		{
+			playAnim('idle', true, false, 10);
+		}
+		super.update(elapsed);
 	}
 }
