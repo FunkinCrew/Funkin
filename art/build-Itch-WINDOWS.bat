7d925ae0eccdbf0241c65385841ddb01aa725274
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-08 17:54:47 -0700

    build script fixin

--- art/build-Itch-WINDOWS.bat
+++ art/build-Itch-WINDOWS.bat
@@ -6,6 +6,6 @@ echo BUILDING GAME
 lime build windows -final
 echo UPLOADING TO ITCH
 butler push ./export/release/windows/bin ninja-muffin24/friday-night-funkin:windows
-butler status ninja-muffin24/ld47:windows
+butler status ninja-muffin24/friday-night-funkin:windows
 echo ITCH SHIT UPDATED LMAOOOOO
 pause
