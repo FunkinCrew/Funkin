e2d77eeec71f75bb095dc874ae633b808e1bd01c
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-15 21:22:13 -0700

    getting section meta data all fixed up

--- source/Section.hx
+++ source/Section.hx
@@ -8,6 +8,13 @@ class Section
 	public var notes:Array<Dynamic> = [];
 
 	public var lengthInSteps:Int = 16;
+	public var typeOfSection:Int = 0;
+	public var mustHitSection:Bool = true;
+
+	/**
+	 *	Copies the first section into the second section!
+	 */
+	public static var COPYCAT:Int = 0;
 
 	public function new(lengthInSteps:Int = 16)
 	{
