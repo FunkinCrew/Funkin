b595d6248637bef23ac3ff217ec8889a07828b56
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-21 17:22:12 -0700

    FIXED up bullshit

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -1,8 +1,10 @@
 package;
 
+import Controls.Control;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
+import flixel.input.keyboard.FlxKey;
 import flixel.util.FlxColor;
 
 class PauseSubState extends FlxSubState
@@ -19,6 +21,11 @@ class PauseSubState extends FlxSubState
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.J)
+		{
+			PlayerSettings.player1.controls.replaceBinding(Control.LEFT, Keys, FlxKey.J, null);
+		}
 
 		if (FlxG.keys.justPressed.ENTER)
 			close();
