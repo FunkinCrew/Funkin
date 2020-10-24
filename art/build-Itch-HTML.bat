87286b68c0941420344ba54035dac9480268dd5e
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-24 02:36:50 -0700

    fix miss animations

--- art/build-Itch-HTML.bat
+++ art/build-Itch-HTML.bat
@@ -5,7 +5,7 @@ cd ..
 echo BUILDING GAME
 lime build html5 -debug
 echo UPLOADING TO ITCH
-butler push ./export/release/html5/bin ninja-muffin24/funkin:html5
+butler push ./export/debug/html5/bin ninja-muffin24/funkin:html5
 butler status ninja-muffin24/funkin:html5
 echo ITCH SHIT UPDATED LMAOOOOO
 pause
