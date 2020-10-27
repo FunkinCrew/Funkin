847d850f27006caa5ba8f33eb2ec4d30435ffbc3
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-10-27 03:35:23 -0700

    LOSS SCREEN WIP

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TitleState));
+		addChild(new FlxGame(0, 0, FreeplayState));
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
