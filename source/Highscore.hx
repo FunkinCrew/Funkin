85dcbcc429825ae1c86e7ccfbf48ddd5c2b780c1
Brandon <brandy@scarecrowarts.com>
2020-11-06 21:59:51 -0500

    Update Highscore.hx

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -10,7 +10,9 @@ class Highscore
 	{
 		var daSong:String = formatSong(song, diff);
 
+		#if !switch
 		NGio.postScore(score, song);
+		#end
 
 		if (songScores.exists(daSong))
 		{
@@ -23,7 +25,9 @@ class Highscore
 
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0):Void
 	{
+		#if !switch
 		NGio.postScore(score, "Week " + week);
+		#end
 
 		var daWeek:String = formatSong('week' + week, diff);
 
