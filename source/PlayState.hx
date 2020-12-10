68a15dc8f7e30c9639de7ee762bde3b7cfa0a3d2
Gede Hari <harikresnagede@gmail.com>
2020-12-10 13:04:36 +0800

    Fixed song starting early when paused during countdown
    also tweaked something in main menu

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -576,7 +576,7 @@ class PlayState extends MusicBeatState
 	{
 		if (paused)
 		{
-			if (FlxG.sound.music != null)
+			if (FlxG.sound.music != null && !startingSong)
 			{
 				FlxG.sound.music.play();
 				Conductor.songPosition = FlxG.sound.music.time;
