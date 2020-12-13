2ff2d57540c91bbcd8eca3b29f9c7eba25c5b189
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2020-12-13 02:45:36 -0500

    new shit babyu

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -81,7 +81,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0.06);
 
-		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "v" + Application.current.meta.get('version'));
+		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "v" + Application.current.meta.get('version'), 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
