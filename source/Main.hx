ed0ee731b417433dc2af198457bc2fc573f11147
luckydog7 <59097731+luckydog7@users.noreply.github.com>
2021-01-11 15:46:28 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -16,23 +16,3 @@ class Main extends Sprite
 		#end
 	}
 }
-||||||| empty tree
-=======
-package;
-
-import flixel.FlxGame;
-import openfl.display.FPS;
-import openfl.display.Sprite;
-
-class Main extends Sprite
-{
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(0, 0, TitleState));
-
-		#if !mobile
-		addChild(new FPS(10, 3, 0xFFFFFF));
-		#end
-	}
-}
