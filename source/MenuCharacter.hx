9fb2fc1d3afb9fb6370e0d0bdc61444664076456
luckydog7 <59097731+luckydog7@users.noreply.github.com>
2021-01-11 15:29:16 +0300

    Update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package;
 
 import flixel.FlxSprite;
@@ -61,4 +60,3 @@ class MenuCharacter extends FlxSprite
 		updateHitbox();
 	}
 }
->>>>>>> 62febd7ad6964e608ca494f18a8cbaa38ad5cf0d
