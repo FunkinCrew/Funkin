d81e7157ca6c056b106dcc65d773553167e7bffb
luckydog7 <59097731+luckydog7@users.noreply.github.com>
2021-01-11 15:52:20 +0300

    Update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -28,35 +28,3 @@ class MenuCharacter extends FlxSprite
 		updateHitbox();
 	}
 }
-||||||| empty tree
-=======
-package;
-
-import flixel.FlxSprite;
-import flixel.graphics.frames.FlxAtlasFrames;
-
-class MenuCharacter extends FlxSprite
-{
-	public var character:String;
-
-	public function new(x:Float, character:String = 'bf')
-	{
-		super(x);
-
-		this.character = character;
-
-		var tex = FlxAtlasFrames.fromSparrow('assets/images/campaign_menu_UI_characters.png', 'assets/images/campaign_menu_UI_characters.xml');
-		frames = tex;
-
-		animation.addByPrefix('bf', "BF idle dance white", 24);
-		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
-		animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
-		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
-		animation.addByPrefix('spooky', "spooky dance idle BLACK LINES", 24);
-		animation.addByPrefix('pico', "Pico Idle Dance", 24);
-		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
-
-		animation.play(character);
-		updateHitbox();
-	}
-}
