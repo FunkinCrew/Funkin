7894b6e6f29f2d5cf91df0e8a9492004f0a5b2dc
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-01-19 22:58:07 -0500

    fix eggnog normal woops!

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -254,7 +254,9 @@ class PlayState extends MusicBeatState
 		{
 			curStage = 'mall';
 
-			var bg:FlxSprite = new FlxSprite(-1000, -400).loadGraphic('assets/images/christmas/bgWalls.png');
+			defaultCamZoom = 0.80;
+
+			var bg:FlxSprite = new FlxSprite(-1000, -500).loadGraphic('assets/images/christmas/bgWalls.png');
 			bg.antialiasing = true;
 			bg.scrollFactor.set(0.2, 0.2);
 			bg.active = false;
@@ -262,41 +264,41 @@ class PlayState extends MusicBeatState
 			bg.updateHitbox();
 			add(bg);
 
-			upperBoppers = new FlxSprite(-130, 40);
+			upperBoppers = new FlxSprite(-240, -90);
 			upperBoppers.frames = FlxAtlasFrames.fromSparrow('assets/images/christmas/upperBop.png', 'assets/images/christmas/upperBop.xml');
 			upperBoppers.animation.addByPrefix('bop', "Upper Crowd Bob", 24, false);
 			upperBoppers.antialiasing = true;
 			upperBoppers.scrollFactor.set(0.33, 0.33);
-			upperBoppers.setGraphicSize(Std.int(upperBoppers.width * 0.7));
+			upperBoppers.setGraphicSize(Std.int(upperBoppers.width * 0.85));
 			upperBoppers.updateHitbox();
 			add(upperBoppers);
 
-			var bgEscalator:FlxSprite = new FlxSprite(-1000, -430).loadGraphic('assets/images/christmas/bgEscalator.png');
+			var bgEscalator:FlxSprite = new FlxSprite(-1100, -600).loadGraphic('assets/images/christmas/bgEscalator.png');
 			bgEscalator.antialiasing = true;
 			bgEscalator.scrollFactor.set(0.3, 0.3);
 			bgEscalator.active = false;
-			bgEscalator.setGraphicSize(Std.int(bgEscalator.width * 0.8));
+			bgEscalator.setGraphicSize(Std.int(bgEscalator.width * 0.9));
 			bgEscalator.updateHitbox();
 			add(bgEscalator);
 
-			var fgSnow:FlxSprite = new FlxSprite(-400, 700).loadGraphic('assets/images/christmas/fgSnow.png');
-			fgSnow.active = false;
-			fgSnow.antialiasing = true;
-			add(fgSnow);
-
-			var tree:FlxSprite = new FlxSprite(300, -250).loadGraphic('assets/images/christmas/christmasTree.png');
+			var tree:FlxSprite = new FlxSprite(370, -250).loadGraphic('assets/images/christmas/christmasTree.png');
 			tree.antialiasing = true;
-			tree.scrollFactor.set(0.85, 0.85);
+			tree.scrollFactor.set(0.40, 0.40);
 			add(tree);
 
-			bottomBoppers = new FlxSprite(-300, 220);
+			bottomBoppers = new FlxSprite(-300, 140);
 			bottomBoppers.frames = FlxAtlasFrames.fromSparrow('assets/images/christmas/bottomBop.png', 'assets/images/christmas/bottomBop.xml');
 			bottomBoppers.animation.addByPrefix('bop', 'Bottom Level Boppers', 24, false);
 			bottomBoppers.antialiasing = true;
 			bottomBoppers.scrollFactor.set(0.9, 0.9);
-			bottomBoppers.setGraphicSize(Std.int(bottomBoppers.width * 0.9));
+			bottomBoppers.setGraphicSize(Std.int(bottomBoppers.width * 1));
 			bottomBoppers.updateHitbox();
 			add(bottomBoppers);
+
+			var fgSnow:FlxSprite = new FlxSprite(-600, 700).loadGraphic('assets/images/christmas/fgSnow.png');
+			fgSnow.active = false;
+			fgSnow.antialiasing = true;
+			add(fgSnow);
 		}
 		else
 		{
@@ -391,6 +393,10 @@ class PlayState extends MusicBeatState
 
 					resetFastCar();
 					add(fastCar);
+				}
+			case 'mall':
+				{
+					boyfriend.x += 200;
 				}
 		}
 
@@ -933,9 +939,12 @@ class PlayState extends MusicBeatState
 			{
 				camFollow.setPosition(boyfriend.getMidpoint().x - 100, boyfriend.getMidpoint().y - 100);
 
-				if (curStage == 'limo')
+				switch (curStage)
 				{
-					camFollow.x = boyfriend.getMidpoint().x - 300;
+					case 'limo':
+						camFollow.x = boyfriend.getMidpoint().x - 300;
+					case 'mall':
+						camFollow.y = boyfriend.getMidpoint().y - 200;
 				}
 
 				if (SONG.song.toLowerCase() == 'tutorial')
