55b5f56624e19a3ff3379dbab570b8b16a81c782
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-01-20 04:11:55 -0500

    scripted type beat

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -106,9 +106,9 @@ class TitleState extends MusicBeatState
 			diamond.destroyOnNoUse = false;
 
 			FlxTransitionableState.defaultTransIn = new TransitionData(FADE, FlxColor.BLACK, 1, new FlxPoint(0, -1), {asset: diamond, width: 32, height: 32},
-				new FlxRect(-200, 0, FlxG.width * 1.4, FlxG.height));
+				new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
 			FlxTransitionableState.defaultTransOut = new TransitionData(FADE, FlxColor.BLACK, 0.7, new FlxPoint(0, 1),
-				{asset: diamond, width: 32, height: 32}, new FlxRect(-200, 0, FlxG.width * 1.4, FlxG.height));
+				{asset: diamond, width: 32, height: 32}, new FlxRect(-200, -200, FlxG.width * 1.4, FlxG.height * 1.4));
 
 			transIn = FlxTransitionableState.defaultTransIn;
 			transOut = FlxTransitionableState.defaultTransOut;
