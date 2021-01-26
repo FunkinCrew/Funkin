a8c71d3e03114ac2ebb2d860de0f71bcd419b933
Aiden Baker <baker.aiden@protonmail.com>
2021-01-26 18:48:31 -0500

    Cleaner implementation

--- source/NGio.hx
+++ source/NGio.hx
@@ -39,18 +39,22 @@ class NGio
 
 		new FlxTimer().start(2, function(tmr:FlxTimer)
 		{
-			#if ng
-			var call = NG.core.calls.app.getCurrentVersion(GAME_VER).addDataHandler(function(response:Response<GetCurrentVersionResult>)
+			if(api != "")
 			{
-				GAME_VER = response.result.data.current_version;
-				trace('CURRENT NG VERSION: ' + GAME_VER);
-				gotOnlineVer = true;
-			});
+				var call = NG.core.calls.app.getCurrentVersion(GAME_VER).addDataHandler(function(response:Response<GetCurrentVersionResult>)
+				{
+					GAME_VER = response.result.data.current_version;
+					trace('CURRENT NG VERSION: ' + GAME_VER);
+					gotOnlineVer = true;
+				});
 
-			call.send();
-			#else
+				call.send();
+			}
+
+			else
+			{
 				gotOnlineVer = false;
-			#end
+			}
 		});
 	}
 
