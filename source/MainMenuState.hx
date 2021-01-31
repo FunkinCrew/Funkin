f8a236a26df9d3f701ecf49db64fd3643a62d2b0
Bulby <Benharless820@gmail.com>
2021-01-31 16:34:00 -0500

    fix newgrounds stuff

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -10,7 +10,6 @@ import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
-import io.newgrounds.NG;
 import lime.app.Application;
 
 using StringTools;
