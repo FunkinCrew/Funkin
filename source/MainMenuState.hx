a23cfa3274e1b412738a6360165eafbca65d596b
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-02-02 02:20:14 -0500

    idk campaign menu shit

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -22,7 +22,7 @@ class MainMenuState extends MusicBeatState
 	var menuItems:FlxTypedGroup<FlxSprite>;
 
 	#if !switch
-	var optionShit:Array<String> = ['story mode', 'freeplay', 'donate', 'options'];
+	var optionShit:Array<String> = ['story mode', 'freeplay', 'donate'];
 	#else
 	var optionShit:Array<String> = ['story mode', 'freeplay'];
 	#end
