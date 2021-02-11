41efd4f3db3b0e78aa46af410b1ff7bec9e8f261
MtH <mth@mth.moe>
2021-02-11 23:06:26 +0100

    BPM map, trim off useless song stuff, adjust MILF cam zoom

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -1,5 +1,6 @@
 package;
 
+import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.FlxSubState;
 
@@ -37,7 +38,7 @@ class MusicBeatSubstate extends FlxSubState
 		everyStep();
 
 		updateCurStep();
-		curBeat = Math.round(curStep / 4);
+		curBeat = Math.floor(curStep / 4);
 
 		super.update(elapsed);
 	}
@@ -59,7 +60,18 @@ class MusicBeatSubstate extends FlxSubState
 
 	private function updateCurStep():Void
 	{
-		curStep = Math.floor(Conductor.songPosition / Conductor.stepCrochet);
+		var lastChange:BPMChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			bpm: 0
+		}
+		for (i in 0...Conductor.bpmChangeMap.length)
+		{
+			if (Conductor.songPosition > Conductor.bpmChangeMap[i].songTime)
+				lastChange = Conductor.bpmChangeMap[i];
+		}
+
+		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
 	}
 
 	public function stepHit():Void
