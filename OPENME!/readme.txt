3ecc2181744c904b644716e1f20638b0a025de27
luckydog7 <redguy0102@gmail.com>
2021-02-18 14:20:34 +0300

    hi

---
+++ OPENME!/readme.txt
@@ -0,0 +1,2 @@
+this folder need to copy in C:\HaxeToolkit\haxe\lib
+this folder need to gamepad
---
+++ source/CoolUtil.hx
@@ -0,0 +1,30 @@
+package;
+
+import lime.utils.Assets;
+
+using StringTools;
+
+class CoolUtil
+{
+	public static function coolTextFile(path:String):Array<String>
+	{
+		var daList:Array<String> = Assets.getText(path).trim().split('\n');
+
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+
+		return daList;
+	}
+
+	public static function numberArray(max:Int, ?min = 0):Array<Int>
+	{
+		var dumbArray:Array<Int> = [];
+		for (i in min...max)
+		{
+			dumbArray.push(i);
+		}
+		return dumbArray;
+	}
+}
