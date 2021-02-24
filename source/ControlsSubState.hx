f86dc69cde9b3b3f566dc226fd7490e7029047bb
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-02-24 18:32:51 -0500

    options menu in progress

--- source/ControlsSubState.hx
+++ source/ControlsSubState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxSubState;
 
 class ControlsSubState extends FlxSubState
@@ -7,5 +8,8 @@ class ControlsSubState extends FlxSubState
 	public function new()
 	{
 		super();
+
+		var bullshit = new FlxSprite().makeGraphic(100, 100);
+		add(bullshit);
 	}
 }
--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -32,44 +32,50 @@ class OptionsMenu extends MusicBeatState
 		menuBG.antialiasing = true;
 		add(menuBG);
 
-		grpControls = new FlxTypedGroup<Alphabet>();
-		add(grpControls);
+		/* 
+			grpControls = new FlxTypedGroup<Alphabet>();
+			add(grpControls);
 
-		for (i in 0...controlsStrings.length)
-		{
-			if (controlsStrings[i].indexOf('set') != -1)
+			for (i in 0...controlsStrings.length)
 			{
-				var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, controlsStrings[i].substring(3) + ': ' + controlsStrings[i + 1], true, false);
-				controlLabel.isMenuItem = true;
-				controlLabel.targetY = i;
-				grpControls.add(controlLabel);
+				if (controlsStrings[i].indexOf('set') != -1)
+				{
+					var controlLabel:Alphabet = new Alphabet(0, (70 * i) + 30, controlsStrings[i].substring(3) + ': ' + controlsStrings[i + 1], true, false);
+					controlLabel.isMenuItem = true;
+					controlLabel.targetY = i;
+					grpControls.add(controlLabel);
+				}
+				// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
 			}
-			// DONT PUT X IN THE FIRST PARAMETER OF new ALPHABET() !!
-		}
+		 */
 
 		super.create();
+
+		openSubState(new OptionsSubState());
 	}
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		if (controls.ACCEPT)
-		{
-			changeBinding();
-		}
+		/* 
+			if (controls.ACCEPT)
+			{
+				changeBinding();
+			}
 
-		if (isSettingControl)
-			waitingInput();
-		else
-		{
-			if (controls.BACK)
-				FlxG.switchState(new MainMenuState());
-			if (controls.UP_P)
-				changeSelection(-1);
-			if (controls.DOWN_P)
-				changeSelection(1);
-		}
+			if (isSettingControl)
+				waitingInput();
+			else
+			{
+				if (controls.BACK)
+					FlxG.switchState(new MainMenuState());
+				if (controls.UP_P)
+					changeSelection(-1);
+				if (controls.DOWN_P)
+					changeSelection(1);
+			}
+		 */
 	}
 
 	function waitingInput():Void
