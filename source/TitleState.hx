29d3d157413869ee6b80ffc3f49aa86819099d7c
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-02-23 23:32:53 -0500

    bullshit lol

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -237,6 +237,16 @@ class TitleState extends MusicBeatState
 
 		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER;
 
+		#if mobile
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+			{
+				pressedEnter = true;
+			}
+		}
+		#end
+
 		var gamepad:FlxGamepad = FlxG.gamepads.lastActive;
 
 		if (gamepad != null)
