14dcef21300753c2400c9374a5f762478162da6d
Bulby <Benharless820@gmail.com>
2021-03-08 08:52:09 -0500

    remove change modifiers because it is fucked up

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -14,7 +14,7 @@ class PauseSubState extends MusicBeatSubstate
 {
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
-	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Change Modifiers', 'Exit to menu'];
+	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Exit to menu'];
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
