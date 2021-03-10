96b03dd6870577e098544abfb1bfad2b54e5d04b
Kade M <KadePcGames@gmail.com>
2021-03-10 12:31:51 -0800

    offset works

--- source/Note.hx
+++ source/Note.hx
@@ -45,7 +45,7 @@ class Note extends FlxSprite
 		x += 50;
 		// MAKE SURE ITS DEFINITELY OFF SCREEN?
 		y -= 2000;
-		this.strumTime = strumTime;
+		this.strumTime = strumTime + FlxG.save.data.offset;
 
 		this.noteData = noteData;
 
