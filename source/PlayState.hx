656716bd203634ddc540cb8bf1b609f5f2d9ee5b
Lotusotho <69006987+lotusotho@users.noreply.github.com>
2021-03-11 01:03:16 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2388,10 +2388,10 @@ class PlayState extends MusicBeatState
 		if (OptionsSubState.Tritanopiabool)
 		{
 			var matrix:Array<Float> = [
-						0.20, 0.99, -.19, 0, 0,
-						0.16, 0.79, 0.04, 0, 0,
-						0.01, -.01,    1, 0, 0,
-						   0,    0,    0, 1, 0,
+						0.97, 0.11, -.08, 0, 0,
+						0.02, 0.82, 0.16, 0, 0,
+						0.06, 0.88, 0.18, 0, 0,
+					  	   0,    0,    0, 1, 0,
 					];
 			filters.push(new ColorMatrixFilter(matrix));
 		}
