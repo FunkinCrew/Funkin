4c15e30460c952b9a6d0be222c495c6bfd711985
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2021-03-13 13:23:07 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
+title FNF Build - User action required
 echo What would you like to build for?
 echo H - HTML5
 echo M - macOS
@@ -7,9 +8,13 @@ set /p menu="Your answer: "
        if %menu%==H goto BuildHTML5
        if %menu%==M goto BuildMacOS
        if %menu%==W goto BuildWindows
+       if %menu%==h goto BuildHTML5
+       if %menu%==m goto BuildMacOS
+       if %menu%==w goto BuildWindows
        cls
        
 :BuildHTML5
+title FNF Build - Building for HTML5
 echo.
 echo Starting HTML5 build...
 lime test html5 -debug
@@ -18,6 +23,7 @@ pause >nul
 exit
 
 :BuildMacOS
+title FNF Build - Building for macOS
 echo.
 echo Starting macOS build...
 lime test mac -debug
@@ -27,24 +33,10 @@ exit
 
 :BuildWindows
 echo.
-echo This will require the following installed:
-echo * Visual Studio 2019
-echo * MSVC v142 - VS 2019 C++ x64/x86 build tools
-echo * Windows SDK (10.0.17763.0)
-echo * C++ Profiling tools
-echo * C++ CMake tools for windows
-echo * C++ ATL for v142 build tools (x86 & x64)
-echo * C++ MFC for v142 build tools (x86 & x64)
-echo * C++/CLI support for v142 build tools (14.21)
-echo * C++ Modules for v142 build tools (x64/x86)
-echo * Clang Compiler for Windows
-echo * Windows 10 SDK (10.0.17134.0)
-echo * Windows 10 SDK (10.0.16299.0)
-echo * MSVC v141 - VS 2017 C++ x64/x86 build tools
-echo * MSVC v140 - VS 2015 C++ build tools (v14.00)
-echo You can find these in the Visual Studio Installer, seen here: https://visualstudio.microsoft.com
+echo These will require dependencies. You can find them in the README.md. It will also take *forever*.
 echo Press any key to start building.
 pause >nul
+title FNF Build - Building for Windows
 echo Starting Windows build...
 lime test windows -debug
 echo Done. Press any key to exit.
