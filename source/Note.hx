4fcbdca4c14a68c61aa2e2aef9b703b3b187d529
Kade M <KadePcGames@gmail.com>
2021-03-27 10:57:54 -0700

    fix downscroll notes

--- source/Note.hx
+++ source/Note.hx
@@ -64,20 +64,28 @@ class Note extends FlxSprite
 				animation.add('redScroll', [7]);
 				animation.add('blueScroll', [5]);
 				animation.add('purpleScroll', [4]);
-
+				
+				animation.add('purplehold', [0]);
+				animation.add('greenhold', [2]);
+				animation.add('redhold', [3]);
+				animation.add('bluehold', [1]);
+				
 				if (isSustainNote)
 				{
 					loadGraphic(Paths.image('weeb/pixelUI/arrowEnds'), true, 7, 6);
 
-					animation.add('purpleholdend', [4]);
-					animation.add('greenholdend', [6]);
-					animation.add('redholdend', [7]);
-					animation.add('blueholdend', [5]);
-
-					animation.add('purplehold', [0]);
-					animation.add('greenhold', [2]);
-					animation.add('redhold', [3]);
-					animation.add('bluehold', [1]);
+					if(!FlxG.save.data.downscroll)
+					{
+						animation.add('purpleholdend', [4]);
+						animation.add('greenholdend', [6]);
+						animation.add('redholdend', [7]);
+						animation.add('blueholdend', [5]);
+					}else {
+						animation.add('purpleholdend', [4], 0, false, false, true);
+						animation.add('greenholdend', [6], 0, false, false, true);
+						animation.add('redholdend', [7], 0, false, false, true);
+						animation.add('blueholdend', [5], 0, false, false, true);
+					}
 				}
 
 				setGraphicSize(Std.int(width * PlayState.daPixelZoom));
@@ -90,16 +98,24 @@ class Note extends FlxSprite
 				animation.addByPrefix('redScroll', 'red0');
 				animation.addByPrefix('blueScroll', 'blue0');
 				animation.addByPrefix('purpleScroll', 'purple0');
-
-				animation.addByPrefix('purpleholdend', 'pruple end hold');
-				animation.addByPrefix('greenholdend', 'green hold end');
-				animation.addByPrefix('redholdend', 'red hold end');
-				animation.addByPrefix('blueholdend', 'blue hold end');
-
+				
 				animation.addByPrefix('purplehold', 'purple hold piece');
 				animation.addByPrefix('greenhold', 'green hold piece');
 				animation.addByPrefix('redhold', 'red hold piece');
 				animation.addByPrefix('bluehold', 'blue hold piece');
+				
+				if(!FlxG.save.data.downscroll)
+				{
+					animation.addByPrefix('purpleholdend', 'pruple end hold');
+					animation.addByPrefix('greenholdend', 'green hold end');
+					animation.addByPrefix('redholdend', 'red hold end');
+					animation.addByPrefix('blueholdend', 'blue hold end');
+				}else {
+					animation.addByPrefix('purpleholdend', 'pruple end hold', 0, false, false, true);
+					animation.addByPrefix('greenholdend', 'green hold end', 0, false, false, true);
+					animation.addByPrefix('redholdend', 'red hold end', 0, false, false, true);
+					animation.addByPrefix('blueholdend', 'blue hold end', 0, false, false, true);
+				}
 
 				setGraphicSize(Std.int(width * 0.7));
 				updateHitbox();
@@ -122,10 +138,6 @@ class Note extends FlxSprite
 				animation.play('redScroll');
 		}
 
-		// trace(prevNote);
-
-		if (FlxG.save.data.downscroll && isSustainNote)
-			flipY = true;
 		
 		if (isSustainNote && prevNote != null)
 		{
