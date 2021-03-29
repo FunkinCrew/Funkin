193f75a2a4ff73735cdf522ddd1a45dc8e8d3049
Saalvage <29021710+Saalvage@users.noreply.github.com>
2021-03-30 01:48:06 +0200

    Removed dad from tutorial

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -37,7 +37,7 @@ class StoryMenuState extends MusicBeatState
 	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
 
 	var weekCharacters:Array<Dynamic> = [
-		['dad', 'bf', 'gf'],
+		['gf', 'bf', ''],
 		['dad', 'bf', 'gf'],
 		['spooky', 'bf', 'gf'],
 		['pico', 'bf', 'gf'],
