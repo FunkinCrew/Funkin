9f1b18d8dc82342feda52270a79e4963de5aa9c4
MtH <mth@mth.moe>
2021-03-30 05:45:04 +0200

    test song and added to freeplay (JUST FOR TESTING)

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -83,6 +83,8 @@ class FreeplayState extends MusicBeatState
 		if (StoryMenuState.weekUnlocked[7] || isDebug)
 			addWeek(['Ugh', 'Guns', 'Stress'], 7, ['tankman']);
 
+		addWeek(['Test'], 6, ['bf-pixel']);
+
 		// LOAD MUSIC
 
 		// LOAD CHARACTERS
