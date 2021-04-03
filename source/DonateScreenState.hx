f2cea286334832f0733ba03606ca52e55700edb9
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-04-03 14:10:00 +0300

    spaces -> tabs

--- source/DonateScreenState.hx
+++ source/DonateScreenState.hx
@@ -7,21 +7,21 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 
 class DonateScreenState extends MusicBeatState {
-    var blurb:Array<String> = [
-        "your donations help us",
-        "develop the funkiest game",
-        "this side of the internet",
-        "",
-        "support the funky cause",
-        "give a lil bit back"
-    ];
+	var blurb:Array<String> = [
+		"your donations help us",
+		"develop the funkiest game",
+		"this side of the internet",
+		"",
+		"support the funky cause",
+		"give a lil bit back"
+	];
 
-    var textGroup:FlxTypedGroup<Alphabet>;
-    var menuItem:FlxSprite;
+	var textGroup:FlxTypedGroup<Alphabet>;
+	var menuItem:FlxSprite;
 
-    override function create()
-    {
-        transIn = FlxTransitionableState.defaultTransIn;
+	override function create()
+	{
+		transIn = FlxTransitionableState.defaultTransIn;
 		transOut = FlxTransitionableState.defaultTransOut;
 
 		// TODO play "Give a Lil' Bit Back" here
@@ -36,48 +36,48 @@ class DonateScreenState extends MusicBeatState {
 		bg.antialiasing = true;
 		add(bg);
 
-        var tex = Paths.getSparrowAtlas('FNF_main_menu_assets');
+		var tex = Paths.getSparrowAtlas('FNF_main_menu_assets');
 
-        menuItem = new FlxSprite(0, 520);
-        menuItem.frames = tex;
-        menuItem.animation.addByPrefix('selected', "donate white", 24);
-        menuItem.animation.play('selected');
-        menuItem.updateHitbox();
-        menuItem.screenCenter(X);
-        add(menuItem);
-        menuItem.antialiasing = true;
+		menuItem = new FlxSprite(0, 520);
+		menuItem.frames = tex;
+		menuItem.animation.addByPrefix('selected', "donate white", 24);
+		menuItem.animation.play('selected');
+		menuItem.updateHitbox();
+		menuItem.screenCenter(X);
+		add(menuItem);
+		menuItem.antialiasing = true;
 
-        textGroup = new FlxTypedGroup<Alphabet>();
-        add(textGroup);
-        for (i in 0...blurb.length)
-        {
-            var money:Alphabet = new Alphabet(0, 0, blurb[i], true, false);
-            money.screenCenter(X);
-            money.y += (i * 60) + 120;
-            textGroup.add(money);
-        }
+		textGroup = new FlxTypedGroup<Alphabet>();
+		add(textGroup);
+		for (i in 0...blurb.length)
+		{
+			var money:Alphabet = new Alphabet(0, 0, blurb[i], true, false);
+			money.screenCenter(X);
+			money.y += (i * 60) + 120;
+			textGroup.add(money);
+		}
 
-        super.create();
-    }
+		super.create();
+	}
 
-    override function update(elapsed:Float)
-    {
-        if (controls.BACK)
-        {
-            FlxG.switchState(new MainMenuState());
-        }
+	override function update(elapsed:Float)
+	{
+		if (controls.BACK)
+		{
+			FlxG.switchState(new MainMenuState());
+		}
 
-        if (controls.ACCEPT)
-        {
-            #if linux
-            Sys.command('/usr/bin/xdg-open', ["https://ninja-muffin24.itch.io/funkin", "&"]);
-            #else
-            FlxG.openURL('https://ninja-muffin24.itch.io/funkin');
-            #end
-        }
+		if (controls.ACCEPT)
+		{
+			#if linux
+			Sys.command('/usr/bin/xdg-open', ["https://ninja-muffin24.itch.io/funkin", "&"]);
+			#else
+			FlxG.openURL('https://ninja-muffin24.itch.io/funkin');
+			#end
+		}
 
-        super.update(elapsed);
+		super.update(elapsed);
 
-        menuItem.screenCenter(X);
-    }
+		menuItem.screenCenter(X);
+	}
 }
