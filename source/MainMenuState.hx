069ae4fb52868f3ff02ff7f7d4b7d5cc6dd83ca0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2021-04-04 10:17:46 -0700

    camera bullshit for menus a lil cooler

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -237,6 +237,8 @@ class MainMenuState extends MusicBeatState
 
 	override function update(elapsed:Float)
 	{
+		FlxG.camera.followLerp = CoolUtil.camLerpShit(0.06);
+
 		if (FlxG.sound.music.volume < 0.8)
 		{
 			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
