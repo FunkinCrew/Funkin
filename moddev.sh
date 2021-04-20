39a72feb6b237a8735a4daacd990707f37fb7a07
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2021-04-20 13:59:41 -0400

    Update moddev.sh

--- moddev.sh
+++ moddev.sh
@@ -4,17 +4,17 @@ sudo apt update && sudo apt upgrade -y
 #Haxe
 sudo add-apt-repository ppa:haxe/releases -y && sudo apt install haxe -y && mkdir ~/haxelib && haxelib setup ~/haxelib
 
-#MoreHaxe (Flixel)
-haxelib setup && haxelib install lime && haxelib install flixel && haxelib install openfl && haxelib run lime setup -y
+#Haxeflixel
+haxelib install lime && haxelib install flixel && haxelib install openfl && haxelib run lime setup
 
 #Setup
-haxelib install flixel && haxelib install flixel-addons && haxelib install flixel-ui && haxelib install hscript && haxelib install newgrounds
+haxelib install flixel-addons && haxelib install flixel-ui && haxelib install hscript && haxelib install newgrounds
 
 #PolyMod & Discord RPC
 haxelib git polymod https://github.com/larsiusprime/polymod.git && haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 
-#apistuff (new folder in /source and call it APIStuff.hx) Remove #'s.
-
+#apistuff
+#/source and call it APIStuff.hx
 #package;
 
 #class APIStuff
