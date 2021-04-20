c3df6f9594de895e8237e193f3aba885d69361b7
SnowyOwl <49002319+Th3SnowyOwl@users.noreply.github.com>
2021-04-20 18:04:50 -0400

    Update moddev.sh

--- moddev.sh
+++ moddev.sh
@@ -1,6 +1,7 @@
 #One liner:
 sudo add-apt-repository ppa:haxe/releases -y && sudo apt install haxe -y && mkdir ~/haxelib && haxelib setup ~/haxelib && haxelib install lime && haxelib install flixel && haxelib install openfl && haxelib run lime setup && haxelib install flixel-addons && haxelib install flixel-ui && haxelib install hscript && haxelib install newgrounds && haxelib git polymod https://github.com/larsiusprime/polymod.git && haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc && haxelib run lime setup flixel
-#Update & Uprade
+
+#Update & Upgrade
 sudo apt update && sudo apt upgrade -y
 
 #Haxe
