fea40412ec8c046b5538ba229b9223a1a13b8ac4
MtH <mth@mth.moe>
2021-04-22 17:19:55 +0200

    swf shit

--- source/CutsceneAnimTestState.hx
+++ source/CutsceneAnimTestState.hx
@@ -1,6 +1,5 @@
 package;
 
-import animate.FlxAnimate;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -9,16 +8,19 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.math.FlxPoint;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import openfl.Assets;
 import openfl.display.BitmapData;
+import openfl.display.MovieClip;
 
-// import animateAtlasPlayer.assets.AssetManager;
-// import animateAtlasPlayer.core.Animation;
 class CutsceneAnimTestState extends FlxState
 {
 	var cutsceneGroup:CutsceneCharacter;
 
 	var curSelected:Int = 0;
 	var debugTxt:FlxText;
+
+	var funnySprite:FlxSprite = new FlxSprite();
+	var clip:MovieClip;
 
 	public function new()
 	{
@@ -32,50 +34,22 @@ class CutsceneAnimTestState extends FlxState
 		debugTxt.color = FlxColor.BLUE;
 		add(debugTxt);
 
-		var animated:FlxAnimate = new FlxAnimate(600, 200);
-		add(animated);
+		clip = Assets.getMovieClip("tanky:");
+		clip.x = FlxG.width/2;
+		clip.y = FlxG.height/2;
+		FlxG.stage.addChild(clip);
 
-		// createCutscene(0);
-		// createCutscene(1);
-		// createCutscene(2);
-		// createCutscene(3);
-		// createCutscene(4);
+		funnySprite.x = FlxG.width/2;
+		add(funnySprite);
+
 	}
 
 	override function update(elapsed:Float)
 	{
-		/* if (FlxG.keys.pressed.SHIFT)
-			{
-				if (FlxG.keys.justPressed.UP)
-					curSelected -= 1;
-				if (FlxG.keys.justPressed.DOWN)
-					curSelected += 1;
-
-				if (curSelected < 0)
-					curSelected = cutsceneGroup.members.length - 1;
-				if (curSelected >= cutsceneGroup.members.length)
-					curSelected = 0;
-			}
-			else
-			{
-				var valueMulti:Float = 1;
-
-				if (FlxG.keys.pressed.SPACE)
-					valueMulti = 10;
-
-				if (FlxG.keys.justPressed.UP)
-					cutsceneGroup.members[curSelected].y -= valueMulti;
-				if (FlxG.keys.justPressed.DOWN)
-					cutsceneGroup.members[curSelected].y += valueMulti;
-				if (FlxG.keys.justPressed.LEFT)
-					cutsceneGroup.members[curSelected].x -= valueMulti;
-				if (FlxG.keys.justPressed.RIGHT)
-					cutsceneGroup.members[curSelected].x += valueMulti;
-			}
-
-			debugTxt.text = curSelected + " : " + cutsceneGroup.members[curSelected].getPosition();
-		 */
-
 		super.update(elapsed);
+
+		var funnyBmp:BitmapData = new BitmapData(FlxG.width, FlxG.height, true, 0x00000000);
+		funnyBmp.draw(clip, clip.transform.matrix, true);
+		funnySprite.loadGraphic(funnyBmp);
 	}
 }
