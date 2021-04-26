3d1765bc7a4193d87dd3e457795c1d5bd1d8ca77
Bulby <Benharless820@gmail.com>
2021-04-26 12:58:44 -0400

    oops

--- source/Note.hx
+++ source/Note.hx
@@ -4,7 +4,6 @@ import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
-import polymod.format.ParseRules.TargetSignatureElement;
 import lime.system.System;
 import flash.display.BitmapData;
 #if sys
