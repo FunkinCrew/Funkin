ef1dd3a0bac649e0a93a4bcc6cf3ada6bb256fa3
TSG <49595354+AyeTSG@users.noreply.github.com>
2021-04-29 17:39:28 -0400

    discord rpc option

--- source/Options.hx
+++ source/Options.hx
@@ -17,6 +17,7 @@ class STOptions
 	public static var st_customIntro:Bool = true;					// Small Things: Custom intro sequence
 	public static var st_disableFnfVersionCheck:Bool = true;		// Small Things: Disable FNF version check
 	public static var st_debug:Bool = false;						// Small Things: Small Things debug
+	public static var st_discordRpc:Bool = true;					// Small Things: Discord RPC
 	public static var st_extraDialogue:Bool = true;					// Small Things: Extra dialogue
 	public static var st_extraSongs:Bool = true;					// Small Things: Extra songs
 	public static var st_fixMonsterIconFreeplay:Bool = true;		// Small Things: Fix Monster's icon for Monster on Freeplay
@@ -45,6 +46,9 @@ class STOptions
 		{
 			if (i.name == "debug")
 				STOptions.st_debug = i.value;
+
+			if (i.name == "discordRPC")
+				STOptions.st_discordRpc = i.value;
 
 			if (i.name == "extraDialogue")
 				STOptions.st_extraDialogue = i.value;
