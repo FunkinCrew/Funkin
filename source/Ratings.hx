e4703fadc671df8d631ca52ac93fd47dbd94a091
Bulby <Benharless820@gmail.com>
2021-05-14 21:54:01 -0400

    Update Ratings.hx

--- source/Ratings.hx
+++ source/Ratings.hx
@@ -104,7 +104,7 @@ class Ratings
 
 		// trace('Hit Info\nDifference: ' + noteDiff + '\nZone: ' + Conductor.safeZoneOffset * 1.5 + "\nTS: " + customTimeScale + "\nLate: " + 155 * customTimeScale);
 
-		if (FlxG.save.data.botplay)
+		if (ModifierState.modifiers[21].value)
 			return "good"; // FUNNY
 
 		if (noteDiff > 166 * customTimeScale) // so god damn early its a miss
