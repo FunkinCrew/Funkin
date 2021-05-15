f7c545e152bcda9a6ce283c138c4af762e4ccad8
MasterDirector99 <74268152+MasterDirector99@users.noreply.github.com>
2021-05-15 08:48:48 +0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -111,7 +111,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollow, null, 0.06);
 
-		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, "v"+ Application.current.meta.get("version"), 12);
+		var versionShit:FlxText = new FlxText(5, FlxG.height - 18, 0, ""+ Application.current.meta.get("version"), 12);
 		var usingSave:FlxText = new FlxText(5, FlxG.height - 36, 0, FlxG.save.name, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
