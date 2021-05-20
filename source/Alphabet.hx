1ddd27261f5332cadff34cae844606e02fc3b216
Bulby <Benharless820@gmail.com>
2021-05-20 17:08:26 -0400

    do some menu changes :heart:

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -20,7 +20,7 @@ class Alphabet extends FlxSpriteGroup
 	// for menu shit
 	public var targetY:Float = 0;
 	public var isMenuItem:Bool = false;
-
+	public var itemType:String = "Classic";
 	public var text:String = "";
 
 	var _finalText:String = "";
@@ -30,6 +30,7 @@ class Alphabet extends FlxSpriteGroup
 
 	var yMulti:Float = 1;
 	var isStepped:Bool = true;
+	var isWheel:Bool = false;
 	var groupX:Float = 90;
 	var groupY:Float = 0.48;
 	// custom shit
@@ -43,7 +44,7 @@ class Alphabet extends FlxSpriteGroup
 
 	var isBold:Bool = false;
 
-	public function new(x:Float, y:Float, text:String = "", ?bold:Bool = false, typed:Bool = false, stepped:Bool = true, alignX:Float = 90, alignY:Float = 0.48, ?drawHypens:Bool = false)
+	public function new(x:Float, y:Float, text:String = "", ?bold:Bool = false, ?typed:Bool = false, ?stepped:Bool = true, ?alignX:Float = 90, ?alignY:Float = 0.48, ?drawHypens:Bool = false, ?wheel:Bool = false)
 	{
 		super(x, y);
 
@@ -51,6 +52,13 @@ class Alphabet extends FlxSpriteGroup
 		this.text = text;
 		isBold = bold;
 		isStepped = stepped;
+		this.isWheel = wheel;
+		if (!isStepped) {
+			itemType = "Vertical";
+		} 
+		if (isWheel) {
+			itemType = "D-Shape";
+		}
 		groupX = alignX;
 		groupY = alignY;
 		this.drawHypens = drawHypens;
@@ -264,14 +272,44 @@ class Alphabet extends FlxSpriteGroup
 		{
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
-			y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * groupY), 0.16);
-			if (isStepped) {
-				x = FlxMath.lerp(x, (targetY * 20) + groupX, 0.16);
-			} else {
-				// bad no
-				//	x = FlxMath.lerp(x, groupX, 0.16);
-			}
+			switch (itemType) {
+				case "Classic":
+					x = FlxMath.lerp(x, (targetY * 20) + groupX, 0.16);
+					y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * groupY), 0.16);
+				case "Vertical":
+					y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * 0.5), 0.16 / 2);
+					x = FlxMath.lerp(x, (targetY * 0) + 308, 0.16 / 2);
+					x += groupX / 2;
+				case "C-Shape":
+					// not actually a wheel, just trying to imitate mic'd up
+					// use exponent because circles????
+					// using equation of a sideways parabola.
+					// x = a(y-k)^2 + h
+					// k is probably inaccurate because, well, the coordinate system
+					// is flipped veritcally.
+					// We still use lerp as that just makes it move smoothly.
+					// I'm going to add instead and see how that works.
 
+					// :grief: i give up time to steal code
+					y = FlxMath.lerp(y, (scaledY * 65) + (FlxG.height * 0.39), 0.16 / 2);
+
+					x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * 70 + (FlxG.width * 0.1), 0.16 / 2);
+					if (scaledY < 0)
+						x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * 70 + (FlxG.width * 0.1), 0.16 / 2);
+
+					if (x > FlxG.width + 30)
+						x = FlxG.width + 30;
+
+				case "D-Shape":
+					y = FlxMath.lerp(y, (scaledY * 90) + (FlxG.height * 0.45), 0.16 / 2);
+
+					x = FlxMath.lerp(x, Math.exp(scaledY * 0.8) * -70 + (FlxG.width * 0.35), 0.16 / 2);
+					if (scaledY < 0)
+						x = FlxMath.lerp(x, Math.exp(scaledY * -0.8) * -70 + (FlxG.width * 0.35), 0.16 / 2);
+
+					if (x < -900)
+						x = -900;
+			}
 		}
 
 		super.update(elapsed);
