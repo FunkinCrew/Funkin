2d4637cca11ca0f688d43a3d45430c40f14874f1
Lucky56 <55949451+Lucky-56@users.noreply.github.com>
2021-06-08 16:21:55 +0200

    forgot to call the song name

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -203,7 +203,7 @@ class FreeplayState extends MusicBeatState
 		if (accepted)
 		{
 			// pre lowercasing the song name
-			var songLowercase = StringTools.replace(songs[curSelected], " ", "-").toLowerCase();
+			var songLowercase = StringTools.replace(songs[curSelected].songName, " ", "-").toLowerCase();
 			switch (songLowercase) {
 				case 'dad-battle': songLowercase = 'dadbattle';
 				case 'philly-nice': songLowercase = 'philly';
