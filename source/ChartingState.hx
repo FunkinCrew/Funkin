4e314187624d7d0f6f65ba5074edf754b98eeb26
Leather128 <gwr.yt2@gmail.com>
2021-06-09 14:09:14 -0400

    fix again lol

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -228,14 +228,12 @@ class ChartingState extends MusicBeatState
 		var player1DropDown = new FlxUIDropDownMenu(10, 100, FlxUIDropDownMenu.makeStrIdLabelArray(characters, true), function(character:String)
 		{
 			_song.player1 = characters[Std.parseInt(character)];
-			leftIcon.animation.play(_song.player1);
 		});
 		player1DropDown.selectedLabel = _song.player1;
 
 		var player2DropDown = new FlxUIDropDownMenu(140, 100, FlxUIDropDownMenu.makeStrIdLabelArray(characters, true), function(character:String)
 		{
 			_song.player2 = characters[Std.parseInt(character)];
-			rightIcon.animation.play(_song.player2);
 		});
 
 		player2DropDown.selectedLabel = _song.player2;
