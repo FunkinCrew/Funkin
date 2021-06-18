9dfa8fcd7b983f3c6d766d809600f9768dd66b9d
Leather128 <gwr.yt2@gmail.com>
2021-06-18 11:38:20 -0400

    fixed loading songs not loaded into game yet lol

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -570,7 +570,7 @@ class ChartingState extends MusicBeatState
 			PlayState.SONG = _song;
 			FlxG.sound.music.stop();
 			vocals.stop();
-			FlxG.switchState(new PlayState());
+			LoadingState.loadAndSwitchState(new PlayState());
 		}
 
 		if (FlxG.keys.justPressed.E)
