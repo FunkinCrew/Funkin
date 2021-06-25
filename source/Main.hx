bc1d1802e7761dd04fc9468a3cbe00b47053852c
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-25 22:55:19 +0700

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -67,10 +67,16 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
+		#if android
+		zoom = 1;
+		gameWidth = 1280;
+		gameHeight = 720;
+		#end//ldog is mad >:)
+
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		//#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
-		#end
+		//#end
 	}
 }
