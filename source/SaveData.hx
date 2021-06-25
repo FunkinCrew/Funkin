69be975912bf22fdc42577e494376ce0a4e94c1f
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-26 02:05:55 +0700

    update SaveData.hx

--- source/SaveData.hx
+++ source/SaveData.hx
@@ -1,8 +1,17 @@
-package:
-
 import flixel.util.FlxSave;
+import Config;
 
 class SaveData
 {
-	
+	var camlerp:FlxSave;
+	camlerp = new FlxSave();
+	camlerp.bind("camMovement");
+
+	public static function initSave()
+	{
+		if (Config.fps == 60)
+			camlerp.data.camMove = 0.06;
+		else if (Config.fps == 90)
+		
+	}
 }
