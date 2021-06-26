0ad9c3172481cad028ddf2d8bfa86a90cb95f0eb
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-26 21:51:18 +0700

    update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -17,7 +17,7 @@ class PauseSubState extends MusicBeatSubstate
 {
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
-	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Exit to menu'];
+	var menuItems:Array<String> = ['Resume', 'Restart Song', 'Difficulty' 'Exit to menu'];
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
@@ -77,7 +77,7 @@ class PauseSubState extends MusicBeatSubstate
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
 		#if mobileC
-		addVirtualPad(UP_DOWN, A_B);
+		addVirtualPad(FULL, A_B);
 		_virtualpad.cameras = cameras;
 		#end
 	}
@@ -109,6 +109,8 @@ class PauseSubState extends MusicBeatSubstate
 			switch (daSelected)
 			{
 				case "Resume":
+					close();
+				case "Difficulty":
 					close();
 				case "Restart Song":
 					FlxG.resetState();
