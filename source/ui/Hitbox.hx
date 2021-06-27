73c4fbd695eb479a6944dc7a7aa42bf42ad8dd0c
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-28 00:59:37 +0700

    update Hitbox.hx

--- source/ui/Hitbox.hx
+++ source/ui/Hitbox.hx
@@ -19,80 +19,80 @@ import flixel.tweens.FlxEase;
 // copyed from flxvirtualpad
 class Hitbox extends FlxSpriteGroup
 {
-    public var hitbox:FlxSpriteGroup;
+	public var hitbox:FlxSpriteGroup;
 
-    var sizex:Int = 320;
+	var sizex:Int = 320;
 
-    var screensizey:Int = 720;
+	var screensizey:Int = 720;
 
-    public var buttonLeft:FlxButton;
-    public var buttonDown:FlxButton;
-    public var buttonUp:FlxButton;
-    public var buttonRight:FlxButton;
-    
-    public function new(?widghtScreen:Int)
-    {
-        super();
+	public var buttonLeft:FlxButton;
+	public var buttonDown:FlxButton;
+	public var buttonUp:FlxButton;
+	public var buttonRight:FlxButton;
+	
+	public function new(?widghtScreen:Int)
+	{
+		super();
 
-        /*if (widghtScreen == null)
-            widghtScreen = FlxG.width;*/
+		/*if (widghtScreen == null)
+			widghtScreen = FlxG.width;*/
 
-        sizex = widghtScreen != null ? Std.int(widghtScreen / 4) : 320;
+		sizex = widghtScreen != null ? Std.int(widghtScreen / 4) : 320;
 
-        
-        //add graphic
-        hitbox = new FlxSpriteGroup();
-        hitbox.scrollFactor.set();
+		
+		//add graphic
+		hitbox = new FlxSpriteGroup();
+		hitbox.scrollFactor.set();
 
-        var hitbox_hint:FlxSprite = new FlxSprite(0, 0).loadGraphic('assets/shared/images/hitbox/hitbox_hint.png');
+		var hitbox_hint:FlxSprite = new FlxSprite(0, 0).loadGraphic('assets/shared/images/hitbox/hitbox_hint.png');
 
-        hitbox_hint.alpha = 0.2;
+		hitbox_hint.alpha = 0.2;
 
-        add(hitbox_hint);
+		add(hitbox_hint);
 
 
-        hitbox.add(add(buttonLeft = createhitbox(0, "left")));
+		hitbox.add(add(buttonLeft = createhitbox(0, "left")));
 
-        hitbox.add(add(buttonDown = createhitbox(sizex, "down")));
+		hitbox.add(add(buttonDown = createhitbox(sizex, "down")));
 
-        hitbox.add(add(buttonUp = createhitbox(sizex * 2, "up")));
+		hitbox.add(add(buttonUp = createhitbox(sizex * 2, "up")));
 
-        hitbox.add(add(buttonRight = createhitbox(sizex * 3, "right")));
-    }
+		hitbox.add(add(buttonRight = createhitbox(sizex * 3, "right")));
+	}
 
-    public function createhitbox(X:Float, framestring:String) {
-        var button = new FlxButton(X, 0);
-        var frames = FlxAtlasFrames.fromSparrow('assets/shared/images/hitbox/hitbox.png', 'assets/shared/images/hitbox/hitbox.xml');
-        
-        var graphic:FlxGraphic = FlxGraphic.fromFrame(frames.getByName(framestring));
+	public function createhitbox(X:Float, framestring:String) {
+		var button = new FlxButton(X, 0);
+		var frames = FlxAtlasFrames.fromSparrow('assets/shared/images/hitbox/hitbox.png', 'assets/shared/images/hitbox/hitbox.xml');
+		
+		var graphic:FlxGraphic = FlxGraphic.fromFrame(frames.getByName(framestring));
 
-        button.loadGraphic(graphic);
+		button.loadGraphic(graphic);
 
-        button.alpha = 0;
+		button.alpha = 0;
 
-    
-        button.onDown.callback = function (){
-            FlxTween.num(0, 0.75, .075, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
-        };
+	
+		button.onDown.callback = function (){
+			FlxTween.num(0, 0.75, .075, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
+		};
 
-        button.onUp.callback = function (){
-            FlxTween.num(0.75, 0, .1, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
-        }
-        
-        button.onOut.callback = function (){
-            FlxTween.num(button.alpha, 0, .2, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
-        }
+		button.onUp.callback = function (){
+			FlxTween.num(0.75, 0, .1, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
+		}
+		
+		button.onOut.callback = function (){
+			FlxTween.num(button.alpha, 0, .2, {ease: FlxEase.circInOut}, function (a:Float) { button.alpha = a; });
+		}
 
-        return button;
-    }
+		return button;
+	}
 
-    override public function destroy():Void
-        {
-            super.destroy();
-    
-            buttonLeft = null;
-            buttonDown = null;
-            buttonUp = null;
-            buttonRight = null;
-        }
+	override public function destroy():Void
+		{
+			super.destroy();
+	
+			buttonLeft = null;
+			buttonDown = null;
+			buttonUp = null;
+			buttonRight = null;
+		}
 }
