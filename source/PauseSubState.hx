1b321315926e9f318386d8123b2ffda7a5c8a716
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-06-27 11:06:35 +0700

    update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -39,14 +39,14 @@ class PauseSubState extends MusicBeatSubstate
 		add(bg);
 
 		var levelInfo:FlxText = new FlxText(20, 15, 0, "", 32);
-		levelInfo.text += PlayState.SONG.song;
+		levelInfo.text = "SONG: " + PlayState.SONG.song + "\nBlueballed (deaths): " + blueballs + "\n";
 		levelInfo.scrollFactor.set();
 		levelInfo.setFormat(Paths.font("vcr.ttf"), 32);
 		levelInfo.updateHitbox();
 		add(levelInfo);
 
 		var levelDifficulty:FlxText = new FlxText(20, 15 + 32, 0, "", 32);
-		levelDifficulty.text += CoolUtil.difficultyString();
+		levelDifficulty.text = "\n\n" + CoolUtil.difficultyString() + "\n";
 		levelDifficulty.scrollFactor.set();
 		levelDifficulty.setFormat(Paths.font('vcr.ttf'), 32);
 		levelDifficulty.updateHitbox();
