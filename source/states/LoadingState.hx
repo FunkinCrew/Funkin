a411684df703a825c64d7597071943bf6fe777fa
Leather128 <gwr.yt2@gmail.com>
2021-06-29 12:42:33 -0400

    fixed loading also week0 instead of tutorial now???? (yea this is big brain time)

--- source/states/LoadingState.hx
+++ source/states/LoadingState.hx
@@ -37,7 +37,7 @@ class LoadingState extends MusicBeatState
 	override function create()
 	{
 		logo = new FlxSprite(-150, -100);
-		logo.frames = Paths.getSparrowAtlas('logoBumpin');
+		logo.frames = Paths.getSparrowAtlas('title/logoBumpin');
 		logo.antialiasing = true;
 		logo.animation.addByPrefix('bump', 'logo bumpin', 24);
 		logo.animation.play('bump');
@@ -46,7 +46,7 @@ class LoadingState extends MusicBeatState
 		// logoBl.color = FlxColor.BLACK;
 
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+		gfDance.frames = Paths.getSparrowAtlas('title/gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
 		gfDance.antialiasing = true;
@@ -67,10 +67,7 @@ class LoadingState extends MusicBeatState
 				checkLibrary("shared");
 				checkLibrary("stages");
 
-				if (PlayState.storyWeek > 0)
-					checkLibrary("week" + PlayState.storyWeek);
-				else
-					checkLibrary("tutorial");
+				checkLibrary("week" + PlayState.storyWeek);
 				
 				var fadeTime = 0.5;
 				FlxG.camera.fade(FlxG.camera.bgColor, fadeTime, true);
