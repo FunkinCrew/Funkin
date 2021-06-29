c36bb6be20059474b7a20d5a87ef2f40204b85e2
Leather128 <gwr.yt2@gmail.com>
2021-06-29 10:51:18 -0400

    no more stupid ass dashes in song names! yay!

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -28,11 +28,11 @@ class StoryMenuState extends MusicBeatState
 
 	var weekData:Array<Dynamic> = [
 		['Tutorial'],
-		['Bopeebo', 'Fresh', 'Dadbattle'],
+		['Bopeebo', 'Fresh', 'Dad Battle'],
 		['Spookeez', 'South', "Monster"],
 		['Pico', 'Philly Nice', "Blammed"],
-		['Satin-Panties', "High", "Milf"],
-		['Cocoa', 'Eggnog', 'Winter-Horrorland'],
+		['Satin Panties', "High", "Milf"],
+		['Cocoa', 'Eggnog', 'Winter Horrorland'],
 		['Senpai', 'Roses', 'Thorns']
 	];
 	var curDifficulty:Int = 1;
