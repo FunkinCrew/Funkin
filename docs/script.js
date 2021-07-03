18559fbeff3483649f95e6424aaa459469bbbb1f
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-07-03 01:09:04 -0400

    Added some javascript to find ids

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+// Get all ids and stuff
+const wrapper = document.getElementById("wrapper");
+const coolervidwrapper = document.getElementById("coolervidwrapper");
+const videoWrapper = document.getElementById("videoWrapper");
+const twitter = document.getElementById("twitter");
+const ng = document.getElementById("ng");
+const itch = document.getElementById("itch");
