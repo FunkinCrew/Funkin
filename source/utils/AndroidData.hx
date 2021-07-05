47c71144e4fbd5f164e125cb4bc4cb57429b3b18
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-07-06 06:28:57 +0700

    update AndroidData.hx

--- source/utils/AndroidData.hx
+++ source/utils/AndroidData.hx
@@ -6,65 +6,65 @@ import flixel.util.FlxSave;
 import flixel.math.FlxPoint;
 
 class AndroidData{
-    var android:FlxSave;
+	var android:FlxSave;
 	public function new(){
 		android = new FlxSave();
 		android.bind("android-data");
 	}
 
 	public function startData(){
-	    if (android.data.cutscenes == null){
-	        android.data.cutscenes = true;
-	    }
-	    if (android.data.sploosh == null){
-	        android.data.sploosh = true;
-	    }
-	    if (android.data.glow == null){
-	        android.data.glow = false;
-	    }
-	    android.flush();
+		if (android.data.cutscenes == null){
+			android.data.cutscenes = true;
+		}
+		if (android.data.sploosh == null){
+			android.data.sploosh = true;
+		}
+		if (android.data.glow == null){
+			android.data.glow = false;
+		}
+		android.flush();
 	}
 
 	public function flushData(){
-	    android.flush();
+		android.flush();
 	}
 
 	public function saveGlow(glow:Bool){
-	    android.data.glow = glow;
+		android.data.glow = glow;
 	}
 
 	public function getGlow(){
-	    if (android.data.glow == true){
-    	    return true;
-	    }
-	    else{
-	        return false;
-	    }
+		if (android.data.glow == true){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	public function saveSploosh(sploosh:Bool){
-	    android.data.sploosh = sploosh;
+		android.data.sploosh = sploosh;
 	}
 
 	public function getSploosh(){
-	    if (android.data.sploosh == true){
-    	    return true;
-	    }
-	    else{
-	        return false;
-	    }
+		if (android.data.sploosh == true){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	public function getCutscenes(){
-	    if (android.data.cutscenes == true){
-	        return true;
-	    }
-	    else{
-	        return false;
-	    }
+		if (android.data.cutscenes == true){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	public function saveCutscenes(cut:Bool){
-	    android.data.cutscenes = cut;
+		android.data.cutscenes = cut;
 	}
 }
