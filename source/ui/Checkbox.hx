d4e1a82da87efdd8c5035012a52c39c0338f272f
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-07-06 07:03:30 +0700

    update Checkbox.hx

--- source/ui/Checkbox.hx
+++ source/ui/Checkbox.hx
@@ -4,62 +4,62 @@ import flixel.FlxSprite;
 
 class Checkbox extends FlxSprite
 {
-    public var checked:Bool;
+	public var checked:Bool;
 
-    public var animOffsets:Map<String, Array<Dynamic>>;
+	public var animOffsets:Map<String, Array<Dynamic>>;
 
-    public function new(?x, ?y, isChecked:Bool = false) {
-        super(x, y);
+	public function new(?x, ?y, isChecked:Bool = false) {
+		super(x, y);
 
-        animOffsets = new Map<String, Array<Dynamic>>();
+		animOffsets = new Map<String, Array<Dynamic>>();
 
-        checked = isChecked;
+		checked = isChecked;
 
-        var tex = Paths.getSparrowAtlas('checkboxThingie');
+		var tex = Paths.getSparrowAtlas('checkboxThingie');
 		frames = tex;
 
-        animation.addByPrefix('selected', 'Check Box Selected Static', 24, false);
+		animation.addByPrefix('selected', 'Check Box Selected Static', 24, false);
 		animation.addByPrefix('animation', 'Check Box selecting animation', 24, false);
-        animation.addByPrefix('unselected', 'Check Box unselected', 24, false);
+		animation.addByPrefix('unselected', 'Check Box unselected', 24, false);
 
-        addOffset('unselected');
-        addOffset('animation', 15, 133);
-        addOffset('selected', 15, 133);
-        
+		addOffset('unselected');
+		addOffset('animation', 15, 133);
+		addOffset('selected', 15, 133);
+		
 
-        change(checked);
+		change(checked);
 
-        setGraphicSize(Std.int(width * 0.7));
-        updateHitbox();
-    }
+		setGraphicSize(Std.int(width * 0.7));
+		updateHitbox();
+	}
 
-    public function change(?isChecked:Bool) {
-        if (isChecked != null)
-            checked = isChecked;
-        else
-            checked = !checked;
-        trace(checked);
-        if (checked)
-            playAnim('animation');
-        else
-            playAnim('unselected');
+	public function change(?isChecked:Bool) {
+		if (isChecked != null)
+			checked = isChecked;
+		else
+			checked = !checked;
+		trace(checked);
+		if (checked)
+			playAnim('animation');
+		else
+			playAnim('unselected');
 
-        return checked;
-    }
+		return checked;
+	}
 
-    override public function update(dt:Float) {
-        super.update(dt);
+	override public function update(dt:Float) {
+		super.update(dt);
 
-        switch (this.animation.curAnim.name) {
-            case "animation":
-                this.offset.set(17, 70);
-            case "unselected":
-                this.offset.set();
-        }
-        
-    }
+		switch (this.animation.curAnim.name) {
+			case "animation":
+				this.offset.set(17, 70);
+			case "unselected":
+				this.offset.set();
+		}
+		
+	}
 
-    public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
+	public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
 	{
 		animation.play(AnimName, Force, Reversed, Frame);
 
@@ -69,11 +69,11 @@ class Checkbox extends FlxSprite
 			offset.set(daOffset[0], daOffset[1]);
 		}
 		else{
-            offset.set(0, 0);
-        }
+			offset.set(0, 0);
+		}
 	}
 
-    public function addOffset(name:String, x:Float = 0, y:Float = 0)
+	public function addOffset(name:String, x:Float = 0, y:Float = 0)
 	{
 		animOffsets[name] = [x, y];
 	}
