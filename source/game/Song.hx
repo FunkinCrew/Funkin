34ca04e7c3e1304fa26207ea44d4663e17f8ae55
Leather128 <gwr.yt2@gmail.com>
2021-07-06 16:27:33 -0400

    unneccesary comment removed

--- source/game/Song.hx
+++ source/game/Song.hx
@@ -59,22 +59,6 @@ class Song
 		if(folder.toLowerCase() == "ugh")
 			PlayState.stepsTexts = CoolUtil.coolTextFile(Paths.txt(folder.toLowerCase() + "/" + "steps"));
 
-		// FIX THE CASTING ON WINDOWS/NATIVE
-		// Windows???
-		// trace(songData);
-
-		// trace('LOADED FROM JSON: ' + songData.notes);
-		/* 
-			for (i in 0...songData.notes.length)
-			{
-				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
-				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
-			}
-
-				daNotes = songData.notes;
-				daSong = songData.song;
-				daBpm = songData.bpm; */
-
 		return parseJSONshit(rawJson);
 	}
 
