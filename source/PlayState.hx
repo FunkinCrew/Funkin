c64c076562295ed029d5bf464d84e65c90c8d633
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-07-07 23:00:48 +0700

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1072,7 +1072,9 @@ class PlayState extends MusicBeatState
 
 		healthBar = new FlxBar(healthBarBG.x + 4, healthBarBG.y + 4, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 8), Std.int(healthBarBG.height - 8), this,
 			'health', 0, 2);
+
 		healthBar.scrollFactor.set();
+
 		healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
 		// healthBar
 		add(healthBar);
