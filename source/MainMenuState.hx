9723bb68015faffac6280cc76be25a4b88045549
KadeDeveloper <kadepcgames@gmail.com>
2021-07-21 01:01:13 -0700

    update to 1.6

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -38,7 +38,7 @@ class MainMenuState extends MusicBeatState
 	var newGaming2:FlxText;
 	public static var firstStart:Bool = true;
 
-	public static var nightly:String = "-Pre-Release2";
+	public static var nightly:String = "";
 
 	public static var kadeEngineVer:String = "1.6" + nightly;
 	public static var gameVer:String = "0.2.7.1";
