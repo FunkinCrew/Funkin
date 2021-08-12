22da8523e962181843a20210ce76be53e325c0e7
Spel0 <75626813+Spel0@users.noreply.github.com>
2021-08-12 14:12:47 +0300

    dumb

--- source/Note.hx
+++ source/Note.hx
@@ -143,7 +143,7 @@ class Note extends FlxSprite
 						animation.add(dataColor[i] + 'holdend', [i + 4]); // Tails
 					}
 
-					var widthSize = Std.int(PlayState.curStage.startsWith('school') ? (width * PlayState.daPixelZoom) : (isSustainNote ? (width * PlayState.daPixelZoom - 1.5) : (width * PlayState.daPixelZoom)));
+					var widthSize = Std.int(PlayState.curStage.startsWith('school') ? (width * PlayState.daPixelZoom) : (isSustainNote ? (width * (PlayState.daPixelZoom - 1.5)) : (width * PlayState.daPixelZoom)));
 
 					setGraphicSize(widthSize);
 					updateHitbox();
