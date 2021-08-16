df1b3ce08b5f830cff70f84c087d309ba1d09dc7
Luffy <luffydafloffi@gmail.com>
2021-08-16 18:41:33 +0300

    Swap keys instead of setting the other to null

--- source/KeyBindMenu.hx
+++ source/KeyBindMenu.hx
@@ -324,12 +324,15 @@ class KeyBindMenu extends FlxSubState
         var shouldReturn:Bool = true;
 
         var notAllowed:Array<String> = ["START"];
+        var swapKey:Int = -1;
 
         for(x in 0...gpKeys.length)
             {
                 var oK = gpKeys[x];
-                if(oK == r)
+                if(oK == r) {
+                    swapKey = x;
                     gpKeys[x] = null;
+                }
                 if (notAllowed.contains(oK))
                 {
                     gpKeys[x] = null;
@@ -339,6 +342,9 @@ class KeyBindMenu extends FlxSubState
             }
 
         if(shouldReturn){
+            if (swapKey != -1) {
+                gpKeys[swapKey] = tempKey;
+            }
             gpKeys[curSelected] = r;
             FlxG.sound.play(Paths.sound('scrollMenu'));
         }
@@ -356,6 +362,7 @@ class KeyBindMenu extends FlxSubState
         var shouldReturn:Bool = true;
 
         var notAllowed:Array<String> = [];
+        var swapKey:Int = -1;
 
         for(x in blacklist){notAllowed.push(x);}
 
@@ -364,8 +371,10 @@ class KeyBindMenu extends FlxSubState
         for(x in 0...keys.length)
             {
                 var oK = keys[x];
-                if(oK == r)
+                if(oK == r) {
+                    swapKey = x;
                     keys[x] = null;
+                }
                 if (notAllowed.contains(oK))
                 {
                     keys[x] = null;
@@ -384,6 +393,10 @@ class KeyBindMenu extends FlxSubState
         lastKey = "";
 
         if(shouldReturn){
+            // Swap keys instead of setting the other one as null
+            if (swapKey != -1) {
+                keys[swapKey] = tempKey;
+            }
             keys[curSelected] = r;
             FlxG.sound.play(Paths.sound('scrollMenu'));
         }
