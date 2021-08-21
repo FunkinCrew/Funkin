959f3543c2bb0922fb3244bef22e092a8ffbed82
thepercentageguy <54141759+thepercentageguy@users.noreply.github.com>
2021-08-21 16:34:03 +0200

    healthicon unhardcoding

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -14,26 +14,13 @@ class HealthIcon extends FlxSprite
 		super();
 		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 
+		for (tchar in CoolUtil.coolTextFile(Paths.txt('healthicons'))) {
+			var eugh = tchar.split(':');
+
+			animation.add(eugh[0], [Std.parseInt(eugh[1]), Std.parseInt(eugh[2])], 0, false, isPlayer);
+		}
+
 		antialiasing = true;
-		animation.add('bf', [0, 1], 0, false, isPlayer);
-		animation.add('bf-car', [0, 1], 0, false, isPlayer);
-		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
-		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
-		animation.add('spooky', [2, 3], 0, false, isPlayer);
-		animation.add('pico', [4, 5], 0, false, isPlayer);
-		animation.add('mom', [6, 7], 0, false, isPlayer);
-		animation.add('mom-car', [6, 7], 0, false, isPlayer);
-		animation.add('tankman', [8, 9], 0, false, isPlayer);
-		animation.add('face', [10, 11], 0, false, isPlayer);
-		animation.add('dad', [12, 13], 0, false, isPlayer);
-		animation.add('senpai', [22, 22], 0, false, isPlayer);
-		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
-		animation.add('spirit', [23, 23], 0, false, isPlayer);
-		animation.add('bf-old', [14, 15], 0, false, isPlayer);
-		animation.add('gf', [16], 0, false, isPlayer);
-		animation.add('parents-christmas', [17], 0, false, isPlayer);
-		animation.add('monster', [19, 20], 0, false, isPlayer);
-		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
 		animation.play(char);
 		scrollFactor.set();
 	}
