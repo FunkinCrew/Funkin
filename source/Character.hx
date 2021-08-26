12723832f4353510335f0c59e931ee134b97e485
M&M <goldtextwitch@outlook.com>
2021-08-26 00:27:49 -0700

    fix animations for non-bf characters over player 1

--- source/Character.hx
+++ source/Character.hx
@@ -420,7 +420,7 @@ class Character extends FlxSprite
 
 	override function update(elapsed:Float)
 	{
-		if (!curCharacter.startsWith('bf'))
+		if (!isPlayer)
 		{
 			if (animation.curAnim.name.startsWith('sing'))
 			{
