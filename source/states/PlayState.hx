6237f89ae1cac17abde9c0df31229300f0dada68
Leather128 <gwr.yt2@gmail.com>
2021-09-02 21:31:05 -0400

    fixes

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1911,7 +1911,7 @@ class PlayState extends MusicBeatState
 				health += 0.005;
 				hitNotes += 0.3;
 			case 'shit':
-				health -= 0.04;
+				health -= 0.07;
 		}
 
 		/*
@@ -2280,7 +2280,7 @@ class PlayState extends MusicBeatState
 
 		if(canMiss)
 		{
-			health -= 0.04;
+			health -= 0.07;
 			if (combo > 5 && gf.animOffsets.exists('sad'))
 			{
 				gf.playAnim('sad');
