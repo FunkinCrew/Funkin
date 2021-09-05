a43ecd0f158bae6088925247ef5c9f533d42be81
Spel0 <75626813+Spel0@users.noreply.github.com>
2021-09-05 14:03:57 +0300

    actually just do this

--- source/Note.hx
+++ source/Note.hx
@@ -116,6 +116,8 @@ class Note extends FlxSprite
 
 		this.noteData = noteData;
 
+		var daStage:String = (PlayState.instance != null ? PlayState.Stage.curStage : 'stage');
+
 		// defaults if no noteStyle was found in chart
 		var noteTypeCheck:String = 'normal';
 
