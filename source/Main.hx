d0800fcd3954d370baa6d645e8cac734c2c4be29
MtH <mth@mth.moe>
2021-09-07 23:09:34 +0200

    titlestate spring cleaning, move shit into new initstate this is a huge ass commit so cam pls dont merge until u thoroughly check it and know what changed and moved and whatever. got rid of a lot of ludum dare era commented out shit as well. feel free to ask for clarification or add shit back if it's better suited here

--- source/Main.hx
+++ source/Main.hx
@@ -19,7 +19,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = InitState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	#if web
 	var framerate:Int = 60; // How many frames per second the game should run at.
