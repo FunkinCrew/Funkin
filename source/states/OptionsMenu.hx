426706d85137145c883a5502bd2028aedc115657
Leather128 <gwr.yt2@gmail.com>
2021-09-11 13:21:51 -0400

    yes

--- source/states/OptionsMenu.hx
+++ source/states/OptionsMenu.hx
@@ -66,11 +66,10 @@ class OptionsMenu extends MusicBeatState
 		[
 			"Tools",
 			new PageOption("Back", 0, "Categories"),
-			new Option("Coming Soon", "nofricku", 1)
-			/*new GameStateOption("Charter", 1, new ChartingState()),
+			new GameStateOption("Charter", 1, new ChartingState()),
 			new GameStateOption("Animation Debug", 2, new AnimationDebug("dad")),
-			new GameStateOption("Stage Editor", 3, new StageMakingState("stage")),
-			new GameStateOption("Character Creator", 4, new CharacterCreationState("bf"))*/
+			//new GameStateOption("Stage Editor", 3, new StageMakingState("stage")),
+			new GameStateOption("Character Creator", 3, new CharacterCreationState("bf"))
 		],
 		[
 			"Misc",
