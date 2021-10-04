d2655dfc09bc7a30f94e71fb8271ff92e024fc98
The Gedon <82772622+GEDON6676@users.noreply.github.com>
2021-10-04 22:01:24 +0300

    Create WarmUp.hx

---
+++ WarmUp.hx
@@ -0,0 +1,18 @@
+package;
+
+/*
+ / training for contributors
+ /
+*/
+class WarmUp extends MusicBeatState
+{
+    override public function create()
+    {
+        super.create();
+    }
+
+    public override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
+}
