8dbb410a5b8c445c8dde24c4af735e831e528293
ItzFireable <fireableplayz@gmail.com>
2021-10-13 00:13:33 +0300

    Debug menu improvements

--- source/AnimationDebug.hx
+++ source/AnimationDebug.hx
@@ -11,13 +11,23 @@ import flixel.util.FlxColor;
 import openfl.events.Event;
 import openfl.events.IOErrorEvent;
 import openfl.net.FileReference;
+import flixel.addons.ui.FlxInputText;
+import flixel.addons.ui.FlxUI9SliceSprite;
+import flixel.addons.ui.FlxUI;
+import flixel.addons.ui.FlxUIState;
+import flixel.addons.ui.FlxUICheckBox;
+import flixel.addons.ui.FlxUIDropDownMenu;
+import flixel.addons.ui.FlxUIInputText;
+import flixel.addons.ui.FlxUINumericStepper;
+import flixel.addons.ui.FlxUITabMenu;
+import flixel.addons.ui.FlxUITooltip.FlxUITooltipStyle;
 
 /**
 	*DEBUG MODE
  */
 using StringTools;
 
-class AnimationDebug extends FlxState
+class AnimationDebug extends MusicBeatState
 {
 	var _file:FileReference;
 	var bf:Boyfriend;
@@ -31,6 +41,15 @@ class AnimationDebug extends FlxState
 	var daAnim:String = 'spooky';
 	var camFollow:FlxObject;
 
+	var background:FlxSprite;
+	var curt:FlxSprite;
+	var front:FlxSprite;
+
+	var UI_box:FlxUITabMenu;
+	var UI_options:FlxUITabMenu;
+	var offsetX:FlxUINumericStepper;
+	var offsetY:FlxUINumericStepper;
+
 	public function new(daAnim:String = 'spooky')
 	{
 		super();
@@ -39,11 +58,35 @@ class AnimationDebug extends FlxState
 
 	override function create()
 	{
-		FlxG.sound.music.stop();
+		// FlxG.sound.music.stop();
 
-		var gridBG:FlxSprite = FlxGridOverlay.create(10, 10);
-		gridBG.scrollFactor.set(0.5, 0.5);
-		add(gridBG);
+		// var gridBG:FlxSprite = FlxGridOverlay.create(10, 10);
+		// gridBG.scrollFactor.set(0.5, 0.5);
+		// add(gridBG);
+
+		FlxG.mouse.visible = true;
+
+		background = new FlxSprite(-600, -525).loadGraphic(Paths.loadImage('stageback', 'shared'));
+		front = new FlxSprite(-650, 325).loadGraphic(Paths.loadImage('stagefront', 'shared'));
+		curt = new FlxSprite(-500, -625).loadGraphic(Paths.loadImage('stagecurtains', 'shared'));
+		background.antialiasing = FlxG.save.data.antialiasing;
+		front.antialiasing = FlxG.save.data.antialiasing;
+		curt.antialiasing = FlxG.save.data.antialiasing;
+
+		background.screenCenter(X);
+		background.scale.set(0.7, 0.7);
+		front.screenCenter(X);
+		front.scale.set(0.7, 0.7);
+		curt.screenCenter(X);
+		curt.scale.set(0.7, 0.7);
+
+		background.scrollFactor.set(0.9, 0.9);
+		curt.scrollFactor.set(0.9, 0.9);
+		front.scrollFactor.set(0.9, 0.9);
+
+		add(background);
+		add(front);
+		add(curt);
 
 		if (daAnim == 'bf')
 			isDad = false;
@@ -80,7 +123,31 @@ class AnimationDebug extends FlxState
 
 		genBoyOffsets();
 
-    addHelpText();
+		addHelpText();
+
+		var tabs = [{name: "Offsets", label: 'Offset menu'},];
+
+		UI_box = new FlxUITabMenu(null, tabs, true);
+
+		UI_box.scrollFactor.set();
+		UI_box.resize(150, 200);
+		UI_box.x = FlxG.width - UI_box.width - 20;
+		UI_box.y = 20;
+
+		// var opt_tabs = [{name: "test", label: 'test'}];
+
+		// UI_options = new FlxUITabMenu(null, opt_tabs, true);
+
+		// UI_options.scrollFactor.set();
+		// UI_options.selected_tab = 0;
+		// UI_options.resize(300, 200);
+		// UI_options.x = UI_box.x;
+		// UI_options.y = FlxG.height - 300;
+		// no need for now
+		// add(UI_options);
+		add(UI_box);
+
+		addOffsetUI();
 
 		camFollow = new FlxObject(0, 0, 2, 2);
 		camFollow.screenCenter();
@@ -91,6 +158,35 @@ class AnimationDebug extends FlxState
 		super.create();
 	}
 
+	function addOffsetUI():Void
+	{
+		var offsetX_label = new FlxText(10, 10, 'X Offset');
+
+		var UI_offsetX:FlxUINumericStepper = new FlxUINumericStepper(10, offsetX_label.y + offsetX_label.height + 10, 1,
+			char.animOffsets.get(animList[curAnim])[0], -500.0, 500.0, 0);
+		UI_offsetX.value = char.animOffsets.get(animList[curAnim])[0];
+		UI_offsetX.name = 'offset_x';
+		offsetX = UI_offsetX;
+
+		var offsetY_label = new FlxText(10, UI_offsetX.y + UI_offsetX.height + 10, 'Y Offset');
+
+		var UI_offsetY:FlxUINumericStepper = new FlxUINumericStepper(10, offsetY_label.y + offsetY_label.height + 10, 1,
+			char.animOffsets.get(animList[curAnim])[0], -500.0, 500.0, 0);
+		UI_offsetY.value = char.animOffsets.get(animList[curAnim])[1];
+		UI_offsetY.name = 'offset_y';
+		offsetY = UI_offsetY;
+
+		var tab_group_offsets = new FlxUI(null, UI_box);
+		tab_group_offsets.name = "Offsets";
+
+		tab_group_offsets.add(offsetX_label);
+		tab_group_offsets.add(offsetY_label);
+		tab_group_offsets.add(UI_offsetX);
+		tab_group_offsets.add(UI_offsetY);
+
+		UI_box.addGroup(tab_group_offsets);
+	}
+
 	function genBoyOffsets(pushList:Bool = true):Void
 	{
 		var daLoop:Int = 0;
@@ -99,7 +195,8 @@ class AnimationDebug extends FlxState
 		{
 			var text:FlxText = new FlxText(10, 20 + (18 * daLoop), 0, anim + ": " + offsets, 15);
 			text.scrollFactor.set();
-			text.color = FlxColor.BLUE;
+			text.setBorderStyle(OUTLINE, FlxColor.BLACK, 2);
+			text.color = FlxColor.WHITE;
 			dumbTexts.add(text);
 
 			if (pushList)
@@ -166,6 +263,9 @@ class AnimationDebug extends FlxState
 
 	function updateTexts():Void
 	{
+		offsetX.value = char.animOffsets.get(animList[curAnim])[0];
+		offsetY.value = char.animOffsets.get(animList[curAnim])[1];
+
 		dumbTexts.forEach(function(text:FlxText)
 		{
 			text.kill();
@@ -173,22 +273,63 @@ class AnimationDebug extends FlxState
 		});
 	}
 
-  var helpText:FlxText;
-  function addHelpText():Void {
-    var helpTextValue = "Help:\nQ/E : Zoom in and out\nF : Flip\nI/J/K/L : Pan Camera\nW/S : Cycle Animation\nArrows : Offset Animation\nShift-Arrows : Offset Animation x10\nSpace : Replay Animation\nCTRL-S : Save Offsets to File\nEnter/ESC : Exit\nPress F1 to hide/show this!\n";
-    helpText = new FlxText(940, 20, 0, helpTextValue, 15);
-    helpText.scrollFactor.set();
-    helpText.color = FlxColor.BLUE;
+	var helpText:FlxText;
 
-    add(helpText);
-  }
+	function addHelpText():Void
+	{
+		var helpTextValue = "Help:\nQ/E : Zoom in and out\nF : Flip\nI/J/K/L : Pan Camera\nW/S : Cycle Animation\nArrows : Offset Animation\nShift-Arrows : Offset Animation x10\nSpace : Replay Animation\nCTRL-S : Save Offsets to File\nEnter/ESC : Exit\nPress F1 to hide/show this!\n";
+		helpText = new FlxText(940, 20, 0, helpTextValue, 15);
+		helpText.scrollFactor.set();
+		helpText.y = FlxG.height - helpText.height - 20;
+		helpText.setBorderStyle(OUTLINE, FlxColor.BLACK, 2);
+		helpText.color = FlxColor.WHITE;
+
+		add(helpText);
+	}
+
+	override function getEvent(id:String, sender:Dynamic, data:Dynamic, ?params:Array<Dynamic>)
+	{
+		if (id == FlxUINumericStepper.CHANGE_EVENT && (sender is FlxUINumericStepper))
+		{
+			var offset:FlxUINumericStepper = cast sender;
+			var offsetName = offset.name;
+			switch (offsetName)
+			{
+				case 'offset_x':
+					char.animOffsets.get(animList[curAnim])[0] = offset.value;
+					updateTexts();
+					genBoyOffsets(false);
+					char.playAnim(animList[curAnim]);
+				case 'offset_y':
+					char.animOffsets.get(animList[curAnim])[1] = offset.value;
+					updateTexts();
+					genBoyOffsets(false);
+					char.playAnim(animList[curAnim]);
+			}
+		}
+	}
 
 	override function update(elapsed:Float)
 	{
 		textAnim.text = char.animation.curAnim.name;
 
-		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.ESCAPE)
+		if (FlxG.mouse.overlaps(char) && FlxG.mouse.pressed)
+		{
+			// HOW THE FUCK DO I CONVERT THIS
+			char.animOffsets.get(animList[curAnim])[0] = Math.round(-FlxG.mouse.screenX + (char.width * 1.5));
+			char.animOffsets.get(animList[curAnim])[1] = Math.round(-FlxG.mouse.screenY + (char.height / 1.5));
+
+			updateTexts();
+			genBoyOffsets(false);
+			char.playAnim(animList[curAnim]);
+			// TO MOUSE MOVEMENT?????????
+		}
+
+		if (FlxG.keys.justPressed.ESCAPE)
+		{
+			FlxG.mouse.visible = false;
 			FlxG.switchState(new PlayState());
+		}
 
 		if (FlxG.keys.justPressed.E)
 			FlxG.camera.zoom += 0.25;
@@ -273,10 +414,10 @@ class AnimationDebug extends FlxState
 		if (FlxG.keys.pressed.CONTROL && FlxG.keys.justPressed.S)
 			saveBoyOffsets();
 
-    if (FlxG.keys.justPressed.F1)
+		if (FlxG.keys.justPressed.F1)
 			FlxG.save.data.showHelp = !FlxG.save.data.showHelp;
 
-    helpText.visible = FlxG.save.data.showHelp;
+		helpText.visible = FlxG.save.data.showHelp;
 
 		super.update(elapsed);
 	}
