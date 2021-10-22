514d8da62da02773a3084a5304d6ebb13c6b3b28
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2021-10-22 17:31:58 +0200

    Adding some conditionals to make the player feel special in Outdated State
    Yeah... It's basically just some conditionals to make the player feel loved and stalked that they has someone that know them OS.
    
    And also for ppl who debug in debug and hl like me B)

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -13,16 +13,30 @@ class OutdatedSubState extends MusicBeatState
 
 	override function create()
 	{
+		function text():String
+		{
+			#if !hl
+			var ver = "v" + Application.current.meta.get('version');
+			var version = #if windows "Windows" #elseif mac 'Mac' #elseif linux "Linux" #elseif mobile "Mobile" #elseif web "Web" #end;
+			var debugThing = #if debug "to go to The Github Build" #else 'to download the latest version on Itch' #end;
+			return "HEY! You're running a " 
+			+ version
+			+ " outdated version of the game!\nCurrent version is "
+			+ ver
+			+ " while the most recent version is "
+			+ NGio.GAME_VER
+			+ "! Press Space "
+			+ debugThing
+			+ ', or ESCAPE to ignore this!!';
+			#else
+			return "Uhm... So you're using hl to debug, bitch. Why do you want a version?";
+			#end
+		}
 		super.create();
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 		var ver = "v" + Application.current.meta.get('version');
-		var txt:FlxText = new FlxText(0, 0, FlxG.width,
-			"HEY! You're running an outdated version of the game!\nCurrent version is "
-			+ ver
-			+ " while the most recent version is "
-			+ NGio.GAME_VER
-			+ "! Press Space to go to itch.io, or ESCAPE to ignore this!!",
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,text(),
 			32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		txt.screenCenter();
