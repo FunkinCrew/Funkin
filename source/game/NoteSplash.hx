9659346a8ff0b783cbcd9eb3151b55763c86f4a0
Leather128 <81660872+Leather128@users.noreply.github.com>
2021-12-13 20:18:57 -0500

    new note splashes!

--- source/game/NoteSplash.hx
+++ source/game/NoteSplash.hx
@@ -14,14 +14,17 @@ class NoteSplash extends FlxSprite
 
         this.target = target;
 
-        alpha = 0.7;
+        alpha = 0.8;
         frames = PlayState.instance.splash_Texture;
 
-        animation.addByPrefix("default", "note splash " + NoteVariables.Other_Note_Anim_Stuff[PlayState.SONG.keyCount - 1][noteData] + "0", 28, false);
+        animation.addByPrefix("default", "note splash " + NoteVariables.Other_Note_Anim_Stuff[PlayState.SONG.keyCount - 1][noteData] + "0", FlxG.random.int(22, 26), false);
         animation.play("default", true);
 
         setGraphicSize(Std.int(target.width * 2.5));
+
         updateHitbox();
+        centerOrigin();
+        centerOffsets();
     }
 
     override function update(elapsed:Float)
@@ -32,8 +35,8 @@ class NoteSplash extends FlxSprite
             alpha = 0;
         }
         
-        x = target.x - (target.width / 1.35);
-        y = target.y - (target.height / 1.35);
+        x = target.x - (target.width / 1.5);
+        y = target.y - (target.height / 1.5);
 
         color = target.color;
         
