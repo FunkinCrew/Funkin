33e0f0b0b1065498d07f4c459ec56d98b1b74063
MrCheemsAndFriends <84867412+Cheemsandfriends@users.noreply.github.com>
2021-12-25 18:47:33 +0100

    Remove dropText var

--- source/DialogueBox.hx
+++ source/DialogueBox.hx
@@ -24,8 +24,6 @@ class DialogueBox extends FlxSpriteGroup
 	// SECOND DIALOGUE FOR THE PIXEL SHIT INSTEAD???
 	var swagDialogue:FlxTypeText;
 
-	var dropText:FlxText;
-
 	public var finishThing:Void->Void;
 
 	var portraitLeft:FlxSprite;
@@ -129,14 +127,12 @@ class DialogueBox extends FlxSpriteGroup
 			// box.flipX = true;
 		}
 
-		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
-		dropText.font = 'Pixel Arial 11 Bold';
-		dropText.color = 0xFFD89494;
-		add(dropText);
-
 		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
 		swagDialogue.font = 'Pixel Arial 11 Bold';
 		swagDialogue.color = 0xFF3F2021;
+		swagDialogue.borderStyle = SHADOW;
+		swagDialogue.borderColor = 0xFFD89494;
+		swagDialogue.borderSize = 2;
 		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
 		add(swagDialogue);
 
@@ -157,10 +153,8 @@ class DialogueBox extends FlxSpriteGroup
 		{
 			portraitLeft.color = FlxColor.BLACK;
 			swagDialogue.color = FlxColor.WHITE;
-			dropText.color = FlxColor.BLACK;
+			swagDialogue.borderColor = FlxColor.BLACK;
 		}
-
-		dropText.text = swagDialogue.text;
 
 		if (box.animation.curAnim != null)
 		{
@@ -199,7 +193,6 @@ class DialogueBox extends FlxSpriteGroup
 						portraitLeft.visible = false;
 						portraitRight.visible = false;
 						swagDialogue.alpha -= 1 / 5;
-						dropText.alpha = swagDialogue.alpha;
 					}, 5);
 
 					new FlxTimer().start(1.2, function(tmr:FlxTimer)
