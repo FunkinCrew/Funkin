a5c958755eb4c7ce1132e7658d69f0916ead6458
Sailor / Sabine <35895538+sayofthelor@users.noreply.github.com>
2021-12-30 19:30:30 -0500

    unbind pulses from framerate (#105)
    yeah i assume this is taken from psych or something but tbh i dont really care cuz i guess it's nice to have, anyways

--- source/utilities/CoolUtil.hx
+++ source/utilities/CoolUtil.hx
@@ -15,16 +15,10 @@ class CoolUtil
 		return difficultyArray[PlayState.storyDifficulty];
 	}
 
-	public static function boundTo(value:Float, min:Float, max:Float):Float {
-		var newValue:Float = value;
-
-		if(newValue < min)
-			newValue = min;
-		else if(newValue > max)
-			newValue = max;
-		
-		return newValue;
+	inline public static function boundTo(value:Float, min:Float, max:Float):Float {
+		return Math.max(min, Math.min(max, value));
 	}
+
 
 	public static function coolTextFile(path:String):Array<String>
 	{
