042fe47baa375acce2921fd21036e659d22aaa77
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-01-07 20:07:09 -0500

    updated preloader

--- Preloader.hx
+++ Preloader.hx
@@ -1,19 +1,16 @@
-package ;
+package;
  
 import flixel.system.FlxBasePreloader;
-import openfl.display.Sprite;
 import flash.display.Bitmap;
 import flash.display.BitmapData;
-import flash.display.BlendMode;
 import flash.display.Sprite;
 import flash.Lib;
-import flixel.FlxG;
  
 @:bitmap("art/preloaderArt.png") class LogoImage extends BitmapData { }
  
 class Preloader extends FlxBasePreloader
 {
-    public function new(MinDisplayTime:Float=3, ?AllowedURLs:Array<String>) 
+    public function new(MinDisplayTime:Float=6, ?AllowedURLs:Array<String>) 
     {
         super(MinDisplayTime, AllowedURLs);
     }
@@ -29,7 +26,7 @@ class Preloader extends FlxBasePreloader
          
         logo = new Sprite();
         logo.addChild(new Bitmap(new LogoImage(0,0))); //Sets the graphic of the sprite to a Bitmap object, which uses our embedded BitmapData class.
-        logo.scaleX = logo.scaleY = ratio;
+        logo.scaleX = logo.scaleY = ratio / 2;
         logo.x = ((this._width) / 2) - ((logo.width) / 2);
         logo.y = (this._height / 2) - ((logo.height) / 2);
         addChild(logo); //Adds the graphic to the NMEPreloader's buffer.
@@ -39,18 +36,11 @@ class Preloader extends FlxBasePreloader
      
     override function update(Percent:Float):Void 
     {
-        if(Percent < 69)
-        {
-            logo.scaleX += Percent / 1920;
-            logo.scaleY += Percent / 1920;
-            logo.x -= Percent * 0.6;
-            logo.y -= Percent / 2;
-        }else{
-            logo.scaleX = this._width / 1280;
-            logo.scaleY = this._width / 1280;
-            logo.x = ((this._width) / 2) - ((logo.width) / 2);
-            logo.y = (this._height / 2) - ((logo.height) / 2);
-        }
+        logo.alpha = 0.3 + Percent;
+
+        logo.scaleX = logo.scaleY = 0.5 * (0.3 + Percent);
+        logo.x = ((this._width) / 2) - ((logo.width) / 2);
+        logo.y = (this._height / 2) - ((logo.height) / 2);
         
         super.update(Percent);
     }
