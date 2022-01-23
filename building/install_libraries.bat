b7103e8d3edcb92f92d18b1b0474466ac35f7d1a
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-01-23 15:45:07 -0500

    Update install_libraries.bat

--- building/install_libraries.bat
+++ building/install_libraries.bat
@@ -4,5 +4,5 @@ haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install hscript
 haxelib install polymod 1.3.0
-haxelib install linc_luajit 0.0.4
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git linc_luajit https://github.com/AndreiRudenko/linc_luajit
