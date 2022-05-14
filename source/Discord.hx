399fa3cc11f30312533e2c49bcfe86913733a928
SpunBlue <86245524+SpunBlue@users.noreply.github.com>
2022-05-13 22:56:22 -0400

    discord ewwww /j

--- source/Discord.hx
+++ source/Discord.hx
@@ -11,7 +11,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "952546734662352976",
+			clientID: "",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
