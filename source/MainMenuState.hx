c9c0feacc43a4e083fe4e6d62de8e04a7f73b875
SpunBlue <spinnableblue@gmail.com>
2022-05-19 06:19:44 -0400

    windows fixing + 2.12.1

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -40,7 +40,7 @@ class MainMenuState extends MusicBeatState
 
 	//VERSION
 	public static var nightly:String = "";
-	public static var gameVer:String = "2.12.0";
+	public static var gameVer:String = "2.12.1";
 
 	override function create()
 	{
