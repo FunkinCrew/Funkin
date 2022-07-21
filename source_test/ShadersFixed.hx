f45cdf6173c5f4290849c7604d1abbd353318b05
imliterallydanishlol <102416852+bambitheone82112@users.noreply.github.com>
2022-07-21 19:28:48 +0700

    it didnt work

---
+++ source_test/ShadersFixed.hx
@@ -0,0 +1,14 @@
+package;
+
+import flixel.FlxGame;
+import openfl.Lib;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(0, 0, PlayState));
+	}
+}
