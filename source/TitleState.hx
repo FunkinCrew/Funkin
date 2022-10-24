44c379283aa69aa9033c44465d5e8a8cade6d32d
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-24 10:44:05 -0400

    polymod support should be done

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -54,17 +54,51 @@ class TitleState extends MusicBeatState
 
 	var wackyImage:FlxSprite;
 
-	override public function create():Void
-	{
-		/*
-		#if polymod
-		//polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']}); // no
-		#end
-		*/
+	public static var mod_dirs:Array<String> = [];
 
+	public static function reloadMods()
+	{
+		
+		#if polymod
+mod_dirs = FlxG.save.data.mods;
+
+		var new_dirs:Array<String> = [];
+
+		for(dir in mod_dirs)
+		{
+			new_dirs.push(dir);
+		}
+
+		polymod.Polymod.init({
+			modRoot: "mods",
+			dirs: new_dirs,
+			framework: OPENFL,
+			errorCallback: function(error:polymod.Polymod.PolymodError)
+			{
+				#if debug
+				trace(error.message);
+				#end
+			},
+			frameworkParams: {
+				assetLibraryPaths: [
+					"songs" => "songs",
+					"shared" => "shared",
+					"week1" => "week1",
+					"week2" => "week2",
+					"week3" => "week3",
+					"week4" => "week4",
+					"week5" => "week5",
+					"week6" => "week6"
+				]
+			}
+		});		
+		#end	
+	}
 		//var cool = Json.parse("assets/preload/data/introData.json"); //hardcoded for now, i think
 		//trace(cool);		
 
+	override public function create():Void
+	{
 		PlayerSettings.init();
 
 		curWacky = FlxG.random.getObject(getIntroTextShit());
@@ -82,6 +116,12 @@ class TitleState extends MusicBeatState
 
 		FlxG.save.bind('funkin', 'ninjamuffin99');
 
+
+		if(FlxG.save.data.mods == null)
+			FlxG.save.data.mods = [];
+
+		reloadMods();		
+		
 		Highscore.load();
 
 		colorShader = new ColorSwapEffect();
