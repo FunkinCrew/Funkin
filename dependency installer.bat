f6526ea5f128ab635b0aff384141e5ca8666e1f0
CECHAMO81 <102765863+CECHAMO81@users.noreply.github.com>
2022-10-28 11:01:44 -0500

    dependency installer
    This file is not a great thing but it will be very useful for those who do not want to install the haxe dependencies one by one or write a lot, just for fun add the name of the brands as extra decoration

---
+++ dependency installer.bat
@@ -0,0 +1,82 @@
+@echo off
+@echo.
+echo ...
+echo 8888888888 888b    888 8888888888 
+echo 888        8888b   888 888        
+echo 888        88888b  888 888        
+echo 8888888    888Y88b 888 8888888    
+echo 888        888 Y88b888 888        
+echo 888        888  Y88888 888        
+echo 888        888   Y8888 888        
+echo 888        888    Y888 888        
+@echo.
+title CECHAMO 81 - Installation of dependencies for friday night funkin
+echo Are you sure you want to install/update the friday night funkin dependencies? 
+echo to cancel this action just close this window
+pause
+echo ...
+echo '##::::'##::::'###::::'##::::'##:'########:'##:::::::'####:'########::
+echo  ##:::: ##:::'## ##:::. ##::'##:: ##.....:: ##:::::::. ##:: ##.... ##:
+echo  ##:::: ##::'##:. ##:::. ##'##::: ##::::::: ##:::::::: ##:: ##:::: ##:
+echo  #########:'##:::. ##:::. ###:::: ######::: ##:::::::: ##:: ########::
+echo  ##.... ##: #########::: ## ##::: ##...:::: ##:::::::: ##:: ##.... ##:
+echo  ##:::: ##: ##.... ##:: ##:. ##:: ##::::::: ##:::::::: ##:: ##:::: ##:
+echo  ##:::: ##: ##:::: ##: ##:::. ##: ########: ########:'####: ########::
+echo ..:::::..::..:::::..::..:::::..::........::........::....::........:::
+@echo.
+title CECHAMO 81 - installing 1 of 8 haxelib
+haxelib install flixel
+title CECHAMO 81 - installing 2 of 8 haxelib
+haxelib install flixel-addons
+title CECHAMO 81 - installing 3 of 8 haxelib
+haxelib install flixel-ui
+title CECHAMO 81 - installing 4 of 8 haxelib
+haxelib install hscript
+title CECHAMO 81 - installing 5 of 8 haxelib
+haxelib install newgrounds
+@echo.
+title CECHAMO 81 - Installation of dependencies from git
+echo to install the following dependencies you need to have git installed previously
+pause
+@echo.
+echo ...
+echo    .oooooo.     o8o      .   
+echo   d8P'  `Y8b    `"'    .o8   
+echo  888           oooo  .o888oo 
+echo  888           `888    888   
+echo  888     ooooo  888    888   
+echo  `88.    .88'   888    888 . 
+echo   `Y8bood8P'   o888o   "888" 
+@echo.
+title CECHAMO 81 - installing 6 of 8
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+title CECHAMO 81 - installing 7 of 8
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+title CECHAMO 81 - installing 8 of 8
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
+title Dependency installation complete - thanks to ninjamuffin 99
+echo thanks for using the dependency installer
+@echo.
+echo I think it doesn't bother you
+@echo.
+echo ...
+echo 88b 88 88 88b 88  88888    db    8b    d8 88   88 888888 888888 88 88b 88     dP""Yb dP""Yb 
+echo 88Yb88 88 88Yb88     88   dPYb   88b  d88 88   88 88__   88__   88 88Yb88     Ybood8 Ybood8 
+echo 88 Y88 88 88 Y88 o.  88  dP__Yb  88YbdP88 Y8   8P 88""   88""   88 88 Y88       .8P'   .8P' 
+echo 88  Y8 88 88  Y8 "bodP' dP""""Yb 88 YY 88 `YbodP' 88     88     88 88  Y8      .dP'   .dP'  
+pause
+@echo.
+echo created by
+@echo.
+echo ...
+echo   .d8888b.  8888888888 .d8888b.  888    888        d8888 888b     d888  .d88888b.        .d8888b.   d888   
+echo  d88P  Y88b 888       d88P  Y88b 888    888       d88888 8888b   d8888 d88P" "Y88b      d88P  Y88b d8888    
+echo  888    888 888       888    888 888    888      d88P888 88888b.d88888 888     888      Y88b. d88P   888   
+echo  888        8888888   888        8888888888     d88P 888 888Y88888P888 888     888       "Y88888"    888   
+echo  888        888       888        888    888    d88P  888 888 Y888P 888 888     888      .d8P""Y8b.   888   
+echo  888    888 888       888    888 888    888   d88P   888 888  Y8P  888 888     888      888    888   888   
+echo  Y88b  d88P 888       Y88b  d88P 888    888  d8888888888 888   "   888 Y88b. .d88P      Y88b  d88P   888   
+echo   "Y8888P"  8888888888 "Y8888P"  888    888 d88P     888 888       888  "Y88888P"        "Y8888P"  8888888 
+@echo.
+echo if you are reading this it is because you read very fast or because you are seeing the code of this file and I take this opportunity to apologize for any translation errors
+echo contact: cesarchalas81@gmail.com :3 lol 
