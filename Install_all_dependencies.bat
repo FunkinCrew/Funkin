490f618950a0f9a52b2f28c17f4e0ed7742c7423
luckydog7 <you@example.com>
2023-01-28 21:59:24 +0300

    i forgot about revert💀

---
+++ Install_all_dependencies.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET /P yesno=do you really want to install all dependencies? [y/n]:
+IF "%yesno%"=="y" GOTO Confirmation
+IF "%yesno%"=="Y" GOTO Confirmation
+IF "%yesno%"=="n" GOTO End
+IF "%yesno%"=="N" GOTO End
+
+:Confirmation
+haxelib install hmm
+haxelib run hmm install
+
+:End
+exit
