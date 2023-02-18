f3cf6e7240c9599cc01bfb5b44fd363c87bfb0e8
Nennneko5787 <nennneko@tensi.org>
2023-02-18 16:53:01 +0900

    Bug Fix

---
+++ source/APIStuff.hx
@@ -0,0 +1,8 @@
+package;
+
+class APIStuff
+{
+	inline public static var API:String = "51348:TtzK0rZ8";
+	inline public static var EncKey:String = "5NqKsSVSNKHbF9fPgZPqPg==";
+	inline public static var SESSION:String = null;
+}
