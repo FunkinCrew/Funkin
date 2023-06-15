eff5d596252af780d6f33a4c8b498127b15693f2
Paradox <tjsalinas02@gmail.com>
2023-06-15 11:22:37 -0400

    Update Controls.hx
    Changed Control.RESET from [Y] to [6]. 
    
    Why? Because that means instead of pressing Y to reset the song, you instead press the select button, which prevents accidental resets if you are playing on a non-standard xbox controller. 
    
    I brought up this issue back in 2022 and it never got solved so I just decided I'd make the pull request myself.

--- source/Controls.hx
+++ source/Controls.hx
@@ -566,7 +566,7 @@ class Controls extends FlxActionSet
 			Control.NOTE_LEFT => [DPAD_LEFT, X, LEFT_STICK_DIGITAL_LEFT, RIGHT_STICK_DIGITAL_LEFT],
 			Control.NOTE_RIGHT => [DPAD_RIGHT, B, LEFT_STICK_DIGITAL_RIGHT, RIGHT_STICK_DIGITAL_RIGHT],
 			Control.PAUSE => [START],
-			Control.RESET => [Y]
+			Control.RESET => [6]
 			#if CAN_CHEAT
 			,Control.CHEAT => [X]
 			#end
