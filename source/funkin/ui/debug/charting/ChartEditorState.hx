88ca58f9e22ed96d835ef28cd42dfc2aaa8997e0
EliteMasterEric <ericmyllyoja@gmail.com>
2023-12-15 21:09:01 -0500

    Fixes for updated HaxeUI and Flixel.

--- source/funkin/ui/debug/charting/ChartEditorState.hx
+++ source/funkin/ui/debug/charting/ChartEditorState.hx
@@ -2675,14 +2675,16 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
    * Open the backups folder in the file explorer.
    * Don't call this on HTML5.
    */
-  function openBackupsFolder(?_):Void
+  function openBackupsFolder(?_):Bool
   {
     #if sys
     // TODO: Is there a way to open a folder and highlight a file in it?
     var absoluteBackupsPath:String = Path.join([Sys.getCwd(), ChartEditorImportExportHandler.BACKUPS_PATH]);
     WindowUtil.openFolder(absoluteBackupsPath);
+    return true;
     #else
     trace('No file system access, cannot open backups folder.');
+    return false;
     #end
   }
 
