06964ce1e3ee82fa6041a9b27245146c0f5812af
EliteMasterEric <ericmyllyoja@gmail.com>
2024-01-10 20:58:06 -0500

    Fix HTML5 build issue

--- source/funkin/ui/debug/charting/ChartEditorState.hx
+++ source/funkin/ui/debug/charting/ChartEditorState.hx
@@ -5333,6 +5333,7 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
     if (displayAutosavePopup)
     {
       displayAutosavePopup = false;
+      #if sys
       Toolkit.callLater(() -> {
         var absoluteBackupsPath:String = Path.join([Sys.getCwd(), ChartEditorImportExportHandler.BACKUPS_PATH]);
         this.infoWithActions('Auto-Save', 'Chart auto-saved to ${absoluteBackupsPath}.', [
@@ -5342,6 +5343,9 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
           }
         ]);
       });
+      #else
+      // TODO: No auto-save on HTML5?
+      #end
     }
 
     moveSongToScrollPosition();
