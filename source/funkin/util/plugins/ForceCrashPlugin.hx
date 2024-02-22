201c81cfefbb3f45a21fac03b1734ac6e61175f0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-02-22 18:51:18 -0500

    changed keybind to ctrl+shift+L

--- source/funkin/util/plugins/ForceCrashPlugin.hx
+++ source/funkin/util/plugins/ForceCrashPlugin.hx
@@ -22,8 +22,8 @@ class ForceCrashPlugin extends FlxBasic
   {
     super.update(elapsed);
 
-    // Ctrl + Alt + Shift + L = Crash the game for debugging purposes
-    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.ALT && FlxG.keys.pressed.SHIFT && FlxG.keys.justPressed.L)
+    // Ctrl + Shift + L = Crash the game for debugging purposes
+    if (FlxG.keys.pressed.CONTROL && FlxG.keys.pressed.SHIFT && FlxG.keys.pressed.L)
     {
       // TODO: Make this message 87% funnier.
       throw "DEBUG: Crashing the game via debug keybind!";
