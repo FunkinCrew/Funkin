3975d34b70f53e8ff72d714f131184b0547ce004
EliteMasterEric <ericmyllyoja@gmail.com>
2024-03-20 23:06:32 -0400

    Fix scoring lerp issue on story menu.

--- source/funkin/ui/story/StoryMenuState.hx
+++ source/funkin/ui/story/StoryMenuState.hx
@@ -311,7 +311,7 @@ class StoryMenuState extends MusicBeatState
   {
     Conductor.instance.update();
 
-    highScoreLerp = Std.int(MathUtil.coolLerp(highScoreLerp, highScore, 0.5));
+    highScoreLerp = Std.int(MathUtil.smoothLerp(highScoreLerp, highScore, elapsed, 0.5));
 
     scoreText.text = 'LEVEL SCORE: ${Math.round(highScoreLerp)}';
 
