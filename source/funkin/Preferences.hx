90e93fd0afa70e4c689cdc3a7755282a647a3ae5
Rafplayz <rafploooz@gmail.com>
2024-05-08 23:19:54 -0400

    slider values draft 2: merge sliderPrefs into Prefs

--- source/funkin/Preferences.hx
+++ source/funkin/Preferences.hx
@@ -158,4 +158,25 @@ class Preferences
       #end
     }
   }
+
+  /**
+   * How dark the black screen behind gameplay should be.
+   *
+   * 0 = fully transparent. 1 = opaque.
+   * @default `0`
+   */
+  public static var gameplayBackgroundAlpha(get, set):Float;
+
+  static function get_gameplayBackgroundAlpha():Float
+  {
+    return Save?.instance?.options?.gameplayBackgroundAlpha ?? 0;
+  }
+
+  static function set_gameplayBackgroundAlpha(value:Float):Float
+  {
+    var save:Save = Save.instance;
+    save.options.gameplayBackgroundAlpha = value;
+    save.flush();
+    return value;
+  }
 }
