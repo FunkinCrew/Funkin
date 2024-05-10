8f4e82c2e7a2483297b3bfbc7055318de466103d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-05-10 07:19:04 +0300

    Update Controls.hx

--- source/funkin/input/Controls.hx
+++ source/funkin/input/Controls.hx
@@ -1379,7 +1379,7 @@ class FlxActionInputDigitalAndroid extends FlxActionInputDigital
 
   override public function check(Action:FlxAction):Bool
   {
-    returnswitch(trigger)
+    return switch(trigger)
     {
       #if android
       case PRESSED: FlxG.android.checkStatus(inputID, PRESSED) || FlxG.android.checkStatus(inputID, PRESSED);
