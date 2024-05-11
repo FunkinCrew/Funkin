fa4fa8116c7e07487e8b9f6fc4e71be2e0007703
nebulazorua <rainbowsmokefox@gmail.com>
2024-05-11 16:51:27 +0800

    abstract enum pagename
    should make adding modded optionspages better and easier
    makes it so you dont have to leech off an unused PageName or write your own code for making modded options, helping with compatibility between mods.

--- source/funkin/ui/options/OptionsState.hx
+++ source/funkin/ui/options/OptionsState.hx
@@ -266,11 +266,11 @@ class OptionsMenu extends Page
   #end
 }
 
-enum PageName
+enum abstract PageName(String)
 {
-  Options;
-  Controls;
-  Colors;
-  Mods;
-  Preferences;
+  var Options = "options";
+  var Controls = "controls";
+  var Colors = "colors";
+  var Mods = "mods";
+  var Preferences = "preferences";
 }
