df161e9d98310220eb8986c0bdbcc83e6ed92730
KutikiPlayz <kutikiplayz@gmail.com>
2024-05-15 20:50:05 -0600

    make strum confirm anim length consistant

--- source/funkin/play/notes/StrumlineNote.hx
+++ source/funkin/play/notes/StrumlineNote.hx
@@ -131,18 +131,7 @@ class StrumlineNote extends FunkinSprite
   public function playConfirm(hold:Bool = false):Void
   {
     this.active = (forceActive || isAnimationDynamic('confirm'));
-
-    if (!hold)
-    {
-      this.playAnimation('confirm', true);
-
-      // Run a timer before when we play the confirm animation.
-      // On opponent, this prevent issues with hold notes.
-      // On player, this allows holding the confirm key to fall back to press.
-      confirmHoldTimer = 0;
-    }
-    else
-      holdConfirm();
+    this.playAnimation('confirm', true);
   }
 
   public function isConfirm():Bool
