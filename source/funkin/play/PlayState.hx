5b5b47d1a580907fdf7164908f9a3ea2b2093229
KutikiPlayz <kutikiplayz@gmail.com>
2024-05-17 11:16:29 -0600

    put that shit elsewhere

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2518,9 +2518,7 @@ class PlayState extends MusicBeatSubState
       notesInDirection.remove(targetNote);
 
       // Play the strumline animation.
-      if (targetNote.length == 0) playerStrumline.playConfirm(targetNote.direction);
-      else
-        playerStrumline.holdConfirm(targetNote.direction);
+      playerStrumline.playConfirm(targetNote.direction, targetNote.length > 0);
     }
     else
     {
