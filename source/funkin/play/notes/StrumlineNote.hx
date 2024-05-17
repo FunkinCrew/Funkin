1b7b01063b8442d91f660f295ecd7e4efb268b10
KutikiPlayz <kutikiplayz@gmail.com>
2024-05-17 11:16:29 -0600

    put that shit elsewhere

--- source/funkin/play/notes/StrumlineNote.hx
+++ source/funkin/play/notes/StrumlineNote.hx
@@ -131,12 +131,18 @@ class StrumlineNote extends FunkinSprite
   public function playConfirm(hold:Bool = false):Void
   {
     this.active = (forceActive || isAnimationDynamic('confirm'));
-    this.playAnimation('confirm', true);
 
-    // Run a timer before when we play the confirm animation.
-    // On opponent, this prevent issues with hold notes.
-    // On player, this allows holding the confirm key to fall back to press.
-    confirmHoldTimer = 0;
+    if (!hold)
+    {
+      this.playAnimation('confirm', true);
+
+      // Run a timer before when we play the confirm animation.
+      // On opponent, this prevent issues with hold notes.
+      // On player, this allows holding the confirm key to fall back to press.
+      confirmHoldTimer = 0;
+    }
+    else
+      holdConfirm();
   }
 
   public function isConfirm():Bool
