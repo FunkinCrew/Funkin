643601a1416ebf919f155b3d5bdca44eadecb442
sector-a <82838084+sector-a@users.noreply.github.com>
2024-05-26 15:11:16 +0300

    Update InitState.hx

--- source/funkin/InitState.hx
+++ source/funkin/InitState.hx
@@ -32,7 +32,6 @@ import funkin.util.CLIUtil;
 import funkin.util.CLIUtil.CLIParams;
 import funkin.util.TimerUtil;
 import funkin.util.TrackerUtil;
-import funkin.mobile.MobilePreferences;
 #if discord_rpc
 import Discord.DiscordClient;
 #end
@@ -58,7 +57,6 @@ class InitState extends FlxState
     // Load player options from save data.
     // Flixel has already loaded the save data, so we can just use it.
     Preferences.init();
-    MobilePreferences.init();
 
     // Load controls from save data.
     PlayerSettings.init();
