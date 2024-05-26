f83a8234d81417fbe2895c49d65995bf53c506f6
sector-a <82838084+sector-a@users.noreply.github.com>
2024-05-26 12:22:36 +0300

    Add files via upload

---
+++ source/funkin/mobile/MobilePreferences.hx
@@ -0,0 +1,36 @@
+package funkin.mobile;
+
+import funkin.save.Save;
+
+/**
+ * A core class which provides a store of user-configurable, globally relevant values.
+ */
+class MobilePreferences
+{
+  /**
+   * Loads the user's preferences from the save data and apply them.
+   */
+  public static function init():Void
+  {
+    // empty for now
+  }
+
+  /**
+   * If enabled, vpad will be disabled.
+   * @default `true`
+   */
+  public static var legacyControls(get, set):Bool;
+
+  static function get_legacyControls():Bool
+  {
+    return Save?.instance?.options?.legacyControls ?? true;
+  }
+
+  static function set_legacyControls(value:Bool):Bool
+  {
+    var save:Save = Save.instance;
+    save.options.legacyControls = value;
+    save.flush();
+    return value;
+  }
+}
