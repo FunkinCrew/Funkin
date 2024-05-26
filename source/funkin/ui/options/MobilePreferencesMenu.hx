3bd59f2875c4074c63cafd0a64d2a4648fcd3b67
sector-a <82838084+sector-a@users.noreply.github.com>
2024-05-26 16:36:58 +0300

    Update MobilePreferencesMenu.hx

--- source/funkin/ui/options/MobilePreferencesMenu.hx
+++ source/funkin/ui/options/MobilePreferencesMenu.hx
@@ -9,6 +9,12 @@ class MobilePreferencesMenu extends PreferencesMenu
    */
   override function createPrefItems():Void
   {
+    createPrefItemCheckbox('Allow Screen Timeout', 'Toggle screen timeout', function(value:Bool):Void {
+      Preferences.screenTimeout = value;
+    }, Preferences.screenTimeout);
+    createPrefItemCheckbox('Vibration', 'Toggle vibration', function(value:Bool):Void {
+      Preferences.vibration = value;
+    }, Preferences.vibration);
     createPrefItemCheckbox('Legacy Controls', 'Toggle legacy controls', function(value:Bool):Void {
       Preferences.legacyControls = value;
     }, Preferences.legacyControls);
