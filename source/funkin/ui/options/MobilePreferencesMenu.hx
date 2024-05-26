5f497a329693017b7ecfb62d4fb7bda77642be9a
sector-a <82838084+sector-a@users.noreply.github.com>
2024-05-26 14:57:42 +0300

    Add files via upload

---
+++ source/funkin/ui/options/MobilePreferencesMenu.hx
@@ -0,0 +1,16 @@
+package funkin.ui.options;
+
+import funkin.ui.options.PreferencesMenu;
+
+class MobilePreferencesMenu extends PreferencesMenu
+{
+  /**
+   * Create the menu items for each of the preferences.
+   */
+  override function createPrefItems():Void
+  {
+    createPrefItemCheckbox('Legacy Controls', 'Toggle legacy controls', function(value:Bool):Void {
+      Preferences.legacyControls = value;
+    }, Preferences.legacyControls);
+  }
+}
