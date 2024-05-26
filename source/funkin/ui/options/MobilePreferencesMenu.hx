f99162d35d167c3bff8d03fb7db2e9856ae508f2
sector-a <82838084+sector-a@users.noreply.github.com>
2024-05-26 17:49:47 +0300

    Update MobilePreferencesMenu.hx

--- source/funkin/ui/options/MobilePreferencesMenu.hx
+++ source/funkin/ui/options/MobilePreferencesMenu.hx
@@ -21,14 +21,3 @@ class MobilePreferencesMenu extends PreferencesMenu
     }, Preferences.legacyControls);
   }
 }
-
-function createPrefItemTallyNum(prefName:String, prefDesc:String, onChange:Float->Void, defaultValue:Float):Void
-{
-  var num1:TallyNumber = new TallyNumber(0, 120 * (items.length - 1 + 1), defaultValue = 1 ? defaultValue : 0);
-  var num2:TallyNumber = new TallyNumber(num1.x + 10, 120 * (items.length - 1 + 1), defaultValue = 1 ? 0 : defaultValue * 10);
-
-  items.createItem(120, (120 * items.length) + 30, prefName, AtlasFont.BOLD);
-  
-  preferenceItems.add(num1);
-  preferenceItems.add(num2);
-}
