274ddd3d840745130d0b9b2b7678813955ce8672
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-04 23:34:20 +0300

    Update FunkinHitbox.hx

--- source/funkin/mobile/FunkinHitbox.hx
+++ source/funkin/mobile/FunkinHitbox.hx
@@ -1,8 +1,5 @@
 package funkin.mobile;
 
-import funkin.mobile.ControlsHandler;
-import funkin.mobile.FunkinButton;
-import funkin.play.notes.NoteSprite;
 import flixel.input.actions.FlxActionInput;
 import flixel.graphics.FlxGraphic;
 import flixel.group.FlxSpriteGroup;
@@ -10,37 +7,78 @@ import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxSignal;
 import flixel.FlxG;
+import funkin.graphics.shaders.HSVShader;
+import funkin.mobile.ControlsHandler;
+import funkin.mobile.FunkinButton;
+import funkin.play.notes.NoteSprite;
 import openfl.display.Shape;
 import openfl.display.BitmapData;
 import openfl.geom.Matrix;
+
+/**
+ * This class mainly exist just for the hints to be able to use the hsvs shader like the notes.
+ */
+class FunkinHint extends FunkinButton
+{
+  var hsvShader:HSVShader;
+
+  /**
+   * Creates a new `FunkinButton` object.
+   *
+   * @param X The x position of the button.
+   * @param Y The y position of the button.
+   */
+  public function new(X:Float = 0, Y:Float = 0):Void
+  {
+    super(X, Y);
+
+    hsvShader = new HSVShader();
+    hsvShader.hue = 1.0;
+    hsvShader.saturation = 1.0;
+    hsvShader.value = 1.0;
+
+    shader = hsvShader;
+  }
+
+  public function desaturate():Void
+  {
+    this.hsvShader.saturation = 0.2;
+  }
+
+  public function setHue(hue:Float):Void
+  {
+    this.hsvShader.hue = hue;
+  }
+}
 
 /**
  * A zone with 4 buttons (A hitbox).
  *
  * It's really easy to customize the layout.
  */
-class FunkinHitbox extends FlxTypedSpriteGroup<FunkinButton>
+class FunkinHitbox extends FlxTypedSpriteGroup<FunkinHint>
 {
   /**
    * The array containing the hitbox's buttons.
    */
-  public var hints(default, null):Array<FunkinButton> = [];
+  public var hints(default, null):Array<FunkinHint> = [];
 
   /**
    * A `FlxTypedSignal` that triggers every time a button was pressed.
    */
-  public var onHintDown:FlxTypedSignal<FunkinButton->Void> = new FlxTypedSignal<FunkinButton->Void>();
+  public var onHintDown:FlxTypedSignal<FunkinHint->Void> = new FlxTypedSignal<FunkinHint->Void>();
 
   /**
    * A `FlxTypedSignal` that triggers every time a button was released.
    */
-  public var onHintUp:FlxTypedSignal<FunkinButton->Void> = new FlxTypedSignal<FunkinButton->Void>();
+  public var onHintUp:FlxTypedSignal<FunkinHint->Void> = new FlxTypedSignal<FunkinHint->Void>();
 
   var trackedInputs:Array<FlxActionInput> = [];
 
   /**
    * Create the zone.
    */
+  @:access(funkin.play.notes.NoteSprite)
   public function new():Void
   {
     super();
@@ -51,7 +89,7 @@ class FunkinHitbox extends FlxTypedSpriteGroup<FunkinButton>
 
     for (i in 0...NoteSprite.DIRECTION_COLORS)
     {
-      var hint:FunkinButton = createHint(i * perHintWidth, 0, perHintWidth, perHintHeight, i, hintsColors[i]);
+      var hint:FunkinHint = createHint(i * perHintWidth, 0, perHintWidth, perHintHeight, i, hintsColors[i]);
       hints[hint.ID] = hint;
       add(hint);
     }
@@ -63,9 +101,9 @@ class FunkinHitbox extends FlxTypedSpriteGroup<FunkinButton>
     ControlsHandler.setupHitbox(PlayerSettings.player1.controls, this, trackedInputs);
   }
 
-  private function createHint(x:Float, y:Float, width:Int, height:Int, id:Int, color:FlxColor = 0xFFFFFFFF):FunkinButton
+  private function createHint(x:Float, y:Float, width:Int, height:Int, id:Int, color:FlxColor = 0xFFFFFFFF):FunkinHint
   {
-    var hint:FunkinButton = new FunkinButton(x, y);
+    var hint:FunkinHint = new FunkinHint(x, y);
     hint.loadGraphic(createHintGraphic(width, height, color));
     hint.alpha = 0.00001;
     hint.onDown.add(function():Void {
