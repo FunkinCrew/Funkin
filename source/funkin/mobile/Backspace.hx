cdbc1555bcc347c7b701905de570d1cbd7859604
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-04 22:26:19 +0300

    Update Backspace.hx

--- source/funkin/mobile/Backspace.hx
+++ source/funkin/mobile/Backspace.hx
@@ -2,26 +2,18 @@ package funkin.mobile;
 
 import flixel.FlxG;
 import flixel.util.FlxColor;
-import funkin.graphics.FunkinCamera;
 import funkin.mobile.FunkinButton;
 
 class Backspace extends FunkinButton
 {
-  public var backButtonCam:FunkinCamera;
-
   public function new(?xPos:Float = 0, ?yPos:Float = 0, ?theColor:FlxColor = FlxColor.WHITE, ?onClick:Void->Void = null):Void
   {
     super(xPos, yPos);
-
-    backButtonCam = new FunkinCamera('backButton');
-    backButtonCam.bgColor.alpha = 0;
-    FlxG.cameras.add(backButtonCam, false);
 
     frames = Paths.getSparrowAtlas("backspace");
     animation.addByPrefix("idle", "backspace to exit white0");
     animation.play("idle");
     color = theColor;
-    cameras = [backButtonCam];
 
     if (onClick != null) onDown.add(onClick);
 
