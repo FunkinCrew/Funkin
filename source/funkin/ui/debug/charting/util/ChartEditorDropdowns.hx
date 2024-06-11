39c9da65c7bd40edda3f3640a29a502ea7076560
lemz <ismael.amjad07@gmail.com>
2024-06-11 23:48:05 +0200

    Update ChartEditorDropdowns.hx

--- source/funkin/ui/debug/charting/util/ChartEditorDropdowns.hx
+++ source/funkin/ui/debug/charting/util/ChartEditorDropdowns.hx
@@ -126,7 +126,10 @@ class ChartEditorDropdowns
   {
     dropDown.dataSource.clear();
 
-    var noteStyleIds:Array<String> = NoteStyleRegistry.instance.listEntryIds();
+    // hardcoding this because i dont want note kind styles to be shown as well
+    // there is probably a better solution
+    // var noteStyleIds:Array<String> = NoteStyleRegistry.instance.listEntryIds();
+    var noteStyleIds:Array<String> = ['funkin', 'pixel'];
 
     var returnValue:DropDownEntry = {id: "funkin", text: "Funkin'"};
 
