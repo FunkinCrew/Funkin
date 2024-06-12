4c8085bf8a080f66889262afc051cb47f98ce06b
Burgerballs <107233412+Burgerballs@users.noreply.github.com>
2024-06-12 21:59:25 +0100

    Minor stuff

--- source/funkin/play/PauseSubState.hx
+++ source/funkin/play/PauseSubState.hx
@@ -449,13 +449,12 @@ class PauseSubState extends MusicBeatSubState
    */
   function changeSelection(change:Int = 0):Void
   {
-    var prevEntry:Int = currentEntry;
     currentEntry += change;
 
     if (currentEntry < 0) currentEntry = currentMenuEntries.length - 1;
     if (currentEntry >= currentMenuEntries.length) currentEntry = 0;
 
-    if (currentEntry != prevEntry) FunkinSound.playOnce(Paths.sound('scrollMenu'), 0.4);
+    if (change != 0) FunkinSound.playOnce(Paths.sound('scrollMenu'), 0.4);
 
     for (entryIndex in 0...currentMenuEntries.length)
     {
