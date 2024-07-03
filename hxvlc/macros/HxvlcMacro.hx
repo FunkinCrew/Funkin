dd8b8ed9d0bf5e680ef642976db2926a2d6a8fda
Karim Akra <144803230+KarimAkra@users.noreply.github.com>
2024-07-03 09:44:48 +0300

    Update HxvlcMacro.hx

--- hxvlc/macros/HxvlcMacro.hx
+++ hxvlc/macros/HxvlcMacro.hx
@@ -30,50 +30,14 @@ class HxvlcMacro
       }
     ]);
 
-    // Here, we add extra parameters to the play method
-    // haxe.macro.Context.info('[INFO] ${cls.name}: Adding new parameters to the play method in openFL Video...', pos);
-
-    for (field in fields)
-    {
-      // Check if the field/method name is indeed called play
-      if (field.name == "play")
-      {
-        switch (field.kind)
-        {
-          case FFun(func):
-            // Push all the new args into an array.
-            var newArgs = [];
-            newArgs.push(
-              {
-                name: "location",
-                type: macro :String,
-                opt: true,
-                pos: pos
-              });
-
-            newArgs.push(
-              {
-                name: "shouldLoop",
-                type: macro :Bool,
-                opt: true,
-                pos: pos
-              });
-
-            // Connect the main args with the new ones
-            func.args = func.args.concat(newArgs);
-          default:
-        }
-      }
-    }
-
     return fields;
   }
 
   /**
-   * A macro to be called targeting the `FlxVideoSprite` class.
+   * A macro that adds 2 new parameters to the `play` method.
    * @return An array of fields that the class contains.
    */
-  public static macro function buildFlxVideoSprite():Array<haxe.macro.Expr.Field>
+  public static macro function addPlayMethodParams():Array<haxe.macro.Expr.Field>
   {
     var pos:haxe.macro.Expr.Position = haxe.macro.Context.currentPos();
     // The Video class. We can add new properties to this class.
@@ -82,7 +46,7 @@ class HxvlcMacro
     var fields:Array<haxe.macro.Expr.Field> = haxe.macro.Context.getBuildFields();
 
     // Here, we add extra parameters to the play method
-    // haxe.macro.Context.info('[INFO] ${cls.name}: Adding new parameters to the play method in FlxVideoSprite...', pos);
+    // haxe.macro.Context.info('[INFO] ${cls.name}: Adding new parameters to the play method...', pos);
 
     for (field in fields)
     {
