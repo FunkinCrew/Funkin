8496dde824deb951dbb80fca5a83d7b8329aef3b
CrusherNotDrip <90648119+CrusherNotDrip@users.noreply.github.com>
2024-07-10 21:56:43 -0400

    so that happened again

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -1333,7 +1333,7 @@ class PlayState extends MusicBeatSubState
       currentSong.song + ' (' + storyDifficultyText + ')', iconRPC);
     #end
 
-    if (health > Constants.HEALTH_MIN && isInCountdown && mayPauseGame && !justUnpaused)
+    if (health >= Constants.HEALTH_MIN && !isPlayerDying && isInCountdown && mayPauseGame && !justUnpaused)
     {
       var event = new PauseScriptEvent(FlxG.random.bool(1 / 1000));
 
