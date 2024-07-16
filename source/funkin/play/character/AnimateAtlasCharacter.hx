e2297af1cd019b704f7759a84f6542dce67a6289
7oltan <87986834+7oltan@users.noreply.github.com>
2024-07-16 12:52:51 +0300

    FIX CHARACTER PATHS ISSUE

--- source/funkin/play/character/AnimateAtlasCharacter.hx
+++ source/funkin/play/character/AnimateAtlasCharacter.hx
@@ -131,7 +131,7 @@ class AnimateAtlasCharacter extends BaseCharacter
   {
     trace('[ATLASCHAR] Loading sprite atlas for ${characterId}.');
 
-    var sprite:FlxAtlasSprite = new FlxAtlasSprite(0, 0, Paths.animateAtlas(_data.assetPath, 'shared'));
+    var sprite:FlxAtlasSprite = new FlxAtlasSprite(0, 0, Paths.animateAtlas(_data.assetPath));
 
     sprite.onAnimationFinish.removeAll();
     sprite.onAnimationFinish.add(this.onAnimationFinished);
