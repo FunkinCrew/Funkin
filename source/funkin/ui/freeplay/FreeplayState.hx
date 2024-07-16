a93cd05aeb6c30c241c6c8f8211abb953bb65532
AppleHair <95587502+AppleHair@users.noreply.github.com>
2024-07-16 22:36:11 +0300

    Added a comment

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -2080,6 +2080,8 @@ class FreeplaySongData
     if (!this.songDifficulties.contains(currentDifficulty) && currentDifficulty != Constants.DEFAULT_DIFFICULTY)
     {
       currentDifficulty = Constants.DEFAULT_DIFFICULTY;
+      // This method gets called again by the setter-method,
+      // so there's no need to continue.
       return;
     }
 
