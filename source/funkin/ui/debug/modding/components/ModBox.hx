9b5029399d1db0d0d53b4b761725c13637471201
lemz <ismael.amjad07@gmail.com>
2024-07-25 07:26:51 +0200

    remove dragging

--- source/funkin/ui/debug/modding/components/ModBox.hx
+++ source/funkin/ui/debug/modding/components/ModBox.hx
@@ -2,20 +2,16 @@ package funkin.ui.debug.modding.components;
 
 import haxe.ui.containers.VBox;
 import haxe.ui.components.Label;
-import haxe.ui.extensions.Draggable;
 import haxe.ui.events.MouseEvent;
 import haxe.ui.events.Events;
-import haxe.ui.events.DragEvent;
 
 /**
  * HaxeUI component for the mod menu
  */
 @:build(haxe.ui.ComponentBuilder.build("assets/exclude/data/ui/mod-menu/components/modbox.xml"))
 @:composite(ModBoxEvents)
-class ModBox extends VBox implements Draggable
+class ModBox extends VBox
 {
-  var isDragging:Bool;
-
   var modLabel:Label;
 
   public var modId(default, null):String;
@@ -26,15 +22,8 @@ class ModBox extends VBox implements Draggable
   {
     super();
 
-    // this.draggable = true;
-    // TODO: Implement dragging, since that would be nicer
-    // but for now im just gonna do it in a simple way
-    // when we implement dragging we should probably remove the click event
-    this.draggable = false;
-
     this.modLabel.value = modName;
-
-    this.isDragging = false;
+    this.modLabel.tooltip = modDescription;
 
     this.modId = modId;
     this.modName = modName;
@@ -45,7 +34,6 @@ class ModBox extends VBox implements Draggable
 /**
  * Composite class for handling mouse events
  */
-@:access(funkin.ui.debug.modding.components.ModBox)
 class ModBoxEvents extends Events
 {
   var _modBox:ModBox;
@@ -70,14 +58,6 @@ class ModBoxEvents extends Events
     {
       registerEvent(MouseEvent.CLICK, onClick);
     }
-    if (!hasEvent(DragEvent.DRAG_START, onDragStart))
-    {
-      registerEvent(DragEvent.DRAG_START, onDragStart);
-    }
-    if (!hasEvent(DragEvent.DRAG_END, onDragEnd))
-    {
-      registerEvent(DragEvent.DRAG_END, onDragEnd);
-    }
   }
 
   public override function unregister():Void
@@ -85,43 +65,20 @@ class ModBoxEvents extends Events
     unregisterEvent(MouseEvent.MOUSE_OVER, onMouseOver);
     unregisterEvent(MouseEvent.MOUSE_OUT, onMouseOut);
     unregisterEvent(MouseEvent.CLICK, onClick);
-    unregisterEvent(DragEvent.DRAG_START, onDragStart);
-    unregisterEvent(DragEvent.DRAG_END, onDragEnd);
   }
 
   function onMouseOver(event:MouseEvent):Void
   {
-    if (_modBox.isDragging)
-    {
-      return;
-    }
-
     _modBox.addClass(":hover", true, true);
   }
 
   function onMouseOut(event:MouseEvent):Void
   {
     _modBox.removeClass(":hover", true, true);
-    _modBox.removeClass(":down", true, true);
   }
 
   function onClick(event:MouseEvent):Void
   {
     _modBox.removeClass(":hover", true, true);
-    _modBox.removeClass(":down", true, true);
-  }
-
-  function onDragStart(event:DragEvent):Void
-  {
-    _modBox.removeClass(":hover", true, true);
-    _modBox.addClass(":down", true, true);
-    _modBox.isDragging = true;
-  }
-
-  function onDragEnd(event:DragEvent):Void
-  {
-    _modBox.removeClass(":down", true, true);
-    _modBox.addClass(":hover", true, true);
-    _modBox.isDragging = false;
   }
 }
