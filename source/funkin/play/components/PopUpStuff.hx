cca997b7243dcc20e0602253284a61de4ddcbfd0
Til Tjardes <til.tjardes@gmail.com>
2024-09-22 17:59:18 -0600

    fix develop branch cuz git is on crack

--- source/funkin/play/components/PopUpStuff.hx
+++ source/funkin/play/components/PopUpStuff.hx
@@ -125,13 +125,4 @@ class PopUpStuff extends FlxTypedGroup<FunkinSprite>
       daLoop++;
     }
   }
-
-  /**
-   * Reset the popup configuration to the default.
-   */
-  public static function reset()
-  {
-    noteStyle = NoteStyleRegistry.instance.fetchDefault();
-    isPixel = false;
-  }
 }
