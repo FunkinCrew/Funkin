fde4b1f95a582b3a90335af8a289bb707ee7766e
KutikiPlayz <kutikiplayz@gmail.com>
2024-10-08 19:55:02 -0600

    no more pico

--- source/funkin/data/freeplay/player/PlayerData.hx
+++ source/funkin/data/freeplay/player/PlayerData.hx
@@ -95,6 +95,10 @@ class PlayerFreeplayDJData
   var animations:Array<AnimationData>;
 
   @:optional
+  @default(true)
+  var danceToBeat:Bool;
+
+  @:optional
   @:default("BOYFRIEND")
   var text1:String;
 
@@ -143,6 +147,11 @@ class PlayerFreeplayDJData
   public function getAtlasPath():String
   {
     return Paths.animateAtlas(assetPath);
+  }
+
+  public function shouldDanceToBeat():Bool
+  {
+    return danceToBeat;
   }
 
   public function getFreeplayDJText(index:Int):String
