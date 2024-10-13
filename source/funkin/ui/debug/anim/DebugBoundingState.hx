684c3b261c5128cb45b56da5085542c11be8bdd4
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2024-10-13 13:43:12 -0300

    Replace deprecated screenX/Y in MouseUtil

--- source/funkin/ui/debug/anim/DebugBoundingState.hx
+++ source/funkin/ui/debug/anim/DebugBoundingState.hx
@@ -75,7 +75,7 @@ class DebugBoundingState extends FlxState
   function get_haxeUIFocused():Bool
   {
     // get the screen position, according to the HUD camera, temp default to FlxG.camera juuust in case?
-    var hudMousePos:FlxPoint = FlxG.mouse.getScreenPosition(hudCam ?? FlxG.camera);
+    var hudMousePos:FlxPoint = FlxG.mouse.getViewPosition(hudCam ?? FlxG.camera);
     return Screen.instance.hasSolidComponentUnderPoint(hudMousePos.x, hudMousePos.y);
   }
 
