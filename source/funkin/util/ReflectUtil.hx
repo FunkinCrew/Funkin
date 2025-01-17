e73be4a2b3a0f86e2c81a527e91e340d84511829
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-01-17 19:46:53 +0000

    Update ReflectUtil.hx (#4008)

--- source/funkin/util/ReflectUtil.hx
+++ source/funkin/util/ReflectUtil.hx
@@ -44,7 +44,7 @@ class ReflectUtil
     return Reflect.field(obj, name);
   }
 
-  public static function setAnonymousField(obj:Dynamic, name:String, value:Dynamic):Dynamic
+  public static function setAnonymousField(obj:Dynamic, name:String, value:Dynamic):Void
   {
     return Reflect.setField(obj, name, value);
   }
