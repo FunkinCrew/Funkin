c3f5535b69830301bdbf0c8af9bc902154085f8e
cyn0x8 <cyn0x8+git@gmail.com>
2025-01-22 19:27:05 -0800

    scriptable class changes

--- source/funkin/graphics/ScriptedFunkinSprite.hx
+++ source/funkin/graphics/ScriptedFunkinSprite.hx
@@ -1,8 +1,8 @@
-package funkin.modding.base;
+package funkin.graphics;
 
 /**
  * A script that can be tied to a FunkinSprite.
  * Create a scripted class that extends FunkinSprite to use this.
  */
 @:hscriptClass
-class ScriptedFunkinSprite extends funkin.graphics.FunkinSprite implements HScriptedClass {}
+class ScriptedFunkinSprite extends funkin.graphics.FunkinSprite implements polymod.hscript.HScriptedClass {}
---
+++ source/funkin/modding/base/Object.hx
@@ -0,0 +1,13 @@
+package funkin.modding.base;
+
+/**
+ * An empty base class meant to be extended by scripts.
+ */
+
+class Object {
+  public function new() {}
+
+  public function toString():String {
+    return "(Object)";
+  }
+}
