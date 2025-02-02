3530554372c450e985713ea429f79c343e50583d
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-02 21:17:55 +1000

    forgot to uncomment the bit making the async loop null

--- source/funkin/util/plugins/ScreenshotPlugin.hx
+++ source/funkin/util/plugins/ScreenshotPlugin.hx
@@ -149,7 +149,7 @@ class ScreenshotPlugin extends FlxBasic
           // clean up our loop
           asyncLoop.kill();
           asyncLoop.destroy();
-          // asyncLoop = null;
+          asyncLoop = null;
         }
       }
       // Examples ftw! - Lasercar
