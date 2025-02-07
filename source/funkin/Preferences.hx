3486f2868fd74676f5f391ad36d1756ab6a3045e
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-07 16:19:41 +1000

    comment cleanup

--- source/funkin/Preferences.hx
+++ source/funkin/Preferences.hx
@@ -257,7 +257,7 @@ class Preferences
   }
 
   /**
-   * If enabled, the game will save any screenshots taken to this format.
+   * The game will save any screenshots taken to this format.
    * @default `PNG`
    */
   public static var saveFormat(get, set):Any;
@@ -276,7 +276,7 @@ class Preferences
   }
 
   /**
-   * If enabled, the game will save JEPG screenshots with this quality percentage.
+   * The game will save JPEG screenshots with this quality percentage.
    * @default `80`
    */
   public static var jpegQuality(get, set):Int;
