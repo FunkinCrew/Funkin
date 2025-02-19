ee5244c5103ec21a76c6719d70a485e816bca237
JVN-Pixels <146671762+JVNpixels@users.noreply.github.com>
2025-02-18 17:03:08 -0800

    Update Preferences.hx

--- source/funkin/Preferences.hx
+++ source/funkin/Preferences.hx
@@ -176,6 +176,25 @@ class Preferences
     return value;
   }
 
+    /**
+   * If enabled, the game will automatically launch in fullscreen on startup.
+   * @default `true`
+   */
+   public static var autoFullscreen(get, set):Bool;
+
+   static function get_autoFullscreen():Bool
+   {
+     return Save?.instance?.options?.autoFullscreen ?? true;
+   }
+ 
+   static function set_autoFullscreen(value:Bool):Bool
+   {
+     var save:Save = Save.instance;
+     save.options.autoFullscreen = value;
+     save.flush();
+     return value;
+   }
+
   public static var unlockedFramerate(get, set):Bool;
 
   static function get_unlockedFramerate():Bool
@@ -230,6 +249,8 @@ class Preferences
     #if web
     toggleFramerateCap(Preferences.unlockedFramerate);
     #end
+    // Apply the autoFullscreen setting (launches the game in fullscreen automatically)
+    FlxG.fullscreen = Preferences.autoFullscreen;
   }
 
   static function toggleFramerateCap(unlocked:Bool):Void
