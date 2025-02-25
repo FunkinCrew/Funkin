4291733cc2dfa661b4dac62b65bb31f9367d378c
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-26 00:53:21 +1000

    A fix for a thing

--- source/funkin/ui/mainmenu/MainMenuState.hx
+++ source/funkin/ui/mainmenu/MainMenuState.hx
@@ -105,6 +105,7 @@ class MainMenuState extends MusicBeatState
     createMenuItem('freeplay', 'mainmenu/freeplay', function() {
       persistentDraw = true;
       persistentUpdate = false;
+      rememberedSelectedIndex = menuItems.selectedIndex;
       // Freeplay has its own custom transition
       FlxTransitionableState.skipNextTransIn = true;
       FlxTransitionableState.skipNextTransOut = true;
