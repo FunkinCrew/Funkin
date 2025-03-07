7231b3f6450fff10509c51fdaa5d7228582cbf6b
EliteMasterEric <ericmyllyoja@gmail.com>
2025-03-06 23:57:05 -0500

    New chart event: Set the character's health icon.

---
+++ source/funkin/play/event/SetCharacterSongEvent.hx
@@ -0,0 +1,3 @@
+package funkin.play.event;
+
+// TODO: Add a song event which switches characters.
--- source/funkin/ui/debug/charting/toolboxes/ChartEditorEventDataToolbox.hx
+++ source/funkin/ui/debug/charting/toolboxes/ChartEditorEventDataToolbox.hx
@@ -49,9 +49,14 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
   function initialize():Void
   {
     toolboxEventsEventKind.onChange = function(event:UIEvent) {
+      if (event.data == null)
+      {
+        trace('ChartEditorEventDataToolbox: Event data is null');
+      }
+
       var eventType:String = event.data.id;
 
-      trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Event type changed: $eventType');
+      trace('ChartEditorEventDataToolbox - Event type changed: $eventType');
 
       // Edit the event data to place.
       chartEditorState.eventKindToPlace = eventType;
@@ -60,7 +65,7 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
 
       if (schema == null)
       {
-        trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Unknown event kind: $eventType');
+        trace('ChartEditorEventDataToolbox - Unknown event kind: $eventType');
         return;
       }
 
@@ -80,7 +85,7 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
       }
     }
     var startingEventValue = ChartEditorDropdowns.populateDropdownWithSongEvents(toolboxEventsEventKind, chartEditorState.eventKindToPlace);
-    trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Starting event kind: ${startingEventValue}');
+    trace('ChartEditorEventDataToolbox - Starting event kind: ${startingEventValue}');
     toolboxEventsEventKind.value = startingEventValue;
   }
 
@@ -92,7 +97,7 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
 
     if (newDropdownElement == null)
     {
-      throw 'ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Event kind not in dropdown: ${chartEditorState.eventKindToPlace}';
+      throw 'ChartEditorEventDataToolbox - Event kind not in dropdown: ${chartEditorState.eventKindToPlace}';
     }
     else if (toolboxEventsEventKind.value != newDropdownElement || lastEventKind != toolboxEventsEventKind.value.id)
     {
@@ -101,17 +106,17 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
       var schema:SongEventSchema = SongEventRegistry.getEventSchema(chartEditorState.eventKindToPlace);
       if (schema == null)
       {
-        trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Unknown event kind: ${chartEditorState.eventKindToPlace}');
+        trace('ChartEditorEventDataToolbox - Unknown event kind: ${chartEditorState.eventKindToPlace}');
       }
       else
       {
-        trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Event kind changed: ${toolboxEventsEventKind.value.id} != ${newDropdownElement.id} != ${lastEventKind}, rebuilding form');
+        trace('ChartEditorEventDataToolbox - Event kind changed: ${toolboxEventsEventKind.value.id} != ${newDropdownElement.id} != ${lastEventKind}, rebuilding form');
         buildEventDataFormFromSchema(toolboxEventsDataGrid, schema, chartEditorState.eventKindToPlace);
       }
     }
     else
     {
-      trace('ChartEditorToolboxHandler.buildToolboxEventDataLayout() - Event kind not changed: ${toolboxEventsEventKind.value} == ${newDropdownElement} == ${lastEventKind}');
+      trace('ChartEditorEventDataToolbox - Event kind not changed: ${toolboxEventsEventKind.value} == ${newDropdownElement} == ${lastEventKind}');
     }
 
     for (pair in chartEditorState.eventDataToPlace.keyValueIterator())
@@ -123,7 +128,7 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
 
       if (field == null)
       {
-        throw 'ChartEditorToolboxHandler.refresh() - Field "${fieldId}" does not exist in the event data form for kind ${lastEventKind}.';
+        throw 'ChartEditorEventDataToolbox - Field "${fieldId}" does not exist in the event data form for kind ${lastEventKind}.';
       }
       else
       {
@@ -142,7 +147,7 @@ class ChartEditorEventDataToolbox extends ChartEditorBaseToolbox
             var textField:TextField = cast field;
             textField.text = value;
           default:
-            throw 'ChartEditorToolboxHandler.refresh() - Field "${fieldId}" is of unknown type "${Type.getClassName(Type.getClass(field))}".';
+            throw 'ChartEditorEventDataToolbox - Field "${fieldId}" is of unknown type "${Type.getClassName(Type.getClass(field))}".';
         }
       }
     }
