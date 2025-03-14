ff8fe9000321e6fcac9582e809e7b6de0457530a
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-03-14 22:51:33 +1000

    unmute character vocals for animations

--- source/funkin/play/event/PlayAnimationSongEvent.hx
+++ source/funkin/play/event/PlayAnimationSongEvent.hx
@@ -50,6 +50,7 @@ class PlayAnimationSongEvent extends SongEvent
       if (Std.isOfType(target, BaseCharacter))
       {
         var targetChar:BaseCharacter = cast target;
+        targetChar.tempVocals = force;
         targetChar.playAnimation(anim, force, force);
       }
       else
