2515251d4472a701141bb2931fd0eef53e346d5e
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-03-15 13:57:44 +1000

    New highscore plays twice fix

--- source/funkin/play/ResultState.hx
+++ source/funkin/play/ResultState.hx
@@ -530,16 +530,6 @@ class ResultState extends MusicBeatSubState
         // scorePopin.animation.play("score");
 
         // scorePopin.visible = true;
-
-        if (params.isNewHighscore ?? false)
-        {
-          highscoreNew.visible = true;
-          highscoreNew.animation.play("new");
-        }
-        else
-        {
-          highscoreNew.visible = false;
-        }
       };
     }
 
