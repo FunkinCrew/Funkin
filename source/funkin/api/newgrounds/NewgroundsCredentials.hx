9580875c232fa761cd137382c951897a9e5944b3
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2025-03-31 14:42:16 -0400

    remove NewgroundsCredentials.hx derp

--- source/funkin/api/newgrounds/NewgroundsCredentials.hx
+++
@@ -1,7 +0,0 @@
-package funkin.api.newgrounds;
-
-class NewgroundsCredentials
-{
-  public static final APP_ID:String = #if API_NG_APP_ID haxe.macro.Compiler.getDefine("API_NG_APP_ID") #else '58004:a1GXEVhr' #end;
-  public static final ENCRYPTION_KEY:String = #if API_NG_ENC_KEY haxe.macro.Compiler.getDefine("API_NG_ENC_KEY") #else 'pxl4MHYpT737Fe0/7qNBEQ==' #end;
-}
