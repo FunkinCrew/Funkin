ff09cb88cdf0ca1e194c58b581e6de7e3a170932
unknown <47027981+ACrazyTown@users.noreply.github.com>
2025-04-04 21:32:36 +0200

    use System.totalMemoryNumber

--- source/funkin/util/MemoryUtil.hx
+++ source/funkin/util/MemoryUtil.hx
@@ -47,16 +47,11 @@ class MemoryUtil
 
   /**
    * Calculate the total memory usage of the program, in bytes.
-   * @return Int
+   * @return Float
    */
-  public static function getMemoryUsed():#if cpp Float #else Int #end
+  public static function getMemoryUsed():Float
   {
-    #if cpp
-    // There is also Gc.MEM_INFO_RESERVED, MEM_INFO_CURRENT, and MEM_INFO_LARGE.
-    return cpp.vm.Gc.memInfo64(cpp.vm.Gc.MEM_INFO_USAGE);
-    #else
-    return openfl.system.System.totalMemory;
-    #end
+    return openfl.system.System.totalMemoryNumber;
   }
 
   /**
