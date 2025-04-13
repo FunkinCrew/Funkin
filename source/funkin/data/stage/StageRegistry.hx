963e2ecd630fae24dae4206d315618cf4c4be6db
EliteMasterEric <ericmyllyoja@gmail.com>
2025-04-13 03:39:31 -0400

    Edit RegistryMacro to automatically populate baseGameEntryIds list.

--- source/funkin/data/stage/StageRegistry.hx
+++ source/funkin/data/stage/StageRegistry.hx
@@ -16,28 +16,8 @@ class StageRegistry extends BaseRegistry<Stage, StageData> implements ISingleton
 
   public static final STAGE_DATA_VERSION_RULE:thx.semver.VersionRule = ">=1.0.0 <1.1.0";
 
-  static final baseGameStageIds:Array<String> = funkin.util.macro.DataMacro.listBaseGameStageIds();
-
   public function new()
   {
     super('STAGE', 'stages', STAGE_DATA_VERSION_RULE);
-  }
-
-  /**
-   * A list of all the stages from the base game, in order.
-   */
-  public function listBaseGameStageIds():Array<String>
-  {
-    return baseGameStageIds;
-  }
-
-  /**
-   * A list of all installed story weeks that are not from the base game.
-   */
-  public function listModdedStageIds():Array<String>
-  {
-    return listEntryIds().filter(function(id:String):Bool {
-      return listBaseGameStageIds().indexOf(id) == -1;
-    });
   }
 }
