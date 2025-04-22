dfd578517d1aa8505547ba59eb695769d7d6f511
EliteMasterEric <ericmyllyoja@gmail.com>
2025-04-21 22:38:50 -0400

    Make merch referrals error-proof (they don't work if the API is disabled whoops!)

--- source/funkin/ui/mainmenu/MainMenuState.hx
+++ source/funkin/ui/mainmenu/MainMenuState.hx
@@ -21,11 +21,12 @@ import funkin.ui.title.TitleState;
 import funkin.ui.story.StoryMenuState;
 import funkin.ui.Prompt;
 import funkin.util.WindowUtil;
+import funkin.api.newgrounds.Referral;
 #if FEATURE_DISCORD_RPC
 import funkin.api.discord.DiscordClient;
 #end
 #if FEATURE_NEWGROUNDS
-import io.newgrounds.NG;
+import funkin.api.newgrounds.NewgroundsClient;
 #end
 
 class MainMenuState extends MusicBeatState
@@ -168,9 +169,9 @@ class MainMenuState extends MusicBeatState
     this.leftWatermarkText.text = Constants.VERSION;
 
     #if FEATURE_NEWGROUNDS
-    if (NG.core?.loggedIn)
+    if (NewgroundsClient.instance.isLoggedIn())
     {
-      this.leftWatermarkText.text += ' | Newgrounds: Logged in as ${NG.core?.user?.name}';
+      this.leftWatermarkText.text += ' | Newgrounds: Logged in as ${NewgroundsClient.instance.user?.name}';
     }
     #end
   }
@@ -233,14 +234,7 @@ class MainMenuState extends MusicBeatState
 
   function selectMerch()
   {
-    NG.core?.calls.loader.loadReferral(false)
-      .addComponentParameter("referral_name", "merch_link")
-      .addResponseHandler(response -> {
-        if (response.success) WindowUtil.openURL(response.result.data.url)
-        else
-          WindowUtil.openURL(Constants.URL_MERCH_FALLBACK);
-      })
-      .send();
+    Referral.doMerchReferral();
   }
   #end
 
