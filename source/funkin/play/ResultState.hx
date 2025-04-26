cb7735421beee5ac382e4d8a54d650278e307e09
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-04-27 02:29:55 +1000

    Misses now actually make you miss your grade Sustain notes no longer add to the miss tally

--- source/funkin/play/ResultState.hx
+++ source/funkin/play/ResultState.hx
@@ -488,7 +488,8 @@ class ResultState extends MusicBeatSubState
     FlxTween.tween(bgFlash, {alpha: 0}, 5 / 24);
     // NOTE: Only divide if totalNotes > 0 to prevent divide-by-zero errors.
     var clearPercentFloat = params.scoreData.tallies.totalNotes == 0 ? 0.0 : (params.scoreData.tallies.sick +
-      params.scoreData.tallies.good) / params.scoreData.tallies.totalNotes * 100;
+    params.scoreData.tallies.good
+      - params.scoreData.tallies.missed) / params.scoreData.tallies.totalNotes * 100;
     clearPercentTarget = Math.floor(clearPercentFloat);
     // Prevent off-by-one errors.
 
