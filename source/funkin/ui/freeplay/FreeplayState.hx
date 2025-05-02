a1993f4b23c97e8664e940ee9baec44d3adc44b0
VioletSnowLeopard <ngzeretzke@gmail.com>
2025-05-01 20:57:39 -0400

    fix switching filters to do its intended behavior

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -510,10 +510,10 @@ class FreeplayState extends MusicBeatSubState
           generateSongList({filterType: REGEXP, filterData: str}, true);
       }
 
-      // If the current song is still in the list (aka not null?), we'll land on it
+      // If the current song is still in the list, or if it was random, we'll land on it
       // Otherwise we want to land on the first song of the group, rather than random song when changing letter sorts
       // that is, only if there's more than one song in the group!
-      if (curSong != null && currentFilteredSongs.length > 1)
+      if (curSong == null || currentFilteredSongs.contains(curSong))
       {
         changeSelection();
       }
