6035f1275e3d8d6f51aab5200512cf540264b44f
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-05-03 02:38:36 +1000

    Run the oncomplete function once
    Also disable and enable the letterSort input lol

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -359,6 +359,7 @@ class FreeplayState extends MusicBeatSubState
 
     // Block input until the intro finishes.
     controls.active = false;
+    letterSort.inputEnabled = false;
 
     // Add a null entry that represents the RANDOM option
     songs.push(null);
@@ -695,6 +696,8 @@ class FreeplayState extends MusicBeatSubState
 
       if (!fromCharSelect)
       {
+        letterSort.inputEnabled = true;
+
         // render optimisation
         if (_parentState != null) _parentState.persistentDraw = false;
 
@@ -1459,6 +1462,7 @@ class FreeplayState extends MusicBeatSubState
   function enterFromCharSel():Void
   {
     controls.active = false;
+    letterSort.inputEnabled = false;
     if (_parentState != null) _parentState.persistentDraw = false;
 
     var transitionGradient = new FlxSprite(0, 720).loadGraphic(Paths.image('freeplay/transitionGradient'));
@@ -1505,11 +1509,16 @@ class FreeplayState extends MusicBeatSubState
           {
             ease: FlxEase.expoOut,
             onComplete: function(_) {
-              for (index => capsule in grpCapsules.members)
+              if (fromCharSelect)
               {
-                capsule.doLerp = true;
+                // This code only needs to be run once (otherwise it causes problems).
+                for (index => capsule in grpCapsules.members)
+                {
+                  capsule.doLerp = true;
+                }
                 fromCharSelect = false;
                 controls.active = true;
+                letterSort.inputEnabled = true;
               }
             }
           });
