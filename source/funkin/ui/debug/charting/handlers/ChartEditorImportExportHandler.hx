a2a1b0b5d7f7a84614bf8426aa2c35465f00dca0
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-05-03 16:00:03 +1000

    Use first difficulty as fallback

--- source/funkin/ui/debug/charting/handlers/ChartEditorImportExportHandler.hx
+++ source/funkin/ui/debug/charting/handlers/ChartEditorImportExportHandler.hx
@@ -117,12 +117,13 @@ class ChartEditorImportExportHandler
   {
     state.songMetadata = newSongMetadata;
     state.songChartData = newSongChartData;
-    state.selectedDifficulty = state.availableDifficulties[0];
 
-    if (!newSongMetadata.exists(state.selectedVariation))
+    if (!state.songMetadata.exists(state.selectedVariation))
     {
       state.selectedVariation = Constants.DEFAULT_VARIATION;
     }
+    // Use the first available difficulty as a fallback if the currently selected one cannot be found.
+    if (state.availableDifficulties.indexOf(state.selectedDifficulty) < 0) state.selectedDifficulty = state.availableDifficulties[0];
 
     Conductor.instance.forceBPM(null); // Disable the forced BPM.
     Conductor.instance.instrumentalOffset = state.currentInstrumentalOffset; // Loads from the metadata.
