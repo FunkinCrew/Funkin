51f1ca130f698ab989c2cbcac7a8abf905800403
JackXson-Real <32755192+JackXson-Real@users.noreply.github.com>
2025-05-12 12:42:39 -0400

    latency state exits to options state

--- source/funkin/ui/debug/latency/LatencyState.hx
+++ source/funkin/ui/debug/latency/LatencyState.hx
@@ -314,7 +314,7 @@ class LatencyState extends MusicBeatSubState
     {
       // close();
       cleanup();
-      FlxG.switchState(() -> new MainMenuState());
+      FlxG.switchState(() -> new funkin.ui.options.OptionsState());
     }
 
     super.update(elapsed);
