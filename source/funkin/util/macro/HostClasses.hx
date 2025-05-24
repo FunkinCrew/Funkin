fa0c09ec7c54a995f2ba70537de92be05ed5463d
lemz1 <ismael.amjad07@gmail.com>
2025-05-25 00:57:04 +0200

    Update HostClasses.hx

--- source/funkin/util/macro/HostClasses.hx
+++ source/funkin/util/macro/HostClasses.hx
@@ -37,46 +37,110 @@ class HostClasses
     'sys.*'
   ];
 
-  /**
-   * TODO: remove as many function bodies as possible from the generated code,
-   * in order to reduce the size of the generated code. Abstract inline functions require
-   * the function body to be present, so we cannot remove them.
-   */
+  static function isBlacklisted(name:String):Bool
+  {
+    for (entry in BLACK_LIST)
+    {
+      if (entry.endsWith(".*"))
+      {
+        var prefix:String = entry.substr(0, entry.length - 2);
+        if (name.startsWith(prefix)) return true;
+      }
+      else
+      {
+        if (name == entry) return true;
+      }
+    }
+    return false;
+  }
+
   static function generate():Array<Field>
   {
     Context.onGenerate((types) -> {
-      var files:Map<String, String> = new Map<String, String>();
+      var files:Map<String, Null<String>> = new Map<String, Null<String>>();
 
       for (type in types)
       {
+        var module:String;
+        var file:String;
+
         switch (type)
         {
           case TInst(_.get() => t, _):
-            if (files.exists(t.pos.getInfos().file)) continue;
-            files.set(t.pos.getInfos().file, t.module.split('.').join('/'));
+            module = t.module;
+            file = t.pos.getInfos().file;
           case TEnum(_.get() => e, _):
-            if (files.exists(e.pos.getInfos().file)) continue;
-            files.set(e.pos.getInfos().file, e.module.split('.').join('/'));
+            module = e.module;
+            file = e.pos.getInfos().file;
           case TType(_.get() => t, _):
-            if (files.exists(t.pos.getInfos().file)) continue;
-            files.set(t.pos.getInfos().file, t.module.split('.').join('/'));
+            module = t.module;
+            file = t.pos.getInfos().file;
           case TAbstract(_.get() => a, _):
-            if (files.exists(a.pos.getInfos().file)) continue;
-            files.set(a.pos.getInfos().file, a.module.split('.').join('/'));
+            module = a.module;
+            file = a.pos.getInfos().file;
           default:
-            Context.warning('funkin.util.macro.HostClasses.generate(): Type not handled: ' + type.toString(), Context.currentPos());
+            throw 'funkin.util.macro.HostClasses.generate(): Type not handled: ${type.toString()}';
         }
+
+        if (isBlacklisted(module)) files.set(file, null);
+        else if (!files.exists(file)) files.set(file, module.split('.').join('/'));
       }
+
+      var count:Int = [for (m in files.iterator()) if (m != null) 0].length;
+      var finished:Int = 0;
 
       for (file => module in files)
       {
-        var content = sys.io.File.getContent(file);
+        if (module == null) continue;
 
-        var folderPath:String = 'script_std/' + module.split('/').slice(0, -1).join('/');
-        var filePath:String = 'script_std/' + module + '.hx';
+        var content:String = sys.io.File.getContent(file);
+
+        var regex:EReg = ~/\b((?:public|private|static|override|dynamic|final|\s)*function\s+\w+\s*\([^)]*\)(?:\s*:\s*[\w<>\[\], ?]+)?)/g;
+
+        var contentCopy:String = content;
+        while (regex.match(contentCopy))
+        {
+          var signature:String = regex.matched(1);
+          var body:String = regex.matchedRight();
+          contentCopy = regex.matchedRight();
+
+          if (~/\b(?:inline|macro|extern)\b/.match(regex.matchedLeft())) continue;
+
+          var doCheck:Bool = false;
+          var braces:Int = 0;
+
+          var i:Int = 0;
+          while (true)
+          {
+            var c:String = body.charAt(i++);
+            if (c.length == 0) break;
+
+            if (c == '{')
+            {
+              doCheck = true;
+              braces++;
+            }
+            else if (c == '}')
+            {
+              braces--;
+            }
+
+            if (doCheck && braces == 0)
+            {
+              content = content.replace(body.substr(0, i), '');
+              contentCopy = contentCopy.replace(body.substr(0, i), '');
+              content = content.replace(signature, 'extern ${signature};');
+              break;
+            }
+          }
+        }
+        var folderPath:String = 'script_std/${module.split('/').slice(0, -1).join('/')}';
+        var filePath:String = 'script_std/${module}.hx';
 
         sys.FileSystem.createDirectory(folderPath);
         sys.io.File.saveContent(filePath, content);
+
+        trace('Generated ${++finished}/${count}');
       }
     });
 
