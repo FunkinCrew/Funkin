207082b5bce65e22fb94bca50b99be825a0eb882
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-05-30 20:14:41 +1000

    Stage editor - Ctrl+N new stage Also windows target configuration preset for straight to stage editor (not to be confused with the stage builder)

--- source/funkin/ui/debug/stageeditor/StageEditorState.hx
+++ source/funkin/ui/debug/stageeditor/StageEditorState.hx
@@ -633,6 +633,7 @@ class StageEditorState extends UIState
         if (FlxG.keys.justPressed.S) FlxG.keys.pressed.SHIFT ? onMenuItemClick("save stage as") : onMenuItemClick("save stage");
         if (FlxG.keys.justPressed.F) onMenuItemClick("find object");
         if (FlxG.keys.justPressed.O) onMenuItemClick("open stage");
+        if (FlxG.keys.justPressed.N) onMenuItemClick("new stage");
       }
 
       if (FlxG.keys.justPressed.TAB) onMenuItemClick("switch mode");
