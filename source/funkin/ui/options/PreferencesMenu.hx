93e4f799f4f0ea435b3d77ca8ef2ab6beeb0a955
Eric <ericmyllyoja@gmail.com>
2025-05-30 22:46:34 -0400

    Remove JPEG (#1065)
    Co-authored-by: Lasercar <64717068+Lasercar@users.noreply.github.com>

--- source/funkin/ui/options/PreferencesMenu.hx
+++ source/funkin/ui/options/PreferencesMenu.hx
@@ -173,27 +173,6 @@ class PreferencesMenu extends Page<OptionsState.OptionsMenuPageName>
     createPrefItemCheckbox('Preview on save', 'If enabled, the preview will be shown only after a screenshot is saved.', function(value:Bool):Void {
       Preferences.previewOnSave = value;
     }, Preferences.previewOnSave);
-    // TODO: having oValue is weird, probably change this later? was done to accomodate VSync changes.
-    createPrefItemEnum('Save Format', 'Save screenshots to this format.', ['PNG' => 'PNG', 'JPEG' => 'JPEG'], function(value:String, oValue:String):Void {
-      Preferences.saveFormat = value;
-    }, Preferences.saveFormat);
-    createPrefItemNumber('JPEG Quality', 'The quality of JPEG screenshots.', function(value:Float) {
-      Preferences.jpegQuality = Std.int(value);
-    }, null, Preferences.jpegQuality, 0, 100, 5, 0);
-    #end
-
-    #if FEATURE_HAPTICS
-    createPrefItemCheckbox('Vibration', 'If enabled, Haptic Feedback vibration effects will be active.', function(value:Bool):Void {
-      Preferences.vibration = value;
-    }, Preferences.vibration);
-    #end
-
-    #if mobile
-    createPrefItemCheckbox('Allow Screen Timeout',
-      'If enabled, The phone screen will timeout (sleep) after few seconds of inactivity.\nDoesn\'t apply when playing a song.', function(value:Bool):Void {
-        Preferences.screenTimeout = value;
-    }, Preferences.screenTimeout);
-    #end
   }
 
   override function update(elapsed:Float):Void
