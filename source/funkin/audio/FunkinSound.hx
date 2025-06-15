c0e0523651e8aaaae2a0eed6d5fef6c5ef1b7315
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-06-14 23:33:36 -0300

    fix: Clear waveform data when destroying audio
    This fixes an issue where recycled sounds would use the previous sound's waveform data.

--- source/funkin/audio/FunkinSound.hx
+++ source/funkin/audio/FunkinSound.hx
@@ -548,6 +548,7 @@ class FunkinSound extends FlxSound implements ICloneable<FunkinSound>
     }
     FlxTween.cancelTweensOf(this);
     this._label = 'unknown';
+    this._waveformData = null;
   }
 
   @:access(openfl.media.Sound)
