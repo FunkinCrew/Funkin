2ea9d078cf93c5e9fa59ee46d7c307bccb8c5e41
Lasercar <64717068+Lasercar@users.noreply.github.com>
2025-06-16 21:00:02 +1000

    There's a reason it's called the MAIN menu

--- source/funkin/ui/debug/stageeditor/StageEditorState.hx
+++ source/funkin/ui/debug/stageeditor/StageEditorState.hx
@@ -1270,7 +1270,7 @@ class StageEditorState extends UIState
         CrashHandler.criticalErrorSignal.remove(autosavePerCrash);
 
         Cursor.hide();
-        FlxG.switchState(() -> new DebugMenuSubState());
+        FlxG.switchState(() -> new MainMenuState());
         FlxG.sound.music.stop();
 
       case "switch mode":
