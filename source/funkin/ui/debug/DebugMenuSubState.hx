f9c1f7a5f7d3906155c2004ad2ec1d02556b9730
Kolo <67389779+JustKolosaki@users.noreply.github.com>
2025-06-19 09:52:51 +0200

    clear up styleSheet be4 entering

--- source/funkin/ui/debug/DebugMenuSubState.hx
+++ source/funkin/ui/debug/DebugMenuSubState.hx
@@ -71,6 +71,9 @@ class DebugMenuSubState extends MusicBeatSubState
     #end
     onMenuChange(items.members[0]);
     FlxG.camera.focusOn(new FlxPoint(camFocusPoint.x, camFocusPoint.y + 500));
+
+    // Remove the "user" stylesheet to prevent components using incorrect style data when entering an editor.
+    haxe.ui.Toolkit.styleSheet.clear("user");
   }
 
   function onMenuChange(selected:TextMenuItem)
