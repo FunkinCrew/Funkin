d4c7339373a57dcdf3e71c9fe7bd70ef8221ec75
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-06-27 22:46:00 -0500

    allow `TouchUtil` functions to be used outside of `FEATURE_TOUCH_CONTROLS`

--- source/funkin/ui/charSelect/CharSelectSubState.hx
+++ source/funkin/ui/charSelect/CharSelectSubState.hx
@@ -786,12 +786,14 @@ class CharSelectSubState extends MusicBeatSubState
 
     mobileAccept = false;
 
-    if (controls.UI_UP_R || controls.UI_DOWN_R || controls.UI_LEFT_R || controls.UI_RIGHT_R || TouchUtil.justReleased) selectSound.pitch = 1;
+    if (controls.UI_UP_R || controls.UI_DOWN_R || controls.UI_LEFT_R || controls.UI_RIGHT_R #if FEATURE_TOUCH_CONTROLS || TouchUtil.justReleased #end)
+      selectSound.pitch = 1;
 
     syncAudio(elapsed);
 
     if (allowInput && !pressedSelect)
     {
+      #if FEATURE_TOUCH_CONTROLS
       if (TouchUtil.pressed || TouchUtil.justReleased)
       {
         for (i => hitbox in grpHitboxes.members)
@@ -822,6 +824,7 @@ class CharSelectSubState extends MusicBeatSubState
       {
         mobileAccept = true;
       }
+      #end
 
       //
       if (controls.UI_UP) holdTmrUp += elapsed;
@@ -913,7 +916,7 @@ class CharSelectSubState extends MusicBeatSubState
     {
       curChar = availableChars.get(getCurrentSelected());
 
-      if (allowInput && pressedSelect && (controls.BACK || (mobileDeny && TouchUtil.justReleased)))
+      if (allowInput && pressedSelect && (controls.BACK #if FEATURE_TOUCH_CONTROLS || (mobileDeny && TouchUtil.justReleased) #end))
       {
         mobileDeny = false;
         cursorConfirmed.visible = false;
@@ -969,7 +972,9 @@ class CharSelectSubState extends MusicBeatSubState
           goToFreeplay();
         });
       }
+      #if FEATURE_TOUCH_CONTROLS
       else if (pressedSelect && TouchUtil.justReleased) mobileDeny = true;
+      #end
 
       mobileAccept = false;
     }
