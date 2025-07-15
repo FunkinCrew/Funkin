f245365ed387b8d7b31afa9bae7f03b0f62869c2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-07-16 00:22:32 +0300

    Merge pull request #1413 from FunkinCrew/mob-320/controls-menu-back-button
    [MOB-320] Add back button to controls menu.

--- source/funkin/ui/options/ControlsMenu.hx
+++ source/funkin/ui/options/ControlsMenu.hx
@@ -14,6 +14,9 @@ import funkin.ui.AtlasText;
 import funkin.ui.MenuList.MenuTypedList;
 import funkin.ui.TextMenuList;
 import funkin.ui.Page;
+#if FEATURE_TOUCH_CONTROLS
+import funkin.mobile.ui.FunkinBackButton;
+#end
 
 class ControlsMenu extends Page<OptionsState.OptionsMenuPageName>
 {
@@ -191,6 +194,20 @@ class ControlsMenu extends Page<OptionsState.OptionsMenuPageName>
     popup.back.scrollFactor.set(0, 0);
     popup.exists = false;
     add(popup);
+
+    #if FEATURE_TOUCH_CONTROLS
+    var backButton:FunkinBackButton = new FunkinBackButton(FlxG.width - 230, FlxG.height - 200, function():Void {
+      if (controlGrid.enabled && deviceList != null && deviceListSelected == false)
+      {
+        goToDeviceList();
+      }
+      else if (canExit)
+      {
+        exit();
+      }
+    }, 1.0);
+    add(backButton);
+    #end
   }
 
   function createItem(x = 0.0, y = 0.0, control:Control, index:Int)
