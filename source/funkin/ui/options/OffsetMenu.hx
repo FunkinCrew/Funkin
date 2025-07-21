2cc43ee1f61b8c8e813c16687aca44f1d1a05055
kade-github <26305836+Kade-github@users.noreply.github.com>
2025-07-21 11:32:38 -0700

    text fix for my mobile fellas

--- source/funkin/ui/options/OffsetMenu.hx
+++ source/funkin/ui/options/OffsetMenu.hx
@@ -353,7 +353,7 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
       jumpInText.text = 'Hit the notes as they come in!';
       #if mobile
       if (OptionsState.instance.hitbox != null) OptionsState.instance.hitbox.visible = true;
-      if (Preferences.controlsScheme == FunkinHitboxControlSchemes.Arrows && !ControlsHandler.usingExternalInputDevice)
+      if (!ControlsHandler.usingExternalInputDevice)
       {
         final amplification:Float = (FlxG.width / FlxG.height) / (FlxG.initialWidth / FlxG.initialHeight);
         final playerStrumlineScale:Float = ((FlxG.height / FlxG.width) * 1.95) * amplification;
@@ -391,7 +391,7 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
       jumpInText.y = testStrumline.y + 175;
 
       #if mobile
-      if (Preferences.controlsScheme != FunkinHitboxControlSchemes.Arrows || ControlsHandler.usingExternalInputDevice)
+      if (ControlsHandler.usingExternalInputDevice)
       {
       #end
         testStrumline.y = Preferences.downscroll ? FlxG.height - (testStrumline.height + 45) - Constants.STRUMLINE_Y_OFFSET : (testStrumline.height / 2)
@@ -399,6 +399,10 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
         if (Preferences.downscroll) jumpInText.y = testStrumline.y - 175;
         testStrumline.isDownscroll = Preferences.downscroll;
       #if mobile
+      }
+      else
+      {
+        jumpInText.y = testStrumline.y - 175;
       }
       #end
     });
