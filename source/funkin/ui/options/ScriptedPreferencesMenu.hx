19328abb9e1f433c50352af85db2f0255da7ccef
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-08-08 21:16:20 -0500

    and then there were 7

--- source/funkin/ui/options/ScriptedPreferencesMenu.hx
+++
@@ -1,8 +0,0 @@
-package funkin.ui.options;
-
-/**
- * A script that can be tied to PreferencesMenu.
- * Create a scripted class that extends PreferencesMenu to use this.
- */
-@:hscriptClass
-class ScriptedPreferencesMenu extends funkin.ui.options.PreferencesMenu implements polymod.hscript.HScriptedClass {}
