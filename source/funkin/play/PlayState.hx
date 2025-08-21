a2e92778513e8e5ae31aa583232f3a1d2dd44ad7
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-08-21 16:23:11 -0300

    the swappening part 2

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -1941,7 +1941,7 @@ class PlayState extends MusicBeatSubState
     else
     {
       // lolol
-      funkin.util.WindowUtil.showError('Unable to load stage ${id}, is its data corrupted?.', 'Stage Error');
+      funkin.util.WindowUtil.showError('Stage Error', 'Unable to load stage $id, is its data corrupted?.');
     }
   }
 
