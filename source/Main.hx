704f38847c6d8295e7562ad926fd16fa313710a4
PurSnake <mikesaygasamp@mail.ru>
2025-08-31 13:01:35 +0300

    fuck

--- source/Main.hx
+++ source/Main.hx
@@ -20,17 +20,6 @@ import funkin.util.WindowUtil;
 
 /**
  * The main class which initializes HaxeFlixel and starts the game in its initial state.
- */ /*
-  class Main extends Sprite
-  {
-  var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
-  var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-  var initialState:Class<FlxState> = funkin.InitState; // The FlxState the game starts with.
-  var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-  var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
-  var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-  // You can pretty much ignore everything from here on - your code should go in your states.
-  }
  */
 class Main extends flixel.FlxGame
 {
