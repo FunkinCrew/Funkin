1b9f5c62fb8915cce54cafa347408d55aa6f5915
kade-github <26305836+Kade-github@users.noreply.github.com>
2025-09-06 09:37:13 -0700

    Fix a bug with Lag Adjustment strumline

--- source/funkin/ui/options/OffsetMenu.hx
+++ source/funkin/ui/options/OffsetMenu.hx
@@ -394,7 +394,8 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
       if (ControlsHandler.usingExternalInputDevice)
       {
       #end
-        testStrumline.y = Preferences.downscroll ? FlxG.height - (testStrumline.height + 45) - Constants.STRUMLINE_Y_OFFSET : (testStrumline.height / 2)
+        var height = testStrumline.strumlineNotes.members[0].height;
+        testStrumline.y = Preferences.downscroll ? FlxG.height - (height + 45) - Constants.STRUMLINE_Y_OFFSET : (height / 2)
         - Constants.STRUMLINE_Y_OFFSET;
         if (Preferences.downscroll) jumpInText.y = FlxG.height - 425;
         testStrumline.isDownscroll = Preferences.downscroll;
