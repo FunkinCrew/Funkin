7ff07534726ae73b2ec95e8cddc3bfe56ce19b89
Abnormal <86753001+AbnormalPoof@users.noreply.github.com>
2025-09-08 21:24:29 -0500

    scripted freeplay and popups shot dead on august 32nd, 2031 with no traces found

--- source/funkin/play/components/ScriptedPopUpStuff.hx
+++
@@ -1,8 +0,0 @@
-package funkin.play.components;
-
-/**
- * A script that can be tied to PopUpStuff.
- * Create a scripted class that extends PopUpStuff to use this.
- */
-@:hscriptClass
-class ScriptedPopUpStuff extends funkin.play.components.PopUpStuff implements polymod.hscript.HScriptedClass {}
--- source/funkin/ui/freeplay/ScriptedFreeplayState.hx
+++
@@ -1,8 +0,0 @@
-package funkin.ui.freeplay;
-
-/**
- * A script that can be tied to FreeplayState.
- * Create a scripted class that extends FreeplayState to use this.
- */
-@:hscriptClass
-class ScriptedFreeplayState extends funkin.ui.freeplay.FreeplayState implements polymod.hscript.HScriptedClass {}
