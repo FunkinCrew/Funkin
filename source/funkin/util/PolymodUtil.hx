13d1bfa53f1eae53360c5047e001b71068a9b305
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-09-30 19:24:46 +0200

    Added 3 more util functions to PolymodUtil.hx

--- source/funkin/util/PolymodUtil.hx
+++ source/funkin/util/PolymodUtil.hx
@@ -1,8 +1,10 @@
 package funkin.util;
 
+import hscript.Printer;
 import polymod.hscript._internal.PolymodClassDeclEx;
 import polymod.hscript._internal.PolymodEnumDeclEx;
 import polymod.hscript._internal.PolymodInterpEx;
+import polymod.hscript._internal.PolymodScriptClass;
 
 /**
  * Utility class for accessing the AST descriptors and other information from scripted classes and enums loaded through Polymod.
@@ -48,5 +50,36 @@ class PolymodUtil {
   public static function getScriptedClass(key:String):PolymodClassDeclEx
   {
     return PolymodInterpEx.findScriptClassDescriptor(key);
+  }
+
+  /**
+   * Returns a list of the names of all currently loaded scripted classes.
+   */
+  public static function listScriptClassesNames():Array<String>
+  {
+    return PolymodScriptClass.listScriptClasses();
+  }
+
+  /**
+   * Returns a list of all currently loaded scripted classes that extend the specified class.
+   *
+   * @param clazz The parent class that the returned scripted classes should extend.
+   * @return An array containing all fully qualified names of all scripted classes that are subclasses of the provided class.
+   */
+  public static function listScriptedClassesExtendingClass(clazz:Class<Dynamic>):Array<String>
+  {
+    @:privateAccess
+    return PolymodScriptClass.listScriptClassesExtendingClass(clazz);
+  }
+
+  /**
+   * Returns a new HScript Printer instance.
+   *
+   * Can be used to convert scripted class expressions into a readable string representation,
+   * which is useful for debugging or inspecting scripted classes.
+   */
+  public static function getPrinter():Printer
+  {
+    return new Printer();
   }
 }
