f5fc096da68388b9641e8aff8ca21c2cdfd86173
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-10-05 23:03:12 +0200

    Forgor to change in haxedocs to specify that Hot Reload config in Controls

--- source/funkin/util/plugins/ReloadAssetsDebugPlugin.hx
+++ source/funkin/util/plugins/ReloadAssetsDebugPlugin.hx
@@ -10,7 +10,7 @@ import funkin.external.android.CallbackUtil;
 #end
 
 /**
- * A plugin which adds functionality to press `F5` to reload all game assets, then reload the current state.
+ * A plugin which adds functionality to press `F5` (configurable in Controls) to reload all game assets, then reload the current state.
  * This is useful for hot reloading assets during development.
  */
 @:nullSafety
