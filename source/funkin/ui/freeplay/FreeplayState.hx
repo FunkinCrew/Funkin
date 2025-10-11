95af46cbf72291d9d3226df91ed17f1aa3d3839f
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-10-11 14:29:32 +0200

    Double pressing not allowed, one press at a time

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -1827,9 +1827,7 @@ class FreeplayState extends MusicBeatSubState
       dj?.onPlayerAction(); // dj?.resetAFKTimer();
       changeDiff(-1);
       generateSongList(currentFilter, true, false);
-    }
-
-    if (rightPressed)
+    } else if (rightPressed)
     {
       dj?.onPlayerAction(); // dj?.resetAFKTimer();
       changeDiff(1);
