3384ab9adc756079a1b9a69322780b18e2671253
EliteMasterEric <ericmyllyoja@gmail.com>
2025-10-30 13:23:44 -0400

    remove some commmented out code

--- source/funkin/ui/debug/WaveformTestState.hx
+++ source/funkin/ui/debug/WaveformTestState.hx
@@ -43,26 +43,9 @@ class WaveformTestState extends MusicBeatState
     setPolygonVertices(vertexCount);
     add(polygonSprite);
 
-    // waveformSprite = WaveformSprite.buildFromWaveformData(waveformData, HORIZONTAL, FlxColor.fromString("#ADD8E6"));
-    // waveformSprite.duration = 5.0 * 160;
-    // waveformSprite.width = FlxG.width * 160;
-    // waveformSprite.height = FlxG.height; // / 2;
-    // waveformSprite.amplitude = 2.0;
-    // waveformSprite.minWaveformSize = 25;
-    // waveformSprite.clipRect = new FlxRect(0, 0, FlxG.width, FlxG.height);
-    // add(waveformSprite);
-    //
-    // waveformSprite2 = WaveformSprite.buildFromWaveformData(waveformData2, HORIZONTAL, FlxColor.fromString("#FF0000"), 5.0);
-    // waveformSprite2.width = FlxG.width;
-    // waveformSprite2.height = FlxG.height / 2;
-    // waveformSprite2.y = FlxG.height / 2;
-    // add(waveformSprite2);
-
     timeMarker = new FlxSprite(0, FlxG.height * 1 / 6);
     timeMarker.makeGraphic(1, Std.int(FlxG.height * 2 / 3), FlxColor.RED);
     add(timeMarker);
-
-    // drawWaveform(time, duration);
   }
 
   override public function update(elapsed:Float):Void
