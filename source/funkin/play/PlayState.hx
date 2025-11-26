c969393c9d338e6332c88f32498e300ce63cd42a
VirtuGuy <Wyatt2010shaw@gmail.com>
2025-11-26 07:41:57 -0500

    Fix ghost tapping behavior when unpausing

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2679,8 +2679,6 @@ class PlayState extends MusicBeatSubState
      */
   function onKeyRelease(event:PreciseInputEvent):Void
   {
-    if (isGamePaused) return;
-
     // Do the minimal possible work here.
     inputReleaseQueue.push(event);
   }
