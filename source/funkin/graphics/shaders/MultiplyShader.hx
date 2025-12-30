b87f23c7a246aeb519dffb3dc009832abbdfc6ed
Toadette8394 <toadette8394@gmail.com>
2025-12-30 17:40:54 -0500

    Improve Multiply Shader
    Rewrites MultiplyShader.hx and adds a function to allow for it to apply a multiply blending mode color over an attached sprite

--- source/funkin/graphics/shaders/MultiplyShader.hx
+++ source/funkin/graphics/shaders/MultiplyShader.hx
@@ -1,31 +1,31 @@
 package funkin.graphics.shaders;
 
 import flixel.system.FlxAssets.FlxShader;
+import flixel.util.FlxColor;
 
 class MultiplyShader extends FlxShader
 {
-  @:glFragmentSource('
-		#pragma header
-        uniform sampler2D funnyImage;
-		uniform vec4 uBlendColor;
+    @:glFragmentSource('
+        #pragma header
+        uniform vec4 uBlendColor;
 
-		vec4 blendMultiply(vec4 base, vec4 blend)
-		{
-			return base * blend;
-		}
+        void main()
+        {
+            vec4 base = texture2D(bitmap, openfl_TextureCoordv);
 
-		vec4 blendMultiply(vec4 base, vec4 blend, float opacity)
-		{
-			return (blendMultiply(base, blend) * opacity + base * (1.0 - opacity));
-		}
+            vec3 rgb = mix(base.rgb, base.rgb * uBlendColor.rgb, uBlendColor.a);
 
-		void main()
-		{
-			vec4 base = texture2D(bitmap, openfl_TextureCoordv);
-			gl_FragColor = blendMultiply(base, uBlendColor, uBlendColor.a);
-		}')
-  public function new()
-  {
-    super();
-  }
+            gl_FragColor = vec4(rgb, base.a);
+        }')
+
+      public function new()
+      {
+        super();
+      }
+
+    public function set_blendColor(color:FlxColor):FlxColor
+    {
+        uBlendColor.value = [color.red / 255, color.green / 255, color.blue / 255, color.alpha / 255];
+        return color;
+    }
 }
