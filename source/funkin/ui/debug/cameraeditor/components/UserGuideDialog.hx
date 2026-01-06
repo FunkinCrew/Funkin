71972e09679bb98650ed8b6a114deb3c733960b6
EliteMasterEric <ericmyllyoja@gmail.com>
2026-01-06 16:40:55 -0500

    Launch the game with -DEYESOFGOD to open the camera editor

---
+++ source/funkin/ui/debug/cameraeditor/components/UserGuideDialog.hx
@@ -0,0 +1,10 @@
+package funkin.ui.debug.cameraeditor.components;
+
+#if FEATURE_CAMERA_EDITOR
+import haxe.ui.containers.dialogs.Dialog;
+
+@:build(haxe.ui.macros.ComponentMacros.build("assets/exclude/data/ui/camera-editor/dialogs/user-guide.xml"))
+class UserGuideDialog extends Dialog
+{
+}
+#end
