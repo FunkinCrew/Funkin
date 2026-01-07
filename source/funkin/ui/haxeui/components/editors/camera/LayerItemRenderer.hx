7ce70adfd95f4dc501d0c84287ac30a03854840a
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2026-01-06 19:42:30 -0500

    Add in simple color picker

--- source/funkin/ui/haxeui/components/editors/camera/LayerItemRenderer.hx
+++ source/funkin/ui/haxeui/components/editors/camera/LayerItemRenderer.hx
@@ -7,6 +7,7 @@ import haxe.ui.core.ItemRenderer;
   <item-renderer layout="horizontal" width="100%" height="40">
       <hbox>
         <textfield id="layerName" verticalAlign="center" horizontalAlign="left"/>
+        <color-picker-popup id="picker" horizontalAlign="right" styleName="no-controls" />
       </hbox>
       <vbox width="80%" height="100%" id="layerFrames">
           <label text="Layer stuff here" />
@@ -19,6 +20,7 @@ class LayerItemRenderer extends ItemRenderer
   {
     super();
   }
+
   // note: for some reason the SUBMIT UI event doesn't occur. HaxeUI related issue on the flixel backend perchance.
   // perhaps this has been fixed upstream?
   //
@@ -28,4 +30,21 @@ class LayerItemRenderer extends ItemRenderer
   //   trace("Submit text" + e.value);
   //   layerName.focus = false;
   // }
+
+  override function onDataChanged(data:Dynamic):Void
+  {
+    super.onDataChanged(data);
+
+    // Restore color from data if it exists
+    // if (data != null && data.color != null) layerFrames.backgroundColor = data.color;
+  }
+
+  @:bind(picker, UIEvent.CHANGE)
+  function onPickerChange(e:UIEvent):Void
+  {
+    layerFrames.backgroundColor = picker.selectedItem;
+
+    // Optionally save to data so it persists when scrolling (virtual lists)
+    // if (_data != null) _data.color = picker.selectedItem;
+  }
 }
