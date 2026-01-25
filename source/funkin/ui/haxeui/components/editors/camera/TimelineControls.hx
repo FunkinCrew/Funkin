cac6f0291533f9dbb2bcee174b44ebb7ed762611
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2026-01-25 18:19:16 -0500

    use DataBehaviour for songPosition and songLength to be accessed/updated easier by other haxeUI stuff

--- source/funkin/ui/haxeui/components/editors/camera/TimelineControls.hx
+++ source/funkin/ui/haxeui/components/editors/camera/TimelineControls.hx
@@ -2,6 +2,7 @@ package funkin.ui.haxeui.components.editors.camera;
 
 import haxe.ui.containers.HBox;
 import flixel.util.FlxStringUtil;
+import haxe.ui.behaviours.DataBehaviour;
 
 @:xml('
 <hbox width="100%" backgroundColor="#222222">
@@ -14,28 +15,55 @@ import flixel.util.FlxStringUtil;
 ')
 class TimelineControls extends HBox
 {
-  var songLength:Float;
+  /**
+   * Current length of our song/audio, in milliseconds
+   */
+  @:clonable @:behaviour(SongLengthBehaviour, 0) public var songLength:Float;
+
+  /**
+   * The current position in the song in milliseconds
+   */
+  @:cloneable @:behaviour(SongPositionBehaviour, 0) public var songPosition:Float;
+
+  @:clonable @:value(songPosition) public var value:Dynamic;
 
   public function new()
   {
     super();
     songLength = 0;
   }
-  /**
-   * Updates the total length of the song in the timeline.
-   * @param newLength The new length in seconds.
-   */
-  public function updateLength(newLength:Float):Void
-  {
-    songLength = newLength;
-  }
+}
 
-  /**
-   * Updates the time label with the given time in seconds.
-   * @param time The time in seconds to display.
-   */
-  public function updateTime(time:Float):Void
+// Behaviours
+
+@:dox(hide) @:noCompletion
+private class SongPositionBehaviour extends DataBehaviour
+{
+  override public function validateData():Void
   {
-    lblTime.text = FlxStringUtil.formatTime(time, true) + "/" + FlxStringUtil.formatTime(songLength, true);
+    var timelineControls:TimelineControls = cast(_component, TimelineControls);
+
+    // clamp to song length
+    var clampedValue:Float = _value.toFloat().clamp(0, timelineControls.songLength);
+    timelineControls.songPosition = clampedValue;
+    _value = clampedValue;
+
+    // Convert ms to seconds for display
+    timelineControls.lblTime.text = FlxStringUtil.formatTime(timelineControls.songPosition / 1000, true) + "/" + FlxStringUtil.formatTime(timelineControls.songLength / 1000, true);
+  }
+}
+
+@:dox(hide) @:noCompletion
+private class SongLengthBehaviour extends DataBehaviour
+{
+  override public function validateData():Void
+  {
+    var timelineControls:TimelineControls = cast(_component, TimelineControls);
+
+    // clamp songPosition incase we updated our songLength to be shorter than songPosition
+    timelineControls.songPosition = _value.toFloat().clamp(0, timelineControls.songLength);
+
+    // Convert ms to seconds for display
+    timelineControls.lblTime.text = FlxStringUtil.formatTime(timelineControls.songPosition / 1000, true) + "/" + FlxStringUtil.formatTime(timelineControls.songLength / 1000, true);
   }
 }
