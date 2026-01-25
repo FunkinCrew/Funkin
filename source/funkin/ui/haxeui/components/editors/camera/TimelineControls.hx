6b83fd4304f7274223afbd0d8660c1a001750667
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2026-01-25 18:19:16 -0500

    small random styling for TimelineControls

--- source/funkin/ui/haxeui/components/editors/camera/TimelineControls.hx
+++ source/funkin/ui/haxeui/components/editors/camera/TimelineControls.hx
@@ -4,11 +4,11 @@ import haxe.ui.containers.HBox;
 import flixel.util.FlxStringUtil;
 
 @:xml('
-<hbox width="100%">
+<hbox width="100%" backgroundColor="#222222">
   <label text="Layer Controls!" />
   <button id="btnRemoveLayer" text="Remove Layer" />
   <button id="btnAddLayer" text="Add Layer" />
-  <button text="Play/Pause" toggle="true" />
+  <button id="btnTogglePlayback" text="Play/Pause" allowFocus="false" />
   <label id="lblTime" text="0:00.00/0:00.00" />
 </hbox>
 ')
