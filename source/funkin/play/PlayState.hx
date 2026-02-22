ea742a41879703f0bb8683754c7ebeb399146cff
blas <55158797+gamerbross@users.noreply.github.com>
2026-02-22 18:17:08 +0100

    Changes to `SortUtil` and also sort notes by length

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -1873,7 +1873,7 @@ class PlayState extends MusicBeatSubState
     if (generatedMusic)
     {
       // TODO: Sort more efficiently, or less often, to improve performance.
-      // activeNotes.sort(SortUtil.byStrumtime, FlxSort.DESCENDING);
+      // activeNotes.sort(SortUtil.noteSpriteByStrumtime, FlxSort.DESCENDING);
     }
 
     if (FlxG.sound.music != null)
