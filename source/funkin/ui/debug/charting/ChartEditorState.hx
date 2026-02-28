6cd87e75af0b0008b6c4d5c35375bbc8800de6cb
AnimatingLegend <thesfmgod@gmail.com>
2026-02-28 00:34:32 -0500

    fix: chart editor playback-speed  rounding upon reload

--- source/funkin/ui/debug/charting/ChartEditorState.hx
+++ source/funkin/ui/debug/charting/ChartEditorState.hx
@@ -2561,7 +2561,7 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
     menubarItemVolumeInstrumental.value = Std.int(save.chartEditorInstVolume.value * 100);
     menubarItemVolumeVocalsPlayer.value = Std.int(save.chartEditorPlayerVoiceVolume.value * 100);
     menubarItemVolumeVocalsOpponent.value = Std.int(save.chartEditorOpponentVoiceVolume.value * 100);
-    menubarItemPlaybackSpeed.value = Std.int(save.chartEditorPlaybackSpeed.value * 100.0);
+    menubarItemPlaybackSpeed.value = Math.round(save.chartEditorPlaybackSpeed.value * 100.0);
   }
 
   public function writePreferences(hasBackup:Bool):Void
