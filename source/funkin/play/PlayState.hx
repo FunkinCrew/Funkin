edf12f5daf0a845094486f52e3d70f400f34626e
ImVeryBad <nightbear843@yahoo.com>
2026-03-25 22:01:46 +0100

    Fix strumline background overlapping other UI elements.

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2254,6 +2254,9 @@ class PlayState extends MusicBeatSubState
     opponentStrumline.zIndex = 1000;
     opponentStrumline.cameras = [camHUD];
 
+    playerStrumline.initBackground(this);
+    opponentStrumline.initBackground(this);
+
     #if mobile
     if (Preferences.controlsScheme == FunkinHitboxControlSchemes.Arrows && !ControlsHandler.hasExternalInputDevice)
     {
