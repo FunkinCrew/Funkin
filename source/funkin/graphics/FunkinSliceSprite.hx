8b3d29a8f6701ab058789eaa12de4e8c23a95d37
fabs <flamingkitty24@gmail.com>
2026-03-25 04:21:48 +0000

    make a version of FlxSliceSprite that supports vcam points

---
+++ source/funkin/graphics/FunkinSliceSprite.hx
@@ -0,0 +1,31 @@
+package funkin.graphics;
+
+import flixel.math.FlxPoint;
+import flixel.FlxCamera;
+import flixel.addons.display.FlxSliceSprite;
+
+/**
+ * An FlxSprite that inherits the vcam positioning from FunkinSprite.
+ */
+@:nullSafety
+class FunkinSliceSprite extends FlxSliceSprite
+{
+  public var vcamPoint:Null<FlxPoint> = null;
+
+  /**
+   * Gets the screen position of the sprite, taking into account the camera scroll and the `vcamPoint` if it exists.
+   * @param result An optional `FlxPoint` to store the result in. If null, a new `FlxPoint` will be created.
+   * @param camera The camera to calculate the screen position relative to. If null, the default camera will be used.
+   * @return The screen position of the sprite.
+   */
+  override function getScreenPosition(?result:FlxPoint, ?camera:FlxCamera):FlxPoint
+  {
+    if (result == null) result = FlxPoint.get();
+    if (camera == null) camera = getDefaultCamera();
+    result.set(x, y);
+
+    if (vcamPoint != null) return result.subtract((vcamPoint.x * scrollFactor.x) + camera.scroll.x, (vcamPoint.y * scrollFactor.y) + camera.scroll.y);
+
+    return result.subtract(camera.scroll.x * scrollFactor.x, camera.scroll.y * scrollFactor.y);
+  }
+}
