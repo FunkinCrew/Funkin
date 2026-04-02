816b29b9723cdc6e9830e937a9c24042af64e9a9
fabs <flamingkitty24@gmail.com>
2026-04-02 10:31:10 +0100

    add triangle image to block

--- source/funkin/ui/haxeui/components/editors/timeline/TimelineEventBlock.hx
+++ source/funkin/ui/haxeui/components/editors/timeline/TimelineEventBlock.hx
@@ -1,6 +1,7 @@
 package funkin.ui.haxeui.components.editors.timeline;
 
 #if FEATURE_CAMERA_EDITOR
+import funkin.play.event.SongEvent;
 import funkin.data.song.SongData.SongEventData;
 import haxe.ui.behaviours.DefaultBehaviour;
 import haxe.ui.components.Image;
@@ -12,7 +13,8 @@ import flixel.util.FlxColor;
 class TimelineEventBlock extends Box
 {
   public static inline var BLOCK_HEIGHT:Int = 42;
-  static inline var CORNER_RADIUS:Int = 3;
+  static inline var CORNER_RADIUS:Int = 5;
+  public static inline var TRIANGLE_OPACITY:Float = 0.3;
   public static inline var EDGE_DRAG_ZONE:Float = 6.0;
   public static final ICON_RESOURCES:Map<String, String> = [
     "FocusCamera" => "assets/ui/editors/camera-editor/event-icons/focus_event.png",
@@ -27,6 +29,7 @@ class TimelineEventBlock extends Box
   public var blockTop:Float = 0;
   public var blockWidth:Float = 0;
   public var _cachedIcon:Image;
+  public var _cachedTriangle:Image;
 
   public function new()
   {
@@ -41,9 +44,24 @@ class TimelineEventBlock extends Box
     var borderColor = selected ? 0xFFFFFF : layerColor;
 
     customStyle.backgroundColor = bodyColor;
-    customStyle.borderColor = borderColor;
-    customStyle.borderSize = selected ? 2 : 1;
-    customStyle.borderRadius = CORNER_RADIUS;
+    customStyle.backgroundOpacity = selected ? 0.6 : 0.4;
+
+    this.borderColor = borderColor;
+    this.borderSize = selected ? 2 : 1;
+    this.borderRadius = isInstant() ? 0 : CORNER_RADIUS;
+
+    invalidateComponentStyle();
+  }
+
+  public function updateVisuals():Void
+  {
+    this.borderRadius = isInstant() ? 0 : CORNER_RADIUS;
+
+    if (_cachedTriangle != null)
+    {
+      _cachedTriangle.opacity = isInstant() ? TRIANGLE_OPACITY : 0;
+    }
+
     invalidateComponentStyle();
   }
 
@@ -58,6 +76,17 @@ class TimelineEventBlock extends Box
   {
     return ICON_RESOURCES.get(eventKind);
   }
+
+  public function isInstant():Bool
+  {
+    var easeStr:String = eventData.getString('ease') ?? SongEvent.DEFAULT_EASE;
+    if (easeStr != null && (easeStr == 'CLASSIC' || easeStr == 'INSTANT'))
+    {
+      return true;
+    }
+
+    return false;
+  }
 }
 
 @:dox(hide) @:noCompletion
@@ -65,6 +94,14 @@ private class TimelineBlockBuilder extends CompositeBuilder
 {
   override public function create():Void
   {
+    var triangle = new Image();
+    triangle.id = "block-instantTriangle";
+    triangle.addClass("timeline-block-instant-triangle");
+    triangle.customStyle.pointerEvents = "none";
+    triangle.resource = 'assets/ui/editors/camera-editor/triangle.png';
+    triangle.opacity = 0.3;
+    _component.addComponent(triangle);
+
     var icon = new Image();
     icon.id = "block-icon";
     icon.addClass("timeline-block-icon");
