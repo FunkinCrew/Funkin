9a13b91f85373722203af135f798e502a9366e4c
MoonDroid <81515012+moondroidcoder@users.noreply.github.com>
2026-04-05 21:04:54 +0700

    Call the font directly in iOS credits
    It fixes compiling

--- source/funkin/ui/credits/CreditsState.hx
+++ source/funkin/ui/credits/CreditsState.hx
@@ -97,7 +97,7 @@ class CreditsState extends MusicBeatState
 
     #if ios
     var fix = new FlxText();
-    fix.font = CREDITS_FONT;
+    fix.font = funkin.assets.Paths.font('ui/fonts/Inconsolata Black');
     fix.draw();
     #end
 
