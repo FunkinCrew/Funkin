0e3d44f369e8cd2465ea6f4b4702745d02783843
EliteMasterEric <ericmyllyoja@gmail.com>
2026-04-07 00:07:08 -0400

    Rework how the Chart and Camera editors save and load vocals, to use the vocal IDs from the metadata more closely.

--- source/funkin/ui/debug/charting/ChartEditorState.hx
+++ source/funkin/ui/debug/charting/ChartEditorState.hx
@@ -6733,7 +6733,7 @@ class ChartEditorState extends UIState // UIState derives from MusicBeatState
   function switchToCurrentInstrumental():Void
   {
     // ChartEditorAudioHandler
-    this.switchToInstrumental(currentInstrumentalId, currentSongMetadata.playData.characters.player, currentSongMetadata.playData.characters.opponent);
+    this.switchToInstrumental(currentInstrumentalId);
   }
 
   public function updateGridHeight():Void
