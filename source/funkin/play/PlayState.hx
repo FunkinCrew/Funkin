d49f67478aa78588260d88d52ee3b9f172a8459f
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2026-04-13 23:00:29 -0300

    fix: Improve null-safety for character handling in `PlayState`

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -1256,9 +1256,10 @@ class PlayState extends MusicBeatSubState
       camHUD.zoom = FlxMath.lerp(defaultHUDCameraZoom, camHUD.zoom, Math.pow(decayRate, dt));
     }
 
-    if (currentStage != null && currentStage.getBoyfriend() != null)
+    var bf:Null<BaseCharacter> = currentStage?.getBoyfriend();
+    if (bf != null)
     {
-      FlxG.watch.addQuick('bfAnim', currentStage.getBoyfriend().getCurrentAnimation());
+      FlxG.watch.addQuick('bfAnim', bf.getCurrentAnimation());
     }
     FlxG.watch.addQuick('health', health);
     FlxG.watch.addQuick('cameraBopIntensity', cameraBopIntensity);
@@ -2871,9 +2872,10 @@ class PlayState extends MusicBeatSubState
       if (holdNote.hitNote && !holdNote.missedNote && holdNote.sustainLength > 0)
       {
         // Make sure the opponent keeps singing while the note is held.
-        if (currentStage != null && currentStage.getDad() != null && currentStage.getDad().isSinging())
+        var dad:Null<BaseCharacter> = currentStage?.getDad();
+        if (dad != null && dad.isSinging())
         {
-          currentStage.getDad().holdTimer = 0;
+          dad.holdTimer = 0;
         }
       }
 
@@ -2886,7 +2888,7 @@ class PlayState extends MusicBeatSubState
         {
           // We dropped a hold note.
           // Play miss animation, but don't penalize.
-          if (currentStage != null) currentStage.getOpponent().playSingAnimation(holdNote.noteData.getDirection(), true);
+          currentStage?.getOpponent()?.playSingAnimation(holdNote.noteData.getDirection(), true);
         }
       }
     }
@@ -2964,9 +2966,10 @@ class PlayState extends MusicBeatSubState
         }
 
         // Make sure the player keeps singing while the note is held by the bot.
-        if (isBotPlayMode && currentStage != null && currentStage.getBoyfriend() != null && currentStage.getBoyfriend().isSinging())
+        var bf:Null<BaseCharacter> = currentStage?.getBoyfriend();
+        if (isBotPlayMode && bf != null && bf.isSinging())
         {
-          currentStage.getBoyfriend().holdTimer = 0;
+          bf.holdTimer = 0;
         }
       }
 
--- source/funkin/play/stage/Stage.hx
+++ source/funkin/play/stage/Stage.hx
@@ -556,7 +556,7 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
   /**
    * Retrieves a given character from the stage.
    */
-  public function getCharacter(id:String):BaseCharacter
+  public function getCharacter(id:String):Null<BaseCharacter>
   {
     return this.characters.get(id);
   }
@@ -566,11 +566,11 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param pop If true, the character will be removed from the stage as well.
    * @return The Boyfriend character.
    */
-  public function getBoyfriend(pop:Bool = false):BaseCharacter
+  public function getBoyfriend(pop:Bool = false):Null<BaseCharacter>
   {
     if (pop)
     {
-      var boyfriend:BaseCharacter = getCharacter('bf');
+      var boyfriend:BaseCharacter = getCharacter('bf') ?? return null;
 
       // Remove the character from the stage.
       this.remove(boyfriend);
@@ -589,7 +589,7 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param pop If true, the character will be removed from the stage as well.
    * @return The player/Boyfriend character.
    */
-  public function getPlayer(pop:Bool = false):BaseCharacter
+  public function getPlayer(pop:Bool = false):Null<BaseCharacter>
   {
     return getBoyfriend(pop);
   }
@@ -599,11 +599,11 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param pop If true, the character will be removed from the stage as well.
    * @return The Girlfriend character.
    */
-  public function getGirlfriend(pop:Bool = false):BaseCharacter
+  public function getGirlfriend(pop:Bool = false):Null<BaseCharacter>
   {
     if (pop)
     {
-      var girlfriend:BaseCharacter = getCharacter('gf');
+      var girlfriend:BaseCharacter = getCharacter('gf') ?? return null;
 
       // Remove the character from the stage.
       this.remove(girlfriend);
@@ -622,11 +622,11 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param pop If true, the character will be removed from the stage as well.
    * @return The Dad character.
    */
-  public function getDad(pop:Bool = false):BaseCharacter
+  public function getDad(pop:Bool = false):Null<BaseCharacter>
   {
     if (pop)
     {
-      var dad:BaseCharacter = getCharacter('dad');
+      var dad:BaseCharacter = getCharacter('dad') ?? return null;
 
       // Remove the character from the stage.
       this.remove(dad);
@@ -645,7 +645,7 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param pop If true, the character will be removed from the stage as well.
    * @return The opponent character.
    */
-  public function getOpponent(pop:Bool = false):BaseCharacter
+  public function getOpponent(pop:Bool = false):Null<BaseCharacter>
   {
     return getDad(pop);
   }
@@ -655,7 +655,7 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    * @param name The name of the prop to retrieve.
    * @return The corresponding FlxSprite.
    */
-  public function getNamedProp(name:String):StageProp
+  public function getNamedProp(name:String):Null<StageProp>
   {
     return this.namedProps.get(name);
   }
@@ -740,7 +740,7 @@ class Stage extends FlxSpriteGroup implements IPlayStateScriptedClass implements
    */
   public function dispatchToCharacter(characterId:String, event:ScriptEvent):Void
   {
-    var character:BaseCharacter = getCharacter(characterId);
+    var character:Null<BaseCharacter> = getCharacter(characterId);
     if (character != null)
     {
       ScriptEventDispatcher.callEvent(character, event);
