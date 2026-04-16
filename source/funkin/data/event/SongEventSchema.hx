d700469e378255b3c458e27bd1c74ec8035007ce
Ahmed7p39 <pa549557@gmail.com>
2026-04-16 16:30:21 +0300

    Add collapsed field to SongEventSchema

--- source/funkin/data/event/SongEventSchema.hx
+++ source/funkin/data/event/SongEventSchema.hx
@@ -243,6 +243,11 @@ typedef SongEventSchemaField =
    */
   ?collapsible:Bool,
   /**
+   * Used for FRAME values.
+   * Whether to make the collapsible frame start collapsed.
+   */
+  ?collapsed:Bool,
+  /**
    * An optional default value for the field.
    */
   ?defaultValue:Dynamic,
