9c3f8005bc6cbbd38c97537c28e1a539cdf6477b
Kolo <67389779+KoloInDaCrib@users.noreply.github.com>
2026-04-25 11:50:03 +0200

    Remove the vcamPoint link when switching stages

--- source/funkin/ui/debug/cameraeditor/CameraEditorState.hx
+++ source/funkin/ui/debug/cameraeditor/CameraEditorState.hx
@@ -1005,6 +1005,7 @@ class CameraEditorState extends UIState implements ConsoleClass
 
     if (currentStage != null)
     {
+      currentStage.vcamPoint = null;
       ScriptEventDispatcher.callEvent(currentStage, new ScriptEvent(DESTROY, false));
       remove(currentStage);
       currentStage.kill();
