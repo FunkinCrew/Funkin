d37481292ce001e722f7ebeb0efcb9d66a7fe214
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2026-05-21 22:59:31 -0400

    Add Play Animation stuff to Add Event menu

--- source/funkin/ui/debug/cameraeditor/components/AddEventMenu.hx
+++ source/funkin/ui/debug/cameraeditor/components/AddEventMenu.hx
@@ -23,16 +23,24 @@ class AddEventMenu
   static final ICON_SIZE:Float = 16;
   static final ICON_PATH:String = 'assets/ui/editors/camera-editor/event-icons/';
   static final EVENT_ITEMS:Array<
-    {kind:String, label:String, icon:String}> = [
+    {kind:String, label:String, icon:String, submenu:String}> = [
     {
       kind: 'FocusCamera',
       label: 'Focus Camera',
-      icon: 'focus_event.png'
+      icon: 'focus_event.png',
+      submenu: 'Camera'
     },
     {
       kind: 'ZoomCamera',
       label: 'Zoom Camera',
-      icon: 'zoom_event.png'
+      icon: 'zoom_event.png',
+      submenu: 'Camera'
+    },
+    {
+      kind: 'PlayAnimation',
+      label: 'Play Animation',
+      icon: 'playanim_event.png',
+      submenu: 'Character'
     },
   ];
 
@@ -59,12 +67,21 @@ class AddEventMenu
     header.shortcutText = 'Shift+A';
     header.disabled = true;
 
-    var cameraSubmenu = new Menu();
-    cameraSubmenu.text = 'Camera';
-    cameraSubmenu.width = MENU_WIDTH;
+    var submenus:Map<String, Menu> = new Map();
+    var submenuOrder:Array<String> = [];
 
     for (info in EVENT_ITEMS)
     {
+      var submenu:Null<Menu> = submenus.get(info.submenu);
+      if (submenu == null)
+      {
+        submenu = new Menu();
+        submenu.text = info.submenu;
+        submenu.width = MENU_WIDTH;
+        submenus.set(info.submenu, submenu);
+        submenuOrder.push(info.submenu);
+      }
+
       var item = new MenuItem();
       item.text = info.label;
       item.id = info.kind;
@@ -77,14 +94,17 @@ class AddEventMenu
       icon.height = ICON_SIZE;
       item.addComponentAt(icon, 0);
 
-      cameraSubmenu.addComponent(item);
+      submenu.addComponent(item);
     }
 
     menu = new Menu();
     menu.width = MENU_WIDTH;
     menu.addComponent(header);
     menu.addComponent(new MenuSeparator());
-    menu.addComponent(cameraSubmenu);
+    for (submenuName in submenuOrder)
+    {
+      menu.addComponent(submenus.get(submenuName));
+    }
 
     menu.registerEvent(MenuEvent.MENU_SELECTED, onMenuSelected);
     menu.registerEvent(UIEvent.CLOSE, function(_)
