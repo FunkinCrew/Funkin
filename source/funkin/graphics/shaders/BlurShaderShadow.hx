0984cf90fbd295cb8bd422d726d8b3f9b113bb0d
fabs <flamingkitty24@gmail.com>
2026-06-02 02:42:50 +0100

    blur and drop shadow shaders

---
+++ source/funkin/graphics/shaders/BlurShaderShadow.hx
@@ -0,0 +1,41 @@
+package funkin.graphics.shaders;
+
+import flixel.system.FlxAssets.FlxShader;
+import openfl.display.BitmapData;
+import flixel.util.FlxColor;
+
+class BlurShaderShadow extends FlxShader
+{
+  @:glFragmentSource('
+        #pragma header
+
+// literally just offsets the image and colors it
+// used as the last step in the DropShadowLayer
+
+// x offset of drop shadow
+uniform float offsetX;
+// y offset of drop shadow
+uniform float offsetY;
+
+// color of shadow
+uniform vec4 shadowCol;
+
+void main() {
+
+  vec2 imageRatio = vec2(1.0 / openfl_TextureSize.x, 1.0 / openfl_TextureSize.y);
+
+  vec4 color = texture2D(bitmap, vec2(openfl_TextureCoordv.x - (offsetX*imageRatio.x),openfl_TextureCoordv.y - (offsetY*imageRatio.y)));
+
+	gl_FragColor = vec4(shadowCol * color.a);
+}
+
+    ')
+  public function new(_shadowColor:FlxColor = 0xFF000000, _distX:Float = 5, _distY:Float = 5)
+  {
+    super();
+
+    shadowCol.value = [_shadowColor.redFloat, _shadowColor.greenFloat, _shadowColor.blueFloat, _shadowColor.alphaFloat];
+    offsetX.value = [_distX];
+    offsetY.value = [_distY];
+  }
+}
---
+++ source/funkin/graphics/shaders/BlurShaderUp.hx
@@ -0,0 +1,49 @@
+package funkin.graphics.shaders;
+
+import flixel.system.FlxAssets.FlxShader;
+import openfl.display.BitmapData;
+
+class BlurShaderUp extends FlxShader
+{
+  @:glFragmentSource('
+        #pragma header
+precision highp float;
+
+// slightly edited Dual Kawase implementation taken from https://blog.frost.kiwi/dual-kawase/
+// this is the up-sample part of the effect.
+
+// blur strength (distance of samples)
+uniform float offset;
+// scale of current image
+uniform float scale;
+
+void main() {
+  vec2 uv = openfl_TextureCoordv / scale;
+  vec2 rcp = vec2(1.0 / (openfl_TextureSize.x/scale), 1.0 / (openfl_TextureSize.y/scale));
+
+	vec2 halfpixel = rcp * 0.5;
+	vec2 o = halfpixel * (offset / scale);
+
+	vec4 color = vec4(0.0);
+
+	color += flixel_texture2D(bitmap, uv + vec2(-o.x * 2.0, 0.0));
+	color += flixel_texture2D(bitmap, uv + vec2( o.x * 2.0, 0.0));
+	color += flixel_texture2D(bitmap, uv + vec2(0.0, -o.y * 2.0));
+	color += flixel_texture2D(bitmap, uv + vec2(0.0,  o.y * 2.0));
+
+	color += flixel_texture2D(bitmap, uv + vec2(-o.x,  o.y)) * 2.0;
+	color += flixel_texture2D(bitmap, uv + vec2( o.x,  o.y)) * 2.0;
+	color += flixel_texture2D(bitmap, uv + vec2(-o.x, -o.y)) * 2.0;
+	color += flixel_texture2D(bitmap, uv + vec2( o.x, -o.y)) * 2.0;
+
+	gl_FragColor = color / 12.0;
+}
+
+    ')
+  public function new(_scale:Float = 1, _offset:Float = 1)
+  {
+    super();
+    offset.value = [_offset];
+    scale.value = [_scale];
+  }
+}
