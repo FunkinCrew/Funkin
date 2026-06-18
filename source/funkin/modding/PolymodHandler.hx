4635fd8dea8c5ab4e52f29c8136afcd7cdf0f277
EliteMasterEric <ericmyllyoja@gmail.com>
2026-06-18 16:01:09 -0400

    Forcibly uncache all assets when hot reloading.

--- source/funkin/modding/PolymodHandler.hx
+++ source/funkin/modding/PolymodHandler.hx
@@ -643,6 +643,10 @@ class PolymodHandler
    */
   public static function forceReloadAssets():Void
   {
+    // We need to clear the cache, including persistent assets,
+    // because some persistent assets may have changed since they were cached.
+    funkin.assets.FunkinAssetCache.instance.clear();
+
     // Forcibly clear scripts so that scripts can be edited.
     ModuleHandler.clearModuleCache();
     Polymod.clearScripts();
