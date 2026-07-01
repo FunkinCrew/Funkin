bd09579893b84a98d243bced613641e2aaeba831
EliteMasterEric <ericmyllyoja@gmail.com>
2026-07-01 15:13:59 -0400

    Fix a bug where working file path wasn't being applied when loading charts from the Welcome dialog

--- source/funkin/ui/debug/cameraeditor/components/WelcomeDialog.hx
+++ source/funkin/ui/debug/cameraeditor/components/WelcomeDialog.hx
@@ -150,7 +150,7 @@ class WelcomeDialog extends Dialog
 
     try
     {
-      CameraEditorImportExportHandler.loadSongFromFNFCBytes(this.cameraEditorState, selectedFile.bytes);
+      CameraEditorImportExportHandler.loadSongFromFNFCBytes(this.cameraEditorState, selectedFile.bytes, selectedFile.fullPath);
       // If we failed, it'd throw.
       this.hideDialog(DialogButton.APPLY);
     }
