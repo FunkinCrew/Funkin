31a81eb5e5d811fd9f50c9fc759c12fc807e107e
AbnormalPoof <abnormalpoof@icloud.com>
2026-07-10 01:04:48 -0500

    Make `FunkinBufferSprite` scriptable if you're freaky like that

---
+++ source/funkin/graphics/framebuffer/ScriptedFunkinBufferSprite.hx
@@ -0,0 +1,10 @@
+package funkin.graphics.framebuffer;
+
+/**
+ * A script that can be tied to a FunkinBufferSprite.
+ * Create a scripted class that extends FunkinBufferSprite to use this.
+ */
+@:hscriptClass
+class ScriptedFunkinBufferSprite extends funkin.graphics.framebuffer.FunkinBufferSprite implements polymod.hscript.HScriptedClass
+{
+}
