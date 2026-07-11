d6db4811a2f053b3fa9b06261760c976f5f6a5d0
dankert <197002865+Dan-Kert@users.noreply.github.com>
2026-07-11 18:50:12 +0000

    Update install-deps.sh

--- .github/scripts/install-deps.sh
+++ .github/scripts/install-deps.sh
@@ -16,27 +16,25 @@ fi
 
 rm -rf .haxelib
 mkdir -p .haxelib
-haxelib setup .haxelib
 haxelib --never newrepo
+
+git config --global advice.detachedHead false
 
 echo "Installing FunkinCrew's patched haxelib & hmm..."
 haxelib --global git haxelib https://github.com/FunkinCrew/haxelib.git --quiet
 haxelib --global git hmm https://github.com/FunkinCrew/hmm.git --quiet
 
+echo "Reformatting haxelib repository for patched haxelib..."
+haxelib --always fixrepo
+
 echo "Installing dependencies via hmm (hmm.json)..."
-haxelib run hmm install -q
+haxelib --global run hmm install -q
 
 echo "Compiling hxcpp tools..."
 (
   cd .haxelib/hxcpp/git/tools/hxcpp
   haxe compile.hxml
 )
-
-echo "Setting up Lime..."
-haxelib run lime setup -y
-
-echo "Rebuilding Lime tools..."
-haxelib run lime rebuild tools -v
 
 if [ "$TARGET" = "android" ]; then
   mkdir -p "$HOME/.lime"
@@ -47,9 +45,4 @@ if [ "$TARGET" = "android" ]; then
   <set name="ANDROID_NDK_ROOT" value="$ANDROID_NDK_HOME" />
 </xml>
 EOF
-  echo "Rebuilding Lime Android ndll..."
-  haxelib run lime rebuild android -v
-else
-  echo "Rebuilding Lime $TARGET ndll..."
-  haxelib run lime rebuild "$TARGET" -v
 fi
