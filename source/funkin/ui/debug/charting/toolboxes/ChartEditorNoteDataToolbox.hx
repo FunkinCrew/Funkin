b42cd4b6e17ac9bbff9934a69b10c7fbb2e6f5e6
kiry vajiravuth <coolguy34393538@gmail.com>
2026-07-11 19:35:31 +0700

    charteditor wont crash now

--- source/funkin/ui/debug/charting/toolboxes/ChartEditorNoteDataToolbox.hx
+++ source/funkin/ui/debug/charting/toolboxes/ChartEditorNoteDataToolbox.hx
@@ -120,7 +120,7 @@ class ChartEditorNoteDataToolbox extends ChartEditorBaseToolbox
       var customKind:Null<String> = event?.target?.text;
       chartEditorState.noteKindToPlace = customKind;
 
-      if (toolboxNotesNoteKind.value.id != '~CUSTOM~') return;
+      if (toolboxNotesNoteKind.value == null || toolboxNotesNoteKind.value.id != '~CUSTOM~') return;
 
       if (!_initializing && chartEditorState.currentNoteSelection.length > 0)
       {
@@ -149,8 +149,9 @@ class ChartEditorNoteDataToolbox extends ChartEditorBaseToolbox
     toolboxNotesCustomKind.pauseEvent(UIEvent.CHANGE, true);
 
     toolboxNotesCustomKind.value = chartEditorState.noteKindToPlace;
-    toolboxNotesNoteKind.value = ChartEditorDropdowns.lookupNoteKind(chartEditorState.noteKindToPlace);
-    if (toolboxNotesNoteKind.value.id == '~CUSTOM~' && chartEditorState.noteKindToPlace != null)
+    var noteKindEntry = ChartEditorDropdowns.lookupNoteKind(chartEditorState.noteKindToPlace);
+    toolboxNotesNoteKind.value = noteKindEntry;
+    if (noteKindEntry.id == '~CUSTOM~' && chartEditorState.noteKindToPlace != null)
     {
       showCustom();
     }
