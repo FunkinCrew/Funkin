a18bab932f6074539ceb0b5cb2cf50c95a63cb7d
MAJigsaw77 <mihaialexandru427@yahoo.com>
2026-07-20 19:41:24 +0300

    Update `lime` and `openfl`

--- templates/haxe/ApplicationMain.hx
+++ templates/haxe/ApplicationMain.hx
@@ -24,7 +24,7 @@ using funkin.util.WindowUtil;
 @:access(lime.system.System)
 @:access(openfl.display.Stage)
 @:access(openfl.events.UncaughtErrorEvents)
-#if (static_link || ios || tvos)
+#if (static_link || ios)
 @:cppFileCode("\nextern \"C\" int zlib_register_prims ();\nextern \"C\" int lime_register_prims ();\n::foreach ndlls::::if (registerStatics)::extern \"C\" int ::nameSafe::_register_prims ();::end::::end::")
 #end
 class ApplicationMain
@@ -32,7 +32,7 @@ class ApplicationMain
   #if !macro
   public static function main():Void
   {
-    #if (static_link || ios || tvos)
+    #if (static_link || ios)
     untyped __cpp__("zlib_register_prims ()");
     untyped __cpp__("lime_register_prims ()");
     ::foreach ndlls::::if (registerStatics)::untyped __cpp__("::nameSafe::_register_prims ()");::end::::end::
