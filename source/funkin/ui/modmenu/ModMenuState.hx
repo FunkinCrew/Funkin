7550b5ea95daccf19e44af4818b2de67e7f33f12
ImMalloy <lefehanims@gmail.com>
2026-08-10 16:47:15 -0600

    Use the reset blink deadlines patch

--- source/funkin/ui/modmenu/ModMenuState.hx
+++ source/funkin/ui/modmenu/ModMenuState.hx
@@ -1248,6 +1248,13 @@ class ModMenuState extends MusicBeatState
     {
       blinkTimer += elapsed;
 
+      if (blinkTimer >= 100)
+      {
+        blinkTimer = 0;
+        bfBlink = 0;
+        gfBlink = 0;
+      }
+
       if (blinkTimer >= bfBlink && bf.animation.finished)
       {
         bfBlink = blinkTimer + Math.random() + (Math.random() * 6);
