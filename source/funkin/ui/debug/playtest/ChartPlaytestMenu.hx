f2fe17910976515ca67362959384ebd66dab0f74
Karim Akra <karimakra512@gmail.com>
2026-08-22 21:39:59 +0300

    playest menu camera reset

--- source/funkin/ui/debug/playtest/ChartPlaytestMenu.hx
+++ source/funkin/ui/debug/playtest/ChartPlaytestMenu.hx
@@ -39,14 +39,20 @@ class ChartPlaytestMenu extends MusicBeatState
 
     playtestCam = new FunkinCamera('playtestCam');
     playtestCam.bgColor = 0x0;
-    // playtestCam.alpha = 0;
-    FlxG.cameras.add(playtestCam, false);
+    cameras = [playtestCam];
+    FlxG.cameras.reset(playtestCam);
+
+    subStateClosed.add((_) ->
+    {
+      FlxG.cameras.reset(playtestCam = new FunkinCamera('playtestCam'));
+      playtestCam.bgColor = 0x0;
+      cameras = [playtestCam];
+    });
 
     var blackBG:FunkinSprite = new FunkinSprite(0, 0);
     blackBG.makeSolidColor(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
     blackBG.scrollFactor.set();
     blackBG.alpha = 0.4;
-    blackBG.cameras = [playtestCam];
     add(blackBG);
 
     this.filePath = filePath;
@@ -54,7 +60,6 @@ class ChartPlaytestMenu extends MusicBeatState
     songName = new FlxText(0, FlxG.height * 0.1, 0, 'Loaded Song: ${targetSong.songName}',
       30).setFormat(Paths.font('vcr.ttf'), 50, FlxColor.WHITE, FlxTextAlign.CENTER);
     songName.screenCenter(X);
-    songName.cameras = [playtestCam];
     add(songName);
 
     variationButton = new ChartPlaytestMenuButtonListToggle(0, FlxG.height * 0.55, "Variation", targetSong.variations, function(value:String)
@@ -63,7 +68,6 @@ class ChartPlaytestMenu extends MusicBeatState
       variationButton.screenCenter(X);
     });
     variationButton.screenCenter(X);
-    variationButton.cameras = [playtestCam];
     add(variationButton);
 
     difficultyButton = new ChartPlaytestMenuButtonListToggle(0, FlxG.height * 0.45, "Difficulty",
@@ -73,7 +77,6 @@ class ChartPlaytestMenu extends MusicBeatState
       difficultyButton.screenCenter(X);
     });
     difficultyButton.screenCenter(X);
-    difficultyButton.cameras = [playtestCam];
     add(difficultyButton);
 
     playtestButton = new ChartPlaytestMenuButton(0, FlxG.height * 0.8, "Playtest Song", function()
@@ -88,7 +91,6 @@ class ChartPlaytestMenu extends MusicBeatState
       }
     });
     playtestButton.screenCenter(X);
-    playtestButton.cameras = [playtestCam];
     add(playtestButton);
 
     FlxTween.tween(playtestCam, {alpha: 1}, 0.5);
