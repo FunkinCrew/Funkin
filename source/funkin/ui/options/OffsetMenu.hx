91bdab01af0f2f51bfff1dfa7d680c139347ae40
zxcksharks (formerly xenkap) <datafetchsyserror@gmail.com>
2026-09-02 16:47:43 +0800

    Fix calibration offset direction
    that should fix it

--- source/funkin/ui/options/OffsetMenu.hx
+++ source/funkin/ui/options/OffsetMenu.hx
@@ -490,7 +490,7 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
     if (differences.length % 4 == 0 && calibrating)
     {
       var avg:Float = getAverage();
-      tempOffset = -Std.int(avg);
+      tempOffset = Std.int(avg);
       _lastOffset = appliedOffsetLerp;
       _offsetLerpTime = 0;
       trace('New offset calculated: ' + tempOffset + 'ms');
@@ -627,7 +627,7 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
         var arrow:ArrowData = getClosestArrowAtBeat(b);
 
         var closestBeat:Float = Math.round(b);
-        var diff:Float = closestBeat - b;
+        var diff:Float = b - closestBeat;
         var ms:Float = (diff * msPerBeat);
 
         if (arrow != null) // eric sees this and goes "OMG NULL REF!!!!"
@@ -826,7 +826,8 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
     var inputLatencyNs:Int64 = PreciseInputManager.getCurrentTimestamp() - input.timestamp;
     var inputLatencyMs:Float = inputLatencyNs.toFloat() / Constants.NS_PER_MS;
 
-    var noteDiff:Int = Std.int(note.noteData.time - inputPosition - inputLatencyMs);
+    var noteDiff:Int = Std.int(inputPosition - note.noteData.time + inputLatencyMs);
+
     addDifference(noteDiff);
 
     if (noteDiff == 0)
@@ -840,7 +841,7 @@ class OffsetMenu extends Page<OptionsState.OptionsMenuPageName>
     }
     else
     {
-      jumpInText.text = noteDiff > 0 ? 'Early!\n' + noteDiff + 'ms' : 'Late!\n' + noteDiff + 'ms';
+      jumpInText.text = noteDiff < 0 ? 'Early!\n' + noteDiff + 'ms' : 'Late!\n' + noteDiff + 'ms';
     }
 
     jumpInText.text += '\nAvg: ' + Std.int(getAverage()) + 'ms';
