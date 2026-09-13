a1189297e177e7f713bc11d15cc4452354b83568
zxcksharks (formerly xenkap) <datafetchsyserror@gmail.com>
2026-09-13 15:57:40 +0800

    Rework hold note scoring + Create SongScore.hx

--- source/funkin/play/notes/Strumline.hx
+++ source/funkin/play/notes/Strumline.hx
@@ -724,8 +724,7 @@ class Strumline extends FlxSpriteGroup
           holdNote.cover.kill();
         }
 
-        holdNote.visible = false;
-        holdNote.kill();
+        holdNote.endedNote = true;
       }
       else if (holdNote.missedNote && (holdNote.fullSustainLength > holdNote.sustainLength))
       {
