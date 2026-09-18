fe0d617026d6799aec4d972f10581d14bada02d1
Kade-gtihub <26305836+Kade-github@users.noreply.github.com>
2026-09-18 00:59:53 -0700

    Fix resync going backwards at the end of a song

--- source/funkin/play/PlayState.hx
+++ source/funkin/play/PlayState.hx
@@ -2765,20 +2765,15 @@ class PlayState extends MusicBeatSubState
     // Skip this if the music is paused (GameOver, Pause menu, start-of-song offset, etc.)
     if (!(FlxG.sound.music?.playing ?? false)) return;
 
-    var timeToPlayAt:Float = Math.min(
-      FlxG.sound.music.length - 1,
-      Math.max(Math.min(Conductor.instance.combinedOffset, 0), Conductor.instance.songPosition) - Conductor.instance.combinedOffset
-    );
+    var timeToPlayAt:Float = FlxG.sound.music.time;
+
+    if (timeToPlayAt >= FlxG.sound.music.length - 1) return;
+
     trace('Resyncing vocals to ${timeToPlayAt}');
 
-    FlxG.sound.music.pause();
     vocals.pause();
-
-    FlxG.sound.music.time = timeToPlayAt;
-    FlxG.sound.music.play(false, timeToPlayAt);
-
     vocals.time = timeToPlayAt;
-    vocals.play(false, timeToPlayAt);
+    vocals.play(true, timeToPlayAt);
   }
 
   /**
